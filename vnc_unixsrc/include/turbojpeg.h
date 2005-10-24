bdb4a9d756013686b62fd267d659fcaebd0766b2
DRC <dcommander@users.sourceforge.net>
2005-10-24 17:08:43 +0000

    Renamed JPEG API to TurboJPEG
    git-svn-id: svn+ssh://svn.code.sf.net/p/turbovnc/code/trunk@139 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

--- vnc_unixsrc/include/turbojpeg.h
+++ vnc_unixsrc/include/turbojpeg.h
@@ -25,26 +25,24 @@
 
 /* Subsampling */
 #define NUMSUBOPT 3
-#define HPJ_MINWIDTH 32
-#define HPJ_MINHEIGHT 16
 
-enum {HPJ_444=0, HPJ_422, HPJ_411};
+enum {TJ_444=0, TJ_422, TJ_411};
 
 /* Flags */
-#define HPJ_BGR       1
-#define HPJ_BOTTOMUP  2
-#define HPJ_FORCEMMX  8   /* Force IPP to use MMX code even if SSE available */
-#define HPJ_FORCESSE  16  /* Force IPP to use SSE1 code even if SSE2 available */
-#define HPJ_FORCESSE2 32  /* Force IPP to use SSE2 code (useful if auto-detect is not working properly) */
-#define HPJ_ALPHAFIRST 64 /* BGR buffer is ABGR and RGB buffer is ARGB */
+#define TJ_BGR       1
+#define TJ_BOTTOMUP  2
+#define TJ_FORCEMMX  8   /* Force IPP to use MMX code even if SSE available */
+#define TJ_FORCESSE  16  /* Force IPP to use SSE1 code even if SSE2 available */
+#define TJ_FORCESSE2 32  /* Force IPP to use SSE2 code (useful if auto-detect is not working properly) */
+#define TJ_ALPHAFIRST 64 /* BGR buffer is ABGR and RGB buffer is ARGB */
 
-typedef void* hpjhandle;
+typedef void* tjhandle;
 
-#define HPJPAD(p) (((p)+3)&(~3))
+#define TJPAD(p) (((p)+3)&(~3))
 #ifndef max
  #define max(a,b) ((a)>(b)?(a):(b))
 #endif
-#define HPJBUFSIZE(width,height) (max((max(width,16))*(max(height,16))*3,2048))
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -53,7 +51,7 @@ extern "C" {
 
 
 /*
-  hpjhandle hpjInitCompress(void)
+  tjhandle tjInitCompress(void)
 
   Creates a new JPEG compressor instance, allocates memory for the structures,
   and returns a handle to the instance.  Most applications will only
@@ -63,22 +61,22 @@ extern "C" {
 
   RETURNS: NULL on error
 */
-DLLEXPORT hpjhandle DLLCALL hpjInitCompress(void);
+DLLEXPORT tjhandle DLLCALL tjInitCompress(void);
 
 
 /*
-  int hpjCompress(hpjhandle j,
+  int tjCompress(tjhandle j,
      unsigned char *srcbuf, int width, int pitch, int height, int pixelsize,
      unsigned char *dstbuf, unsigned long *size,
      int jpegsubsamp, int jpegqual, int flags)
 
   [INPUT] j = instance handle previously returned from a call to
-     hpjInitCompress()
+     tjInitCompress()
   [INPUT] srcbuf = pointer to user-allocated image buffer containing pixels in
      RGB(A) or BGR(A) form
   [INPUT] width =  width (in pixels) of the source image
   [INPUT] pitch = bytes per line of the source image (width*pixelsize if the
-     bitmap is unpadded, else HPJPAD(width*pixelsize) if each line of the bitmap
+     bitmap is unpadded, else TJPAD(width*pixelsize) if each line of the bitmap
      is padded to the nearest 32-bit boundary, such as is the case for Windows
      bitmaps.  You can also be clever and use this parameter to skip lines, etc.,
      as long as the pitch is greater than 0.)
@@ -86,7 +84,7 @@ DLLEXPORT hpjhandle DLLCALL hpjInitCompress(void);
   [INPUT] pixelsize = size (in bytes) of each pixel in the source image
      RGBA and BGRA: 4, RGB and BGR: 3
   [INPUT] dstbuf = pointer to user-allocated image buffer which will receive
-     the JPEG image.  Use the macro HPJBUFSIZE(width, height) to determine
+     the JPEG image.  Use the macro TJBUFSIZE(width, height) to determine
      the appropriate size for this buffer based on the image width and height.
   [OUTPUT] size = pointer to unsigned long which receives the size (in bytes)
      of the compressed image
@@ -97,36 +95,37 @@ DLLEXPORT hpjhandle DLLCALL hpjInitCompress(void);
      image clarity (the human eye is more sensitive to small changes in
      brightness than small changes in color.)
 
-     HPJ_411: 4:1:1 subsampling.  Discards every other Cb, Cr pixel in both
+     TJ_411: 4:1:1 subsampling.  Discards every other Cb, Cr pixel in both
         horizontal and vertical directions.
-     HPJ_422: 4:2:2 subsampling.  Discards every other Cb, Cr pixel only in
+     TJ_422: 4:2:2 subsampling.  Discards every other Cb, Cr pixel only in
         the horizontal direction.
-     HPJ_444: no subsampling.
+     TJ_444: no subsampling.
 
   [INPUT] jpegqual = JPEG quality (an integer between 0 and 100 inclusive.)
   [INPUT] flags = the bitwise OR of one or more of the following
 
-     HPJ_BGR: The components of each pixel in the source image are stored in
+     TJ_BGR: The components of each pixel in the source image are stored in
         B,G,R order, not R,G,B
-     HPJ_BOTTOMUP: The source image is stored in bottom-up (Windows) order,
+     TJ_BOTTOMUP: The source image is stored in bottom-up (Windows) order,
         not top-down
-     HPJ_FORCEMMX: Valid only for the Intel Performance Primitives implementation
+     TJ_FORCEMMX: Valid only for the Intel Performance Primitives implementation
         of this codec-- force IPP to use MMX code (bypass CPU auto-detection)
-     HPJ_FORCESSE: Valid only for the Intel Performance Primitives implementation
+     TJ_FORCESSE: Valid only for the Intel Performance Primitives implementation
         of this codec-- force IPP to use SSE code (bypass CPU auto-detection)
-     HPJ_FORCESSE2: Valid only for the Intel Performance Primitives implementation
+     TJ_FORCESSE2: Valid only for the Intel Performance Primitives implementation
         of this codec-- force IPP to use SSE2 code (bypass CPU auto-detection)
 
   RETURNS: 0 on success, -1 on error
 */
-DLLEXPORT int DLLCALL hpjCompress(hpjhandle j,
+DLLEXPORT int DLLCALL tjCompress(tjhandle j,
 	unsigned char *srcbuf, int width, int pitch, int height, int pixelsize,
 	unsigned char *dstbuf, unsigned long *size,
 	int jpegsubsamp, int jpegqual, int flags);
 
+DLLEXPORT unsigned long DLLCALL TJBUFSIZE(int width, int height);
 
 /*
-  hpjhandle hpjInitDecompress(void)
+  tjhandle tjInitDecompress(void)
 
   Creates a new JPEG decompressor instance, allocates memory for the
   structures, and returns a handle to the instance.  Most applications will
@@ -136,17 +135,37 @@ DLLEXPORT int DLLCALL hpjCompress(hpjhandle j,
 
   RETURNS: NULL on error
 */
-DLLEXPORT hpjhandle DLLCALL hpjInitDecompress(void);
+DLLEXPORT tjhandle DLLCALL tjInitDecompress(void);
 
 
 /*
-  int hpjDecompress(hpjhandle j,
+  int tjDecompressHeader(tjhandle j,
+     unsigned char *srcbuf, unsigned long size,
+     int *width, int *height)
+
+  [INPUT] j = instance handle previously returned from a call to
+     tjInitDecompress()
+  [INPUT] srcbuf = pointer to a user-allocated buffer containing the JPEG image
+     to decompress
+  [INPUT] size = size of the JPEG image buffer (in bytes)
+  [OUTPUT] width = width (in pixels) of the JPEG image
+  [OUTPUT] height = height (in pixels) of the JPEG image
+
+  RETURNS: 0 on success, -1 on error
+*/
+DLLEXPORT int DLLCALL tjDecompressHeader(tjhandle j,
+	unsigned char *srcbuf, unsigned long size,
+	int *width, int *height);
+
+
+/*
+  int tjDecompress(tjhandle j,
      unsigned char *srcbuf, unsigned long size,
      unsigned char *dstbuf, int width, int pitch, int height, int pixelsize,
      int flags)
 
   [INPUT] j = instance handle previously returned from a call to
-     hpjInitDecompress()
+     tjInitDecompress()
   [INPUT] srcbuf = pointer to a user-allocated buffer containing the JPEG image
      to decompress
   [INPUT] size = size of the JPEG image buffer (in bytes)
@@ -156,7 +175,7 @@ DLLEXPORT hpjhandle DLLCALL hpjInitDecompress(void);
      a specific region of a larger buffer.
   [INPUT] width =  width (in pixels) of the destination image
   [INPUT] pitch = bytes per line of the destination image (width*pixelsize if the
-     bitmap is unpadded, else HPJPAD(width*pixelsize) if each line of the bitmap
+     bitmap is unpadded, else TJPAD(width*pixelsize) if each line of the bitmap
      is padded to the nearest 32-bit boundary, such as is the case for Windows
      bitmaps.  You can also be clever and use this parameter to skip lines, etc.,
      as long as the pitch is greater than 0.)
@@ -165,44 +184,44 @@ DLLEXPORT hpjhandle DLLCALL hpjInitDecompress(void);
      RGBA/RGBx and BGRA/BGRx: 4, RGB and BGR: 3
   [INPUT] flags = the bitwise OR of one or more of the following
 
-     HPJ_BGR: The components of each pixel in the destination image should be
+     TJ_BGR: The components of each pixel in the destination image should be
         written in B,G,R order, not R,G,B
-     HPJ_BOTTOMUP: The destination image should be stored in bottom-up
+     TJ_BOTTOMUP: The destination image should be stored in bottom-up
         (Windows) order, not top-down
-     HPJ_FORCEMMX: Valid only for the Intel Performance Primitives implementation
+     TJ_FORCEMMX: Valid only for the Intel Performance Primitives implementation
         of this codec-- force IPP to use MMX code (bypass CPU auto-detection)
-     HPJ_FORCESSE: Valid only for the Intel Performance Primitives implementation
+     TJ_FORCESSE: Valid only for the Intel Performance Primitives implementation
         of this codec-- force IPP to use SSE code (bypass CPU auto-detection)
-     HPJ_FORCESSE2: Valid only for the Intel Performance Primitives implementation
+     TJ_FORCESSE2: Valid only for the Intel Performance Primitives implementation
         of this codec-- force IPP to use SSE2 code (bypass CPU auto-detection)
 
   RETURNS: 0 on success, -1 on error
 */
-DLLEXPORT int DLLCALL hpjDecompress(hpjhandle j,
+DLLEXPORT int DLLCALL tjDecompress(tjhandle j,
 	unsigned char *srcbuf, unsigned long size,
 	unsigned char *dstbuf, int width, int pitch, int height, int pixelsize,
 	int flags);
 
 
 /*
-  int hpjDestroy(hpjhandle h)
+  int tjDestroy(tjhandle h)
 
   Frees structures associated with a compression or decompression instance
   
   [INPUT] h = instance handle (returned from a previous call to
-     hpjInitCompress() or hpjInitDecompress()
+     tjInitCompress() or tjInitDecompress()
 
   RETURNS: 0 on success, -1 on error
 */
-DLLEXPORT int DLLCALL hpjDestroy(hpjhandle h);
+DLLEXPORT int DLLCALL tjDestroy(tjhandle h);
 
 
 /*
-  char *hpjGetErrorStr(void)
+  char *tjGetErrorStr(void)
   
   Returns a descriptive error message explaining why the last command failed
 */
-DLLEXPORT char* DLLCALL hpjGetErrorStr(void);
+DLLEXPORT char* DLLCALL tjGetErrorStr(void);
 
 #ifdef __cplusplus
 }
