8e242b525989a8940646394fb92497c89c02cb1e
DRC <dcommander@users.sourceforge.net>
2008-07-12 18:59:36 +0000

    1.2.3
    git-svn-id: svn+ssh://svn.code.sf.net/p/turbovnc/code/trunk@660 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

--- vnc_winsrc/winvnc/zlib/zconf.in.h
+++ vnc_winsrc/winvnc/zlib/zconf.in.h
@@ -3,7 +3,7 @@
  * For conditions of distribution and use, see copyright notice in zlib.h
  */
 
-/* @(#) $Id: zconf.h,v 1.3 2008-07-12 18:59:36 dcommander Exp $ */
+/* @(#) $Id: zconf.in.h,v 1.1 2008-07-12 18:59:36 dcommander Exp $ */
 
 #ifndef ZCONF_H
 #define ZCONF_H
