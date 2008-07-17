50c56eedb1a69b3a57d123bffff149fcf625f659
DRC <dcommander@users.sourceforge.net>
2008-07-17 07:04:34 +0000

    Don't make connection profile match contingent upon double buffering or on available encodings
    git-svn-id: svn+ssh://svn.code.sf.net/p/turbovnc/code/trunk@677 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

--- vnc_winsrc/vncviewer/SessionDialog.cpp
+++ vnc_winsrc/vncviewer/SessionDialog.cpp
@@ -257,49 +257,37 @@ int SessionDialog::cmp(HWND hwnd)
 	HWND hCustomRadio = GetDlgItem(hwnd, IDC_CUSTOM_RADIO);
 	HWND hModemRadio = GetDlgItem(hwnd, IDC_MODEM_RADIO);
 	HWND hLocNetRadio = GetDlgItem(hwnd, IDC_LOC_NET_RADIO);
-	int i;
-	for (i = rfbEncodingRaw; i <= LASTENCODING; i++)
-		if ((m_pOpt->m_UseEnc[i] != false) && (i != rfbEncodingTight
-			&& i != rfbEncodingRaw && i != rfbEncodingCopyRect)) a = 0;
+
 	if (m_pOpt->m_UseEnc[rfbEncodingTight] != true) a = 0;
 	if (m_pOpt->m_UseEnc[rfbEncodingCopyRect] != true) a = 0;
 	if (m_pOpt->m_PreferredEncoding != rfbEncodingTight) a = 0;
 	if (m_pOpt->m_subsampLevel != TVNC_4X) a = 0;
 	if (m_pOpt->m_jpegQualityLevel != 30) a = 0;
 	if (!m_pOpt->m_enableJpegCompression) a = 0;
-	if (!m_pOpt->m_DoubleBuffer) a = 0;
 	if (a == 1) {
 		SendMessage(hModemRadio, BM_CLICK, 0, 0);
 		return a;
 	}
 
 	a = 2;
-	for (i = rfbEncodingRaw; i <= LASTENCODING; i++)
-		if ((m_pOpt->m_UseEnc[i] != false) && (i != rfbEncodingTight
-			&& i != rfbEncodingRaw && i != rfbEncodingCopyRect)) a = 0;
 	if (m_pOpt->m_UseEnc[rfbEncodingTight] != true) a = 0;
 	if (m_pOpt->m_UseEnc[rfbEncodingCopyRect] != true) a = 0;
 	if (m_pOpt->m_PreferredEncoding != rfbEncodingTight) a = 0;
 	if (m_pOpt->m_subsampLevel != TVNC_1X) a = 0;
 	if (m_pOpt->m_jpegQualityLevel != 95) a = 0;
 	if (!m_pOpt->m_enableJpegCompression) a = 0;
-	if (!m_pOpt->m_DoubleBuffer) a = 0;
 	if (a == 2) {
 		SendMessage(hLocNetRadio, BM_CLICK, 0, 0);
 		return a;
 	}
 
 	a = 3;
-	for (i = rfbEncodingRaw; i <= LASTENCODING; i++)
-		if ((m_pOpt->m_UseEnc[i] != false) && (i != rfbEncodingTight
-			&& i != rfbEncodingRaw && i != rfbEncodingCopyRect)) a = 0;
 	if (m_pOpt->m_UseEnc[rfbEncodingTight] != true) a = 0;
 	if (m_pOpt->m_UseEnc[rfbEncodingCopyRect] != true) a = 0;
 	if (m_pOpt->m_PreferredEncoding != rfbEncodingTight) a = 0;
 	if (m_pOpt->m_subsampLevel != TVNC_2X) a = 0;
 	if (m_pOpt->m_jpegQualityLevel != 80) a = 0;
 	if (!m_pOpt->m_enableJpegCompression) a = 0;
-	if (!m_pOpt->m_DoubleBuffer) a = 0;
 	if (a == 3) {
 		SendMessage(hCustomRadio, BM_CLICK, 0, 0);
 		return a;
@@ -316,9 +304,6 @@ int SessionDialog::cmp(HWND hwnd)
 
 void SessionDialog::SetConnectionProfile(int encoding, int subsamp, int qual)
 {
-	for (int i = rfbEncodingRaw; i <= LASTENCODING; i++)
-		m_pOpt->m_UseEnc[i] = false;
-
 	m_pOpt->m_UseEnc[rfbEncodingTight] = true;
 	m_pOpt->m_UseEnc[rfbEncodingCopyRect] = true;
 
@@ -326,7 +311,6 @@ void SessionDialog::SetConnectionProfile(int encoding, int subsamp, int qual)
 	m_pOpt->m_subsampLevel = subsamp;
 	m_pOpt->m_jpegQualityLevel = qual;
 	m_pOpt->m_enableJpegCompression = true;
-	m_pOpt->m_DoubleBuffer = true;
 }
 	
 	
