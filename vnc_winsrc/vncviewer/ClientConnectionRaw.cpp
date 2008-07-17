560793d8224d71464fefbd3d9badfe3debc63c1c
DRC <dcommander@users.sourceforge.net>
2008-07-17 05:24:47 +0000

    Moved optimized SetPixels functionality to parent class so other encodings can use it
    git-svn-id: svn+ssh://svn.code.sf.net/p/turbovnc/code/trunk@673 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

--- vnc_winsrc/vncviewer/ClientConnectionRaw.cpp
+++ vnc_winsrc/vncviewer/ClientConnectionRaw.cpp
@@ -41,84 +41,13 @@ void ClientConnection::ReadRawRect(rfbFramebufferUpdateRectHeader *pfburh) {
 	ReadExact(m_netbuf, numbytes);
 
 	SETUP_COLOR_SHORTCUTS;
+	InitSetPixels();
 
 	{
 		// No other threads can use bitmap DC
 		omni_mutex_lock l(m_bitmapdcMutex);
 		ObjectSelector b(m_hBitmapDC, m_hBitmap);							  \
 		PaletteSelector p(m_hBitmapDC, m_hPalette);							  \
-
-		if((m_myFormat.bitsPerPixel==24 || m_myFormat.bitsPerPixel==32)
-			&& rm==0xFF && gm==0xFF && bm==0xFF && (rs%8)==0 && (gs%8)==0
-			&& (bs%8)==0) {
-
-			// Fast path
-			int srcpf=-1, srcps=m_myFormat.bitsPerPixel/8;
-			if(rs==0 && gs==8 && bs==16) {
-				if(srcps==3) srcpf=FBX_RGB;
-				else if(srcps==4) srcpf=FBX_RGBA;
-			}
-			else if(rs==16 && gs==8 && bs==0) {
-				if(srcps==3) srcpf=FBX_BGR;
-				else if(srcps==4) srcpf=FBX_BGRA;
-			}
-			else if(rs==8 && gs==16 && bs==24 && srcps==4) srcpf=FBX_ARGB;
-			else if(rs==24 && gs==16 && bs==8 && srcps==4) srcpf=FBX_ABGR;
-			if(srcpf<0) {
-				vnclog.Print(0, _T("Invalid pixel format: rs=%d gs=%d bs=%d rm=%d gm=%d bm=%d\n"),
-					rs, gs, bs, rm, gm, bm);
-				return;
-			}
-
-			int i, j;
-			int srcstride=srcps*pfburh->r.w, dststride=fb.pitch;
-			char *srcptr=m_netbuf,
-				*dstptr=&fb.bits[pfburh->r.y*fb.pitch+pfburh->r.x*fbx_ps[fb.format]];
-			char *srcptr2, *dstptr2;
-			int srcbgr=fbx_bgr[srcpf], dstbgr=fbx_bgr[fb.format],
-				dstps=fbx_ps[fb.format], srcaf=fbx_alphafirst[fb.format],
-				dstaf=fbx_alphafirst[fb.format];
-
-			if(srcbgr==dstbgr && srcps==dstps)
-			{
-				int wps=pfburh->r.w*srcps;
-				if(srcaf) srcptr++;  if(dstaf) dstptr++;
-				if(srcaf || dstaf)  wps--;
-				for(i=0; i<pfburh->r.h; i++, srcptr+=srcstride, dstptr+=dststride)
-				{
-					memcpy(dstptr, srcptr, wps);
-				}
-			}
-			else
-			{
-				if(srcaf) srcptr++;  if(dstaf) dstptr++;
-				if(srcbgr==dstbgr)
-				{
- 					for(i=0; i<pfburh->r.h; i++, srcptr+=srcstride, dstptr+=dststride)
-					{
-						for(j=0, srcptr2=srcptr, dstptr2=dstptr; j<pfburh->r.w; j++,
-							srcptr2+=srcps, dstptr2+=fbx_ps[fb.format])
-						{
-							memcpy(dstptr2, srcptr2, 3);
-					 	}
-					}
-			 	}
-				else
-				{
-					for(i=0; i<pfburh->r.h; i++, srcptr+=srcstride, dstptr+=dststride)
-					{
-						for(j=0, srcptr2=srcptr, dstptr2=dstptr; j<pfburh->r.w; j++,
-							srcptr2+=srcps, dstptr2+=fbx_ps[fb.format])
-						{
-							dstptr2[2]=srcptr2[0];
-							dstptr2[1]=srcptr2[1];
-							dstptr2[0]=srcptr2[2];
-					 	}
-					}
-				}
-			}
-			return;
-		}
 
 		// This big switch is untidy but fast
 		switch (m_myFormat.bitsPerPixel) {
