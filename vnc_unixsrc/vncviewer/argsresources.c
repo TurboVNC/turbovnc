888cf8b491a7cc2c64d18ff5ad4cba0430134791
DRC <dcommander@users.sourceforge.net>
2010-05-14 22:54:22 +0000

    0.6.82
    git-svn-id: svn+ssh://svn.code.sf.net/p/turbovnc/code/trunk@1070 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

--- vnc_unixsrc/vncviewer/argsresources.c
+++ vnc_unixsrc/vncviewer/argsresources.c
@@ -437,7 +437,7 @@ void
 usage(void)
 {
   fprintf(stderr,
-	  "TurboVNC Viewer version 0.6.81 alpha\n"
+	  "TurboVNC Viewer version 0.6.82 alpha\n"
 	  "\n"
 	  "Usage: %s [<OPTIONS>] [<HOST>][:<DISPLAY#>]\n"
 	  "       %s [<OPTIONS>] [<HOST>][::<PORT#>]\n"
