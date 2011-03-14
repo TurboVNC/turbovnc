4f48bdd1de5b0725e3c4b2aae8b3e1b55112b189
DRC <dcommander@users.sourceforge.net>
2011-03-14 10:03:24 +0000

    Open Group superceded X consortium, so no need to specify both copyrights
    git-svn-id: svn+ssh://svn.code.sf.net/p/turbovnc/code/branches/1.0.x@1437 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

--- vnc_unixsrc/Xvnc/programs/Xserver/mi/mibstore.c
+++ vnc_unixsrc/Xvnc/programs/Xserver/mi/mibstore.c
@@ -1,46 +1,6 @@
 /* $XConsortium: mibstore.c,v 5.63 94/10/21 20:25:08 dpw Exp $ */
 /***********************************************************
 
-Copyright (c) 1987  X Consortium
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
-X CONSORTIUM BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
-AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-Except as contained in this notice, the name of the X Consortium shall not be
-used in advertising or otherwise to promote the sale, use or other dealings
-in this Software without prior written authorization from the X Consortium.
-
-
-Copyright 1987 by the Regents of the University of California
-
-                        All Rights Reserved
-
-Permission to use, copy, modify, and distribute this software and its
-documentation for any purpose and without fee is hereby granted, provided
-that the above copyright notice appear in all copies and that both that
-copyright notice and this permission notice appear in supporting
-documentation, and that the name X Consortium not be used in advertising or publicity
-pertaining to distribution of the software without specific, written prior
-permission.  
-
-The University of California makes no representations about the suitability
-of this software for any purpose.  It is provided "as is" without express or
-implied warranty.
-
 Copyright 1987, 1998  The Open Group
 
 Permission to use, copy, modify, distribute, and sell this software and its
