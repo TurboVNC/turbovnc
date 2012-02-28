fa641e19321e6a2b56d9b785009d0d724a07089c
DRC <dcommander@users.sourceforge.net>
2012-02-28 07:50:29 +0000

    Oops.  Need to up N_ENC_CAPS to accommodate ZRLE
    git-svn-id: svn+ssh://svn.code.sf.net/p/turbovnc/code/branches/1.0.x@1798 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

--- vnc_unixsrc/Xvnc/programs/Xserver/hw/vnc/rfbserver.c
+++ vnc_unixsrc/Xvnc/programs/Xserver/hw/vnc/rfbserver.c
@@ -5,7 +5,7 @@
 /*
  *  Copyright (C) 2010 University Corporation for Atmospheric Research.
  *                     All Rights Reserved.
- *  Copyright (C) 2009-2010 D. R. Commander.  All Rights Reserved.
+ *  Copyright (C) 2009-2012 D. R. Commander.  All Rights Reserved.
  *  Copyright (C) 2005-2008 Sun Microsystems, Inc.  All Rights Reserved.
  *  Copyright (C) 2004 Landmark Graphics Corporation.  All Rights Reserved.
  *  Copyright (C) 2000-2006 Constantin Kaplinsky.  All Rights Reserved.
@@ -697,7 +697,7 @@ rfbProcessClientInitMessage(cl)
 /* Update these constants on changing capability lists below! */
 #define N_SMSG_CAPS  0
 #define N_CMSG_CAPS  0
-#define N_ENC_CAPS  14
+#define N_ENC_CAPS  16
 
 void
 rfbSendInteractionCaps(cl)
