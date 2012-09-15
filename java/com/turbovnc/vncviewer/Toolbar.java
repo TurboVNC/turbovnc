8a98d4b46c8c37a5520e69a406888f4900ed7934
DRC <dcommander@users.sourceforge.net>
2012-09-15 08:43:18 +0000

    This should have been checked in with r2213
    git-svn-id: svn+ssh://svn.code.sf.net/p/turbovnc/code/trunk@2214 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

--- java/com/turbovnc/vncviewer/Toolbar.java
+++ java/com/turbovnc/vncviewer/Toolbar.java
@@ -1,4 +1,5 @@
 /* Copyright (C) 2011-2012 Brian P. Hinz
+ * Copyright (C) 2012 D. R. Commander.  All Rights Reserved.
  * 
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
