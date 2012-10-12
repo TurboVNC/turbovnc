TurboVNC/turbovnc

commit 8bb56837aee094747667243679f3a5c36f1aa6da

Fix an issue whereby, if the remote desktop was smaller than the local desktop, maximizing the viewer window and then selecting "Default position/size" would resize the window but leave black borders around it.
git-svn-id: svn+ssh://svn.code.sf.net/p/turbovnc/code/trunk@2304 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

java/com/turbovnc/vncviewer/Viewport.java
@@ -81,9 +81,9 @@ public class Viewport extends JFrame
             cc.opts.scalingFactor == Options.SCALE_FIXEDRATIO) {
           if ((sp.getSize().width != cc.desktop.scaledWidth) ||
               (sp.getSize().height != cc.desktop.scaledHeight)) {
-            int policy = ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER;
-            sp.setHorizontalScrollBarPolicy(policy);
             cc.desktop.setScaledSize();
+            sp.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+            sp.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
             sp.validate();
             if (getExtendedState() != JFrame.MAXIMIZED_BOTH &&
                 !cc.opts.fullScreen) {
@@ -103,8 +103,12 @@ public class Viewport extends JFrame
             }
           }
         } else {
-          int policy = ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED;
-          sp.setHorizontalScrollBarPolicy(policy);
+          sp.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+          sp.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+          if (sp.getSize().width >= cc.desktop.scaledWidth)
+            sp.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+          if (sp.getSize().height >= cc.desktop.scaledHeight)
+            sp.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
           sp.validate();
         }
       }

commit daa374f23f96d3b2ee911024ef275b403f17f5ce

Fix regression caused by r2304 (scrollbars need to be displayed even if the remote desktop area = the local desktop area, because due to window decorations, taskbars, etc., the viewer window's usable area is always going to be less than the local desktop area.)
git-svn-id: svn+ssh://svn.code.sf.net/p/turbovnc/code/trunk@2312 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

java/com/turbovnc/vncviewer/Viewport.java
@@ -105,10 +105,6 @@ public class Viewport extends JFrame
         } else {
           sp.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
           sp.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
-          if (sp.getSize().width >= cc.desktop.scaledWidth)
-            sp.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-          if (sp.getSize().height >= cc.desktop.scaledHeight)
-            sp.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
           sp.validate();
         }
         if ((sp.getSize().width > cc.desktop.scaledWidth) ||