TurboVNC/turbovnc

commit 3bde6a38d8534ebdb8b066a473d0ea82f9348221

If the remote desktop area is smaller than the viewer window, then make the unused area of the window black.
git-svn-id: svn+ssh://svn.code.sf.net/p/turbovnc/code/trunk@2357 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

java/com/turbovnc/vncviewer/Viewport.java
@@ -1,5 +1,5 @@
 /* Copyright (C) 2002-2005 RealVNC Ltd.  All Rights Reserved.
- * Copyright (C) 2011-2012 Brian P. Hinz
+ * Copyright (C) 2011-2013 Brian P. Hinz
  * Copyright (C) 2012 D. R. Commander.  All Rights Reserved.
  *
  * This is free software; you can redistribute it and/or modify
@@ -130,7 +130,7 @@ public class Viewport extends JFrame {
       setSize(w, h);
     }
     setLocation(x, y);
-    setBackground(Color.BLACK);
+    getContentPane().setBackground(Color.BLACK);
   }
 
   public void showToolbar(boolean show) {

commit 906807babbfb5afef576e374dbdab854dd1e6343

Fix several issues introduced by r2357.  Specifically, the unused area of the viewport was not being filled with black on some platforms, the small square between the scrollbars was being filled with black (which it shouldn't be), and on OS X, the unused area of the viewport did not remain black as the scrollbars were used.
git-svn-id: svn+ssh://svn.code.sf.net/p/turbovnc/code/trunk@2372 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

java/com/turbovnc/vncviewer/DesktopWindow.java
@@ -55,8 +55,7 @@ class DesktopWindow extends JPanel implements Runnable, MouseListener,
                        CConn cc_) {
     cc = cc_;
     setSize(width, height);
-    setBackground(Color.BLACK);
-    setOpaque(true);
+    setOpaque(false);
     GraphicsEnvironment ge =
       GraphicsEnvironment.getLocalGraphicsEnvironment();
     GraphicsDevice gd = ge.getDefaultScreenDevice();

java/com/turbovnc/vncviewer/Viewport.java
@@ -39,7 +39,7 @@ public class Viewport extends JFrame {
       new UIDefaults.LazyInputMap(new Object[]{}));
     sp = new JScrollPane();
     sp.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
-    sp.setBackground(Color.BLACK);
+    sp.getViewport().setBackground(Color.BLACK);
     InputMap im = sp.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
     int ctrlAltShiftMask = Event.SHIFT_MASK | Event.CTRL_MASK | Event.ALT_MASK;
     if (im != null) {
@@ -127,7 +127,6 @@ public class Viewport extends JFrame {
       setSize(w, h);
     }
     setLocation(x, y);
-    getContentPane().setBackground(Color.BLACK);
   }
 
   public void showToolbar(boolean show) {