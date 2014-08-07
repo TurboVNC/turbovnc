52163eddbb88ab1da4ece31b255c73a87744ac74
DRC <dcommander@users.sourceforge.net>
2014-08-07 08:49:56 +0000

    Only import the RFB classes that are actually used.  This makes it easier to produce a standalone build of ImageDrawTest.
    git-svn-id: svn+ssh://svn.code.sf.net/p/turbovnc/code/trunk@2789 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

--- java/com/turbovnc/vncviewer/ImageDrawTest.java
+++ java/com/turbovnc/vncviewer/ImageDrawTest.java
@@ -26,7 +26,7 @@ import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.*;
 
-import com.turbovnc.rfb.*;
+import com.turbovnc.rfb.PixelFormat;
 
 public class ImageDrawTest extends JFrame {
 
