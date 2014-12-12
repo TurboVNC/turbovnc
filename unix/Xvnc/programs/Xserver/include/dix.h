e4da23956be3361421a6f0c95549406e904a4b46
DRC <dcommander@users.sourceforge.net>
2014-12-12 13:44:58 +0000

    From e0e11644622a589129a01e11e5d105dc74a098de Mon Sep 17 00:00:00 2001 From: Alan Coopersmith <alan.coopersmith@oracle.com> Date: Wed, 22 Jan 2014 23:44:46 -0800 Subject: dix: integer overflow in REQUEST_FIXED_SIZE() [CVE-2014-8092 4/4]
    Force use of 64-bit integers when evaluating data provided by clients
    in 32-bit fields which can overflow when added or multiplied during
    checks.
    
    Reported-by: Ilja Van Sprundel <ivansprundel@ioactive.com>
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>
    Reviewed-by: Peter Hutterer <peter.hutterer@who-t.net>
    
    From 9802a0162f738de03585ca3f3b8a8266494f7d45 Mon Sep 17 00:00:00 2001
    From: Keith Packard <keithp@keithp.com>
    Date: Tue, 9 Dec 2014 09:30:59 -0800
    Subject: Missing parens in REQUEST_FIXED_SIZE macro [CVE-2014-8092 pt. 5]
    
    The 'n' parameter must be surrounded by parens in both places to
    prevent precedence from mis-computing things.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>
    Reviewed-by: Alan Coopersmith <alan.coopersmith@oracle.com>
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/turbovnc/code/trunk@2922 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

--- unix/Xvnc/programs/Xserver/include/dix.h
+++ unix/Xvnc/programs/Xserver/include/dix.h
@@ -74,7 +74,8 @@ SOFTWARE.
 
 #define REQUEST_FIXED_SIZE(req, n)\
     if (((sizeof(req) >> 2) > client->req_len) || \
-        (((sizeof(req) + (n) + 3) >> 2) != client->req_len)) \
+        (((n) >> 2) >= client->req_len) ||                              \
+        ((((uint64_t) sizeof(req) + (n) + 3) >> 2) != (uint64_t) client->req_len))  \
          return(BadLength)
 
 #define LEGAL_NEW_RESOURCE(id,client)\
