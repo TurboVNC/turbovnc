TurboVNC/turbovnc

commit 7eb2625a1d1dcd6a16750b5b405723da26386b52

Fix several cosmetic issues in the Java blitter:
(1) When Swing double buffering was enabled, an animation or video was playing on the server, and the user was interacting with one of the dialogs (Options, etc.) on the client, moving the dialog or interacting with its components would cause pixels from the remote desktop viewport underlying the dialog to be displayed at the upper left of the viewport.  This was known to be an issue with Java 7 on Windows but may have affected other platforms.

(2) Jumping the scrollbars from top to bottom on OS X under Java 8 would sometimes cause regions from the top or bottom of the remote desktop viewport to remain if it was scrolled to the opposite end.

(3) White pixels would sometimes appear around the Options Dialog GUI components on OS X when interacting with them.

(4) When using full-screen mode on Linux/Windows or non-Lion full-screen mode on Mac, scrolling the viewport would sometimes leave junk pixels in the areas of the window not covered by the remote desktop image.

The cause of these issues was two-fold:

-- We really need to be painting only in the EDT, period.  Not doing so was the primary cause of (1) and (2) and possibly (3).  However, using SwingUtilities.invokeAndWait() has too much overhead for small updates.  We now use repaint() instead of paintImmediately(), primarily because it's a fast way of ensuring that the painting occurs in the EDT.  There are other advantages, such as the fact that Swing can coalesce rapid back-to-back updates into a single paint.  We still use paintImmediately() when benchmarking, since concurrency isn't an issue there and we want to avoid the afore-mentioned update coalescing.  The other viewers take similar steps to synchronously blit whenever benchmarking is enabled.

java/com/turbovnc/vncviewer/DesktopWindow.java
@@ -219,10 +219,8 @@ class DesktopWindow extends JPanel implements Runnable, MouseListener,
     }
   }
 
-  // Update the actual window with the changed parts of the framebuffer.
-  // This is called from the EDT for the first couple of updates following
-  // a viewport size change but otherwise from the RFB thread (for performance
-  // reasons.)
+  // RFB thread: Update the actual window with the changed parts of the
+  // framebuffer.
   public void updateWindow() {
     double tBlitStart = getTime();
     Rect r = damage;
@@ -248,13 +246,19 @@ class DesktopWindow extends JPanel implements Runnable, MouseListener,
         // optimal performance under X11 without requiring MIT-SHM pixmaps.
         if (!swingDB)
           RepaintManager.currentManager(this).setDoubleBufferingEnabled(false);
-        paintImmediately(x, y, width, height);
+        if (cc.viewer.benchFile != null)
+          paintImmediately(x, y, r.width(), r.height());
+        else
+          repaint(x, y, r.width(), r.height());
       } else {
         int x = (cc.viewport.dx > 0) ? cc.viewport.dx + r.tl.x : r.tl.x;
         int y = (cc.viewport.dy > 0) ? cc.viewport.dy + r.tl.y : r.tl.y;
         if (!swingDB)
           RepaintManager.currentManager(this).setDoubleBufferingEnabled(false);
-        paintImmediately(x, y, r.width(), r.height());
+        if (cc.viewer.benchFile != null)
+          paintImmediately(x, y, r.width(), r.height());
+        else
+          repaint(x, y, r.width(), r.height());
       }
       damage.clear();
     }
@@ -379,10 +383,11 @@ class DesktopWindow extends JPanel implements Runnable, MouseListener,
   // EDT 
   public void paintComponent(Graphics g) {
     Graphics2D g2 = (Graphics2D) g;
-    if (repaintBackground && !swingDB) {
+    if (!swingDB &&
+        RepaintManager.currentManager(this).isDoubleBufferingEnabled())
+      // If double buffering is enabled, then this must be a system-triggered
+      // repaint, so we need to repaint all of the parent components.
       super.paintComponent(g);
-      repaintBackground = false;
-    }
     if (cc.viewport.dx > 0 || cc.viewport.dy > 0)
       g2.translate(cc.viewport.dx, cc.viewport.dy);
     if (cc.cp.width != scaledWidth || cc.cp.height != scaledHeight) {
@@ -664,7 +669,7 @@ class DesktopWindow extends JPanel implements Runnable, MouseListener,
   int cursorBackingX, cursorBackingY;
   java.awt.Cursor softCursor, noCursor;
   static Toolkit tk = Toolkit.getDefaultToolkit();
-  boolean swingDB, repaintBackground;
+  boolean swingDB;
 
   public int scaledWidth = 0, scaledHeight = 0;
   float scaleWidthRatio, scaleHeightRatio;

java/com/turbovnc/vncviewer/CConn.java
@@ -50,7 +50,7 @@ import com.turbovnc.network.Socket;
 import com.turbovnc.network.TcpSocket;
 
 public class CConn extends CConnection implements UserPasswdGetter,
-  OptionsDialogCallback, FdInStreamBlockCallback, Runnable {
+  OptionsDialogCallback, FdInStreamBlockCallback {
 
   public final PixelFormat getPreferredPF() { return fullColourPF; }
   static final PixelFormat VERY_LOW_COLOR_PF =
@@ -374,29 +374,10 @@ public class CConn extends CConnection implements UserPasswdGetter,
     if (!benchmark) requestNewUpdate();
   }
 
-  public void run() {
-    desktop.updateWindow();
-  }
-
   // RFB thread
   public void framebufferUpdateEnd() {
 
-    if (newViewport > 0) {
-      try {
-        SwingUtilities.invokeAndWait(this);
-      } catch (InterruptedException e) {
-      } catch (java.lang.reflect.InvocationTargetException e) {
-        Throwable cause = e.getCause();
-        if (cause instanceof ErrorException)
-          throw (ErrorException)cause;
-        if (cause instanceof WarningException)
-          throw (WarningException)cause;
-        else if (cause != null)
-          throw new SystemException(cause.toString());
-      }
-      newViewport--;
-    } else
-      desktop.updateWindow();
+    desktop.updateWindow();
 
     if (firstUpdate) {
       int width, height;
@@ -846,7 +827,6 @@ public class CConn extends CConnection implements UserPasswdGetter,
       viewport.setGeometry(span.x, span.y, span.width, span.height);
       viewport.dx = x - span.x;
       viewport.dy = y - span.y;
-      desktop.repaintBackground = true;
       return;
     }
 
@@ -879,7 +859,6 @@ public class CConn extends CConnection implements UserPasswdGetter,
     } else {
       sizeWindow();
     }
-    newViewport = 2;
   }
 
   // RFB thread: requestNewUpdate() requests an update from the server, having
@@ -1935,7 +1914,6 @@ public class CConn extends CConnection implements UserPasswdGetter,
   private boolean firstUpdate;
   private boolean pendingUpdate;
   private boolean continuousUpdates;
-  private int newViewport;
 
   private boolean forceNonincremental;
 

java/com/turbovnc/vncviewer/ImageDrawTest.java
@@ -119,6 +119,9 @@ public class ImageDrawTest extends JFrame {
 
     public void paintComponent(Graphics g) {
       Graphics2D g2 = (Graphics2D) g;
+      if (!swingDB &&
+          RepaintManager.currentManager(this).isDoubleBufferingEnabled())
+        super.paintComponent(g);
       g2.drawImage((iter % 2 == 0 ? im2.getImage() : im1.getImage()), 0, 0,
                    null);
       g2.dispose();

commit 3afd5b1c7eaad80a6b151eaa5f5dbf10e1282ef4

Fix a regression in r3106 that caused major painting issues when scaling was enabled.
git-svn-id: svn+ssh://svn.code.sf.net/p/turbovnc/code/branches/1.2.x@3115 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

java/com/turbovnc/vncviewer/DesktopWindow.java
@@ -247,9 +247,9 @@ class DesktopWindow extends JPanel implements Runnable, MouseListener,
         if (!swingDB)
           RepaintManager.currentManager(this).setDoubleBufferingEnabled(false);
         if (cc.viewer.benchFile != null)
-          paintImmediately(x, y, r.width(), r.height());
+          paintImmediately(x, y, width, height);
         else
-          repaint(x, y, r.width(), r.height());
+          repaint(x, y, width, height);
       } else {
         int x = (cc.viewport.dx > 0) ? cc.viewport.dx + r.tl.x : r.tl.x;
         int y = (cc.viewport.dy > 0) ? cc.viewport.dy + r.tl.y : r.tl.y;