c102fc232c8011cd657b98a72f5329a4345c679e
DRC <information@virtualgl.org>
2015-09-02 12:24:03 -0500

    Fix VeNCrypt when wrapped in Tight sec. selector Servers that support both the VeNCrypt and Tight security extensions (which currently includes only TurboVNC 2.1, AFAIK) have to treat VeNCrypt as an "authentication capability" within the Tight security selector.  This works fine already in the Java viewer, except that the VeNCrypt handler is designed to be called multiple times.  Thus, when VeNCrypt is wrapped in the Tight security selector, the Tight security handler needs to defer to the VeNCrypt security handler for the second and subsequent invocations.

--- java/com/turbovnc/rfb/CSecurityTight.java
+++ java/com/turbovnc/rfb/CSecurityTight.java
@@ -1,5 +1,5 @@
 /* Copyright (C) 2012 Brian P. Hinz
- * Copyright (C) 2012 D. R. Commander.  All Rights Reserved.
+ * Copyright (C) 2012, 2015 D. R. Commander.  All Rights Reserved.
  *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -49,6 +49,12 @@ public class CSecurityTight extends CSecurity {
     InStream is = cc.getInStream();
     OutStream os = cc.getOutStream();
     List<Integer> supportedAuthTypes = new ArrayList<Integer>();
+
+    if (cs instanceof CSecurityVeNCrypt) {
+      // VeNCrypt has already been selected using the Tight security selector,
+      // so defer any further handshaking to the VeNCrypt handler.
+      return cs.processMsg(cc);
+    }
 
     int nTunnels = is.readU32();
 
