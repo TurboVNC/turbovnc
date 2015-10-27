c8f9e471c68b9a2d8b5556daa54fa161e0eebc17
DRC <information@virtualgl.org>
2015-10-27 17:16:52 -0500

    Server: Comment tweak

--- unix/Xvnc/programs/Xserver/hw/vnc/rfbserver.c
+++ unix/Xvnc/programs/Xserver/hw/vnc/rfbserver.c
@@ -2247,7 +2247,7 @@ rfbSendServerCutText(char *str, int len)
 
 
 /*
- * rfbSendDesktopResize sends a DesktopSize message to all the clients.
+ * rfbSendDesktopResize sends a DesktopSize message to a specific client.
  */
 
 Bool
