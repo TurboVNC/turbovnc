TurboVNC/turbovnc

commit 44dbdc9706441484b8f407e280389fe05e17b02f

Server: Add VeNCrypt TLS support

unix/Xvnc/programs/Xserver/hw/vnc/auth.c
@@ -72,13 +72,12 @@ static int nAuthMethodsEnabled = 0;
 static int preferenceLimit = 1; /* Force one iteration of the loop in
                                    rfbSendAuthCaps() */
 
-Bool rfbOptNoAuth = FALSE;
-Bool rfbOptOtpAuth = FALSE;
-Bool rfbOptPamAuth = FALSE;
-Bool rfbOptRfbAuth = FALSE;
-
 char *rfbAuthOTPValue = NULL;
 int rfbAuthOTPValueLen = 0;
+#if USETLS
+char *rfbAuthX509Cert = NULL;
+char *rfbAuthX509Key = NULL;
+#endif
 
 
 static void
@@ -300,8 +299,6 @@ typedef struct {
     Bool enabled;
     Bool permitted;
     int preference;
-    Bool *optionSet;
-    const char *optionName;
     Bool requiredData;
     SecTypeData *secType;
     AuthCapData *authCap;
@@ -310,37 +307,149 @@ typedef struct {
 
 /*
  * Set the "permitted" member to TRUE if you want the auth method to be
- * available by default.  The value of the "permitted-auth-methods" config file
- * option will take precedence over the defaults below.
+ * available by default.  The value of the "permitted-security-types" config
+ * file option will take precedence over the defaults below.
  *
  * We permit the rfbAuthNone auth method by default for backward compatibility
  * and only enable it when either explicitly told to do so or if it is
  * permitted and no other auth methods were specified on the command line.
  */
 static AuthMethodData authMethods[] = {
-    { "none", FALSE, TRUE, -1, &rfbOptNoAuth, "", FALSE,
-      &secTypeNone, &authCapNone, rfbSecTypeNone },
+#if USETLS
+    /* name         enabled  permitted  preference  requiredData */
+    {  "tlsnone",   FALSE,   TRUE,      -1,         FALSE,
+    /* secType           authCap            subType */
+       &secTypeVeNCrypt, &authCapVeNCrypt,  rfbVeNCryptTLSNone },
 
-    { "vnc", FALSE, TRUE, -1, &rfbOptRfbAuth, "-rfbauth", TRUE,
-      &secTypeVncAuth, &authCapVncAuth, rfbSecTypeVncAuth },
+    {  "tlsvnc",    TRUE,    TRUE,      -1,         TRUE,
+       &secTypeVeNCrypt, &authCapVeNCrypt,  rfbVeNCryptTLSVnc },
 
-    { "otp", FALSE, TRUE, -1, &rfbOptOtpAuth, "-otpauth", TRUE,
-      &secTypeVncAuth, &authCapVncAuth, rfbSecTypeVncAuth },
+    {  "tlsotp",    TRUE,    TRUE,      -1,         TRUE,
+       &secTypeVeNCrypt, &authCapVeNCrypt,  rfbVeNCryptTLSVnc },
 
 #ifdef XVNC_AuthPAM
-    { "pam-userpwd", FALSE, TRUE, -1, &rfbOptPamAuth, "-pamauth", TRUE,
-      &secTypeTight, &authCapUnixLogin, -1 },
+    {  "tlsplain",  TRUE,    TRUE,      -1,         TRUE,
+       &secTypeVeNCrypt, &authCapVeNCrypt,  rfbVeNCryptTLSPlain },
+#endif
 
-    { "pam-userpwd", FALSE, TRUE, -1, &rfbOptPamAuth, "-pamauth", TRUE,
-      &secTypeVeNCrypt, &authCapVeNCrypt, rfbVeNCryptPlain },
+    {  "x509none",  FALSE,   TRUE,      -1,         FALSE,
+       &secTypeVeNCrypt, &authCapVeNCrypt,  rfbVeNCryptX509None },
+
+    {  "x509vnc",   TRUE,    TRUE,      -1,         TRUE,
+       &secTypeVeNCrypt, &authCapVeNCrypt,  rfbVeNCryptX509Vnc },
+
+    {  "x509otp",   TRUE,    TRUE,      -1,         TRUE,
+       &secTypeVeNCrypt, &authCapVeNCrypt,  rfbVeNCryptX509Vnc },
+
+#ifdef XVNC_AuthPAM
+    {  "x509plain", TRUE,    TRUE,      -1,         TRUE,
+       &secTypeVeNCrypt, &authCapVeNCrypt,  rfbVeNCryptX509Plain },
+#endif
+#endif
+
+    {  "none",      FALSE,   TRUE,      -1,         FALSE,
+       &secTypeNone,     &authCapNone,      rfbSecTypeNone },
+
+    {  "vnc",       TRUE,    TRUE,      -1,         TRUE,
+       &secTypeVncAuth,  &authCapVncAuth,   rfbSecTypeVncAuth },
+
+    {  "otp",       TRUE,    TRUE,      -1,         TRUE,
+       &secTypeVncAuth,  &authCapVncAuth,   rfbSecTypeVncAuth },
+
+#ifdef XVNC_AuthPAM
+    {  "unixlogin", TRUE,    TRUE,      -1,         TRUE,
+       &secTypeTight,    &authCapUnixLogin, -1 },
+
+    {  "plain",     TRUE,    TRUE,      -1,         TRUE,
+       &secTypeVeNCrypt, &authCapVeNCrypt,  rfbVeNCryptPlain },
 #endif
 
     { NULL }
 };
 
 
+Bool
+rfbOptOtpAuth(void)
+{
+    AuthMethodData *a;
+
+    for (a = authMethods; a->name != NULL; a++) {
+        if (!strcmp(&a->name[strlen(a->name) - 3], "otp") &&
+            a->enabled)
+            return TRUE;
+    }
+
+    return FALSE;
+}
+
+
+Bool
+rfbOptPamAuth(void)
+{
+    AuthMethodData *a;
+
+    for (a = authMethods; a->name != NULL; a++) {
+        if ((!strcmp(a->name, "unixlogin") ||
+             !strcmp(&a->name[strlen(a->name) - 5], "plain")) &&
+            a->enabled)
+            return TRUE;
+    }
+
+    return FALSE;
+}
+
+
+Bool
+rfbOptRfbAuth(void)
+{
+    AuthMethodData *a;
+
+    for (a = authMethods; a->name != NULL; a++) {
+        if (!strcmp(&a->name[strlen(a->name) - 3], "vnc") &&
+            a->enabled)
+            return TRUE;
+    }
+
+    return FALSE;
+}
+
+
+void
+rfbAuthParseCommandLine(char *securityTypes)
+{
+    char *p1 = securityTypes, *p2 = securityTypes;
+    AuthMethodData *a;
+
+    for (a = authMethods; a->name != NULL; a++)
+        a->enabled = FALSE;
+
+    do {
+        *p2 = *p1;
+        if (!isspace(*p2))
+            p2++;
+    } while (*p1++ != 0);
+
+    while (TRUE) {
+        p1 = strtok_r(securityTypes, ",", &p2);
+        securityTypes = NULL;
+        if (p1 == NULL)
+            break;
+
+        for (a = authMethods; a->name != NULL; a++) {
+            if (!strcasecmp(a->name, p1)) {
+                a->enabled = TRUE;
+                break;
+            }
+        }
+
+        if (a->name == NULL)
+            FatalError("ERROR: Unknown security type '%s'\n", p1);
+    }
+}
+
+
 static void
-setMethods(char *buf)
+setMethods(char *buf, Bool backwardCompatible)
 {
     char *saveptr;
     char *p;
@@ -349,7 +458,6 @@ setMethods(char *buf)
     preferenceLimit = 0;
     for (a = authMethods; a->name != NULL; a++) {
         a->permitted = FALSE;
-        a->enabled = FALSE;
         a->preference = -1;
         a->secType->advertise = FALSE;
     }
@@ -361,15 +469,20 @@ setMethods(char *buf)
             break;
 
         for (a = authMethods; a->name != NULL; a++) {
-            if (!strcmp(a->name, p)) {
-                a->permitted = TRUE;
-                a->preference = preferenceLimit++;
-            }
+            if (backwardCompatible && a->secType == &secTypeVeNCrypt)
+                continue;
+            if (!strcasecmp(a->name, p) ||
+                (backwardCompatible && !strcasecmp(a->name, "unixlogin") &&
+                 !strcasecmp(p, "pam-userpwd")))
+                break;
         }
 
-        if (preferenceLimit == 0) {
+        if (a->name == NULL) {
             FatalError("ERROR: Unknown auth method name '%s'\n", p);
         }
+
+        a->permitted = TRUE;
+        a->preference = preferenceLimit++;
     }
 }
 
@@ -473,6 +586,9 @@ ReadConfigFile(void)
         }
 #endif
 
+        /* permitted-auth-methods provides backward compatibility with TurboVNC
+           2.0.x and earlier.  It can only be used to enable non-VeNCrypt
+           security types. */
         n = 23;
         if (!strncmp(buf2, "permitted-auth-methods=", n)) {
             if (buf2[n] == '\0') {
@@ -480,7 +596,19 @@ ReadConfigFile(void)
                            rfbAuthConfigFile);
             }
 
-            setMethods(&buf2[n]);
+            setMethods(&buf2[n], TRUE);
+            continue;
+        }
+
+        /* permitted-security-types was introduced in TurboVNC 2.1. */
+        n = 25;
+        if (!strncmp(buf2, "permitted-security-types=", n)) {
+            if (buf2[n] == '\0') {
+                FatalError("ERROR in %s: permitted-security-types is empty!",
+                           rfbAuthConfigFile);
+            }
+
+            setMethods(&buf2[n], FALSE);
             continue;
         }
 
@@ -538,19 +666,14 @@ rfbAuthInit()
     ReadConfigFile();
 
     for (a = authMethods; a->name != NULL; a++) {
-        if (*(a->optionSet) == TRUE) {
+        if (a->enabled) {
             nSelected++;
             if (!a->permitted) {
-                if (a->optionName != NULL) {
-                    rfbLog("WARNING: authentication option '%s' is not permitted\n",
-                            a->optionName);
-                }
-
-                *(a->optionSet) = FALSE;
+                rfbLog("WARNING: security type '%s' is not permitted\n",
+                       a->name);
+                a->enabled = FALSE;
                 continue;
             }
-
-            a->enabled = TRUE;
         }
 
         if (a->enabled) {
@@ -584,7 +707,7 @@ rfbAuthInit()
     }
 
 #ifndef XVNC_AuthPAM
-    if (rfbOptPamAuth)
+    if (rfbOptPamAuth())
         rfbLog("WARNING: PAM support is not compiled in.\n");
 #endif
 
@@ -604,7 +727,7 @@ rfbAuthInit()
     }
 
 #ifdef XVNC_AuthPAM
-    if (rfbOptPamAuth && rfbAuthUserACL) {
+    if (rfbOptPamAuth() && rfbAuthUserACL) {
         struct passwd pbuf;
         struct passwd *pw;
         char buf[256];
@@ -726,23 +849,50 @@ rfbSendSecurityType(rfbClientPtr cl, int securityType)
 static void
 rfbSendSecurityTypeList(rfbClientPtr cl)
 {
-    int n;
-    SecTypeData **p;
+    int i, j, n;
+    AuthMethodData *a;
     SecTypeData *s;
     Bool tightAdvertised = FALSE;
 
+    /*
+     * When no preference order was set using "permitted-security-types", the
+     * default value of preferenceLimit (1) will cause us to execute the
+     * outer loop once.  In this case, the a->preference members will all
+     * be the default value (-1), and we skip the order testing.
+     */
     n = 0;
-    for (p = secTypes; *p != NULL; p++) {
-        if (n > MAX_SECURITY_TYPES) {
-            FatalError("rfbSendSecurityTypeList: # enabled security types > MAX_SECURITY_TYPES\n");
+    for (i = 0; i < preferenceLimit; i++) {
+        for (a = authMethods; a->name != NULL; a++) {
+            if (((a->preference != -1) && (i != a->preference)) ||
+                !a->enabled)
+                continue;
+
+            s = a->secType;
+
+            /*
+             * Check whether we have already advertised this security type
+             */
+            for (j = 0; j < n; j++) {
+                if (cl->securityTypes[j + 1] == s->securityType)
+                    break;
+            }
+
+            if (j < n)
+                continue;
+
+            if (n > MAX_SECURITY_TYPES)
+                FatalError("rfbSendSecurityTypeList: # enabled security types > MAX_SECURITY_TYPES\n");
+
+            if (s->advertise &&
+                (cl->protocol_minor_ver >= s->protocolMinorVer)) {
+                cl->securityTypes[++n] = s->securityType;
+                if (s->securityType == rfbSecTypeTight)
+                    tightAdvertised = TRUE;
+            }
         }
 
-        s = *p;
-        if (s->advertise && (cl->protocol_minor_ver >= s->protocolMinorVer)) {
-            cl->securityTypes[++n] = s->securityType;
-            if (s->securityType == rfbSecTypeTight)
-                tightAdvertised = TRUE;
-        }
+        if (n == 0)
+            FatalError("rfbSendSecurityTypeList: no security types enabled! This should not have happened!\n");
     }
 
     if (!tightAdvertised) {
@@ -787,6 +937,52 @@ rfbSendSecurityTypeList(rfbClientPtr cl)
         return;  \
     }
 
+#if USETLS
+#define TLS_INIT(anon)  \
+    if ((ctx = rfbssl_init(cl, anon)) == NULL) {  \
+        reply = 0;  \
+        WRITE(&reply, 1);  \
+        rfbClientAuthFailed(cl, rfbssl_geterr());  \
+        return;  \
+    }  \
+    reply = 1;  \
+    WRITE(&reply, 1);  \
+    cl->sslctx = ctx;  \
+    if ((ret = rfbssl_accept(cl)) < 0) {  \
+        rfbCloseClient(cl);  \
+        return;  \
+    } else if (ret == 1) {  \
+        rfbLog("Deferring TLS handshake\n");  \
+        cl->state = RFB_TLS_HANDSHAKE;  \
+        return;  \
+    }
+
+
+void
+rfbAuthTLSHandshake(rfbClientPtr cl)
+{
+    int ret;
+
+    if ((ret = rfbssl_accept(cl)) < 0) {
+        rfbCloseClient(cl);
+        return;
+    } else if (ret == 1)
+        return;
+
+    switch (cl->selectedAuthType) {
+    case rfbAuthNone:
+        rfbClientAuthSucceeded(cl, rfbAuthNone);
+        break;
+    case rfbAuthVNC:
+        rfbVncAuthSendChallenge(cl);
+        break;
+    case rfbAuthUnixLogin:
+        AuthPAMUserPwdRspFunc(cl);
+        break;
+    }
+}
+#endif
+
 
 void
 rfbVeNCryptAuthenticate(rfbClientPtr cl)
@@ -797,6 +993,10 @@ rfbVeNCryptAuthenticate(rfbClientPtr cl)
     int i, j;
     AuthMethodData *a;
     CARD32 subTypes[MAX_VENCRYPT_SUBTYPES], chosenType = 0;
+#if USETLS
+    rfbSslCtx *ctx;
+    int ret;
+#endif
 
     WRITE(&serverVersion.minor, 1);
     WRITE(&serverVersion.major, 1);
@@ -868,6 +1068,38 @@ rfbVeNCryptAuthenticate(rfbClientPtr cl)
     case rfbVeNCryptPlain:
         AuthPAMUserPwdRspFunc(cl);
         break;
+#if USETLS
+    case rfbVeNCryptTLSNone:
+        cl->selectedAuthType = rfbAuthNone;
+        TLS_INIT(TRUE);
+        rfbClientAuthSucceeded(cl, rfbAuthNone);
+        break;
+    case rfbVeNCryptTLSVnc:
+        cl->selectedAuthType = rfbAuthVNC;
+        TLS_INIT(TRUE);
+        rfbVncAuthSendChallenge(cl);
+        break;
+    case rfbVeNCryptTLSPlain:
+        cl->selectedAuthType = rfbAuthUnixLogin;
+        TLS_INIT(TRUE);
+        AuthPAMUserPwdRspFunc(cl);
+        break;
+    case rfbVeNCryptX509None:
+        cl->selectedAuthType = rfbAuthNone;
+        TLS_INIT(FALSE);
+        rfbClientAuthSucceeded(cl, rfbAuthNone);
+        break;
+    case rfbVeNCryptX509Vnc:
+        cl->selectedAuthType = rfbAuthVNC;
+        TLS_INIT(FALSE);
+        rfbVncAuthSendChallenge(cl);
+        break;
+    case rfbVeNCryptX509Plain:
+        cl->selectedAuthType = rfbAuthUnixLogin;
+        TLS_INIT(FALSE);
+        AuthPAMUserPwdRspFunc(cl);
+        break;
+#endif
     default:
         FatalError("rfbVeNCryptAuthenticate: chosen type is invalid (this should never occur)");
     }
@@ -1004,10 +1236,10 @@ rfbSendAuthCaps(rfbClientPtr cl)
         int i;
 
         /*
-         * When no preference order was set using "permitted-auth-methods", the
-         * default value of preferenceLimit (1) will cause us to execute the
-         * outer loop once.  In this case, the a->preference members will all
-         * be the default value (-1), and we skip the order testing.
+         * When no preference order was set using "permitted-security-types",
+         * the default value of preferenceLimit (1) will cause us to execute
+         * the outer loop once.  In this case, the a->preference members will
+         * all be the default value (-1), and we skip the order testing.
          */
         for (i = 0; i < preferenceLimit; i++) {
             for (a = authMethods; a->name != NULL; a++) {
@@ -1201,7 +1433,7 @@ rfbVncAuthProcessResponse(rfbClientPtr cl)
     }
 
     ok = FALSE;
-    if (rfbOptOtpAuth) {
+    if (rfbOptOtpAuth()) {
         if (rfbAuthOTPValue == NULL) {
             if (nAuthMethodsEnabled == 1) {
                 rfbClientAuthFailed(cl, "The one-time password has not been set on the server");
@@ -1227,7 +1459,7 @@ rfbVncAuthProcessResponse(rfbClientPtr cl)
         }
     }
 
-    if ((ok == FALSE) && rfbOptRfbAuth) {
+    if ((ok == FALSE) && rfbOptRfbAuth()) {
         numPasswords = vncDecryptPasswdFromFile2(rfbAuthPasswdFile,
                                                  passwdFullControl,
                                                  passwdViewOnly);

unix/Xvnc/programs/Xserver/hw/vnc/CMakeLists.txt
@@ -18,6 +18,37 @@ if(TVNC_NVCONTROL)
 	include_directories(${CMAKE_SOURCE_DIR}/unix/libXNVCtrl)
 endif()
 
+set(TVNC_USETLS "OpenSSL" CACHE STRING
+	"Implement server-side TLS encryption using the specified library [Options: OpenSSL (default), GnuTLS, None]")
+set_property(CACHE TVNC_USETLS PROPERTY STRINGS OpenSSL GnuTLS None)
+string(TOLOWER "${TVNC_USETLS}" TVNC_USETLS)
+
+if(TVNC_USETLS STREQUAL "openssl")
+	include(FindOpenSSL)
+	if(NOT OPENSSL_FOUND)
+		message(STATUS "OpenSSL not found.  Disabling TLS encryption")
+		set(TVNC_USEOPENSSL 0)
+	else()
+		set(RFBSSLSRC rfbssl_openssl.c)
+		message(STATUS "Enabling TLS encryption using OpenSSL")
+		add_definitions(-DUSETLS)
+		include_directories(${OPENSSL_INCLUDE_DIR})
+	endif()
+elseif(TVNC_USETLS STREQUAL "gnutls")
+	include(FindGnuTLS)
+	if(NOT GNUTLS_FOUND)
+		message(STATUS "GnuTLS not found.  Disabling TLS encryption")
+		set(TVNC_USEGNUTLS 0)
+	else()
+		set(RFBSSLSRC rfbssl_gnutls.c)
+		message(STATUS "Enabling TLS encryption using GnuTLS")
+		add_definitions(-DUSETLS ${GNUTLS_DEFINITIONS})
+		include_directories(${GNUTLS_INCLUDE_DIR})
+	endif()
+else()
+	message(STATUS "Disabling TLS encryption")
+endif()
+
 add_library(vnc STATIC
 	auth.c
 	cmap.c
@@ -46,4 +77,11 @@ add_library(vnc STATIC
 	zrleoutstream.c
 	zrlepalettehelper.c
 	${PAMSRC}
-	${NVCTRLSRC})
+	${NVCTRLSRC}
+	${RFBSSLSRC})
+
+if(TVNC_USETLS STREQUAL "openssl")
+	target_link_libraries(vnc ${OPENSSL_LIBRARIES})
+elseif(TVNC_USETLS STREQUAL "gnutls")
+	target_link_libraries(vnc ${GNUTLS_LIBRARIES})
+endif()

unix/Xvnc/programs/Xserver/hw/vnc/init.c
@@ -253,20 +253,29 @@ ddxProcessArgument(int argc, char *argv[], int i)
 
     if (strcasecmp(argv[i], "-rfbauth") == 0) {     /* -rfbauth passwd-file */
         if (i + 1 >= argc) UseMsg();
-        rfbOptRfbAuth = TRUE;
         rfbAuthPasswdFile = argv[i + 1];
         return 2;
     }
 
-    if (strcasecmp(argv[i], "-otpauth") == 0) {
-        rfbOptOtpAuth = TRUE;
-        return 1;
+    if (strcasecmp(argv[i], "-securitytypes") == 0) {
+        if (i + 1 >= argc) UseMsg();
+        rfbAuthParseCommandLine(argv[i + 1]);
+        return 2;
     }
 
-    if (strcasecmp(argv[i], "-pamauth") == 0) {
-        rfbOptPamAuth = TRUE;
-        return 1;
+#if USETLS
+    if (strcasecmp(argv[i], "-x509cert") == 0) {
+        if (i + 1 >= argc) UseMsg();
+        rfbAuthX509Cert = argv[i + 1];
+        return 2;
     }
+
+    if (strcasecmp(argv[i], "-x509key") == 0) {
+        if (i + 1 >= argc) UseMsg();
+        rfbAuthX509Key = argv[i + 1];
+        return 2;
+    }
+#endif
 
     if (strcasecmp(argv[i], "-noreverse") == 0) {
         rfbAuthDisableRevCon = TRUE;
@@ -519,7 +528,7 @@ InitOutput(ScreenInfo *screenInfo, int argc, char **argv)
                                   strlen("VNC_LAST_CLIENT_ID"), TRUE);
     VNC_CONNECT = MakeAtom("VNC_CONNECT", strlen("VNC_CONNECT"), TRUE);
 
-    if (rfbOptOtpAuth)
+    if (rfbOptOtpAuth())
         VNC_OTP = MakeAtom("VNC_OTP", strlen("VNC_OTP"), TRUE);
 
 #ifdef XVNC_AuthPAM
@@ -970,7 +979,7 @@ rfbRootPropertyChange(PropertyPtr pProp)
         free(host);
     }
 
-    if (rfbOptOtpAuth && (pProp->propertyName == VNC_OTP) &&
+    if (rfbOptOtpAuth() && (pProp->propertyName == VNC_OTP) &&
         (pProp->type == XA_STRING) && (pProp->format == 8)) {
         if (pProp->size == 0) {
             if (rfbAuthOTPValue != NULL) {
@@ -1157,9 +1166,13 @@ ddxUseMsg()
     ErrorF("-rfbport port          TCP port for RFB protocol\n");
     ErrorF("-rfbwait time          max time in ms to wait for RFB client\n");
     ErrorF("-nocursor              don't display a cursor\n");
-    ErrorF("-rfbauth passwd-file   enable VNC Password authentication\n");
-    ErrorF("-otpauth               enable One-Time Password (OTP) authentication\n");
-    ErrorF("-pamauth               enable PAM User/Password authentication\n");
+    ErrorF("-rfbauth passwd-file   specify password file for VNC Password authentication\n");
+    ErrorF("-securitytypes types   list of security types that the server should support\n");
+    ErrorF("                       (one or more of:  tlsvnc, tlsotp, tlsplain, tlsnone,\n");
+    ErrorF("                       x509vnc, x509otp, x509plain, x509none, vnc, otp,\n");
+    ErrorF("                       unixlogin, plain, none.)  See docs.\n");
+    ErrorF("-x509cert file         specify filename of X.509 signed certificate\n");
+    ErrorF("-x509key file          specify filename of X.509 private key\n");
     ErrorF("-noreverse             disable reverse connections\n");
     ErrorF("-noclipboardsend       disable server->client clipboard synchronization\n");
     ErrorF("-noclipboardrecv       disable client->server clipboard synchronization\n");
@@ -1200,13 +1213,10 @@ ddxUseMsg()
  * rfbLog prints a time-stamped message to the log file (stderr.)
  */
 
-void rfbLog(char *format, ...)
+void vrfbLog(char *format, va_list args)
 {
-    va_list args;
     char buf[256];
     time_t clock;
-
-    va_start(args, format);
 
     time(&clock);
     strftime(buf, 255, "%d/%m/%Y %H:%M:%S ", localtime(&clock));
@@ -1214,7 +1224,15 @@ void rfbLog(char *format, ...)
 
     vfprintf(stderr, format, args);
     fflush(stderr);
+}
 
+
+void rfbLog(char *format, ...)
+{
+    va_list args;
+
+    va_start(args, format);
+    vrfbLog(format, args);
     va_end(args);
 }
 

unix/Xvnc/programs/Xserver/hw/vnc/rfb.h
@@ -160,6 +160,10 @@ typedef void (*rfbTranslateFnType)(char *table, rfbPixelFormat *in,
  * Per-client structure.
  */
 
+#if USETLS
+typedef struct _rfbSslCtx rfbSslCtx;
+#endif
+
 typedef struct rfbClientRec {
 
     int sock;
@@ -177,6 +181,9 @@ typedef struct rfbClientRec {
         RFB_TUNNELING_TYPE,     /* establishing tunneling (RFB v.3.7t) */
         RFB_AUTH_TYPE,          /* negotiating authentication (RFB v.3.7t) */
         RFB_AUTHENTICATION,     /* authenticating (VNC authentication) */
+#if USETLS
+        RFB_TLS_HANDSHAKE,      /* waiting for TLS handshake to complete */
+#endif
         RFB_INITIALISATION,     /* sending initialisation messages */
         RFB_NORMAL              /* normal protocol messages */
     } state;
@@ -359,6 +366,10 @@ typedef struct rfbClientRec {
     Bool pendingDesktopResize;
     int reason;
 
+#if USETLS
+    rfbSslCtx *sslctx;
+#endif
+
 } rfbClientRec, *rfbClientPtr;
 
 
@@ -477,6 +488,7 @@ extern struct in6_addr interface6;
 extern int family;
 
 extern int rfbBitsPerPixel(int depth);
+extern void vrfbLog(char *format, va_list args);
 extern void rfbLog(char *format, ...);
 extern void rfbLogPerror(char *str);
 
@@ -626,7 +638,7 @@ extern int rfbInterframe;
 extern void rfbNewClientConnection(int sock);
 extern rfbClientPtr rfbReverseConnection(char *host, int port, int id);
 extern void rfbClientConnectionGone(rfbClientPtr cl);
-extern void rfbProcessClientMessage(int sock);
+extern void rfbProcessClientMessage(rfbClientPtr cl);
 extern void rfbNewUDPConnection(int sock);
 extern void rfbProcessUDPInput(int sock);
 extern Bool rfbSendFramebufferUpdate(rfbClientPtr cl);
@@ -700,11 +712,12 @@ extern void httpCheckFds(void);
 
 void rfbAuthInit(void);
 void rfbAuthProcessResponse(rfbClientPtr cl);
+void rfbAuthParseCommandLine(char *buf);
 extern char* rfbAuthConfigFile;
 
-extern Bool  rfbOptOtpAuth;
-extern Bool  rfbOptPamAuth;
-extern Bool  rfbOptRfbAuth;
+extern Bool  rfbOptOtpAuth(void);
+extern Bool  rfbOptPamAuth(void);
+extern Bool  rfbOptRfbAuth(void);
 
 extern char* rfbAuthOTPValue;
 extern int   rfbAuthOTPValueLen;
@@ -720,12 +733,19 @@ extern void rfbAuthRevokeUser(const char* name);
 #endif
 
 extern char *rfbAuthPasswdFile;
+#if USETLS
+extern char *rfbAuthX509Cert;
+extern char *rfbAuthX509Key;
+#endif
 
 extern void rfbAuthNewClient(rfbClientPtr cl);
 extern void rfbProcessClientSecurityType(rfbClientPtr cl);
 extern void rfbProcessClientTunnelingType(rfbClientPtr cl);
 extern void rfbProcessClientAuthType(rfbClientPtr cl);
 extern void rfbVncAuthProcessResponse(rfbClientPtr cl);
+#if USETLS
+extern void rfbAuthTLSHandshake(rfbClientPtr cl);
+#endif
 
 extern void rfbClientConnFailed(rfbClientPtr cl, char *reason);
 extern void rfbClientAuthFailed(rfbClientPtr cl, char *reason);
@@ -804,6 +824,22 @@ extern Bool rfbSendCursorPos(rfbClientPtr cl, ScreenPtr pScreen);
 
 extern void rfbResetStats(rfbClientPtr cl);
 extern void rfbPrintStats(rfbClientPtr cl);
+
+
+#if USETLS
+
+/* rfbssl_*.c */
+
+rfbSslCtx *rfbssl_init(rfbClientPtr cl, Bool anon);
+int rfbssl_accept(rfbClientPtr cl);
+int rfbssl_pending(rfbClientPtr cl);
+int rfbssl_peek(rfbClientPtr cl, char *buf, int bufsize);
+int rfbssl_read(rfbClientPtr cl, char *buf, int bufsize);
+int rfbssl_write(rfbClientPtr cl, const char *buf, int bufsize);
+void rfbssl_destroy(rfbClientPtr cl);
+char *rfbssl_geterr(void);
+
+#endif
 
 
 #endif /* __RFB_H__ */

unix/Xvnc/programs/Xserver/hw/vnc/rfbserver.c
@@ -534,22 +534,11 @@ rfbClientConnectionGone(rfbClientPtr cl)
  */
 
 void
-rfbProcessClientMessage(int sock)
+rfbProcessClientMessage(rfbClientPtr cl)
 {
-    rfbClientPtr cl, cl2;
+    rfbClientPtr cl2;
 
-    for (cl = rfbClientHead; cl; cl = cl->next) {
-        if (sock == cl->sock)
-            break;
-    }
-
-    if (!cl) {
-        rfbLog("rfbProcessClientMessage: unknown socket %d\n", sock);
-        rfbCloseSock(sock);
-        return;
-    }
-
-    rfbCorkSock(sock);
+    rfbCorkSock(cl->sock);
 
     if (cl->pendingSyncFence) {
       cl->syncFence = TRUE;
@@ -569,6 +558,12 @@ rfbProcessClientMessage(int sock)
     case RFB_AUTH_TYPE:         /* protocol versions 3.7t, 3.8t */
         rfbProcessClientAuthType(cl);
         break;
+#if USETLS
+    case RFB_TLS_HANDSHAKE:
+        rfbAuthTLSHandshake(cl);
+        break;
+#endif
+
     case RFB_AUTHENTICATION:
         rfbAuthProcessResponse(cl);
         break;
@@ -592,7 +587,7 @@ rfbProcessClientMessage(int sock)
       cl->syncFence = FALSE;
     }
 
-    rfbUncorkSock(sock);
+    rfbUncorkSock(cl->sock);
 }
 
 

commit b757e1e7913f444c6be4a3b81e706557d82c4fea

Xvnc: Fix segfault if -rfbauth not specified
Regression introduced by 44dbdc9706441484b8f407e280389fe05e17b02f
(TLS encryption support)

unix/Xvnc/programs/Xserver/hw/vnc/auth.c
@@ -5,7 +5,7 @@
  */
 
 /*
- *  Copyright (C) 2010, 2012-2015 D. R. Commander.  All Rights Reserved.
+ *  Copyright (C) 2010, 2012-2016 D. R. Commander.  All Rights Reserved.
  *  Copyright (C) 2010 University Corporation for Atmospheric Research.
  *                     All Rights Reserved.
  *  Copyright (C) 2003-2006 Constantin Kaplinsky.  All Rights Reserved.
@@ -1496,6 +1496,11 @@ rfbVncAuthProcessResponse(rfbClientPtr cl)
     }
 
     if ((ok == FALSE) && rfbOptRfbAuth()) {
+        if (!rfbAuthPasswdFile) {
+            rfbClientAuthFailed(cl, "No VNC password file specified on the server (did you forget -rfbauth?)");
+            return;
+        }
+
         numPasswords = vncDecryptPasswdFromFile2(rfbAuthPasswdFile,
                                                  passwdFullControl,
                                                  passwdViewOnly);