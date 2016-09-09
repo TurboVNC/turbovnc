22a2626a14f24095983d72b71baec3f776b59216
DRC <information@virtualgl.org>
2016-09-08 23:15:43 -0500

    Server: Ensure RFB_NORMAL state before sending FBU
    It is unknown whether this really fixes anything, but it is conceivable
    that a drawing operation or cursor operation or RANDR operation could
    trigger an update while a client was initializing.

--- unix/Xvnc/programs/Xserver/hw/vnc/rfbserver.c
+++ unix/Xvnc/programs/Xserver/hw/vnc/rfbserver.c
@@ -1744,6 +1744,8 @@ rfbSendFramebufferUpdate(rfbClientPtr cl)
 
     if (cl->syncFence) return TRUE;
 
+    if (cl->state != RFB_NORMAL) return TRUE;
+
     if (rfbProfile) {
         tUpdateStart = gettime();
         if (tStart < 0.) tStart = tUpdateStart;
