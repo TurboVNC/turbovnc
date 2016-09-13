TurboVNC/turbovnc

commit 5e7d286dd70f05df72465297a5aca0e706db7b95

Java: Send new desktop size requests immediately
This commit causes the Java viewer to request a full framebuffer update
immediately after the Options dialog is dismissed, if the remote desktop
size was changed in that dialog and the new desktop resize mode is
"Auto" or "Manual".  Previously, the new desktop size was not
communicated to the server until the next framebuffer update was
received.

This also avoids an unnecessary viewport reconfigure when switching the
remote desktop size mode to "Server".

java/com/turbovnc/vncviewer/CConn.java
@@ -1374,13 +1374,15 @@ public class CConn extends CConnection implements UserPasswdGetter,
     Options.DesktopSize oldDesktopSize = opts.desktopSize;
     opts.setDesktopSize(options.desktopSize.getSelectedItem().toString());
     if (desktop != null && !opts.desktopSize.isEqual(oldDesktopSize)) {
-      reconfigure = true;
       if (oldDesktopSize.mode != Options.SIZE_AUTO &&
           opts.desktopSize.mode == Options.SIZE_AUTO &&
           viewport.lionFSSupported() && opts.fullScreen &&
           options.fullScreen.isSelected())
         defaultSize = true;
-      firstUpdate = true;
+      if (opts.desktopSize.mode != Options.SIZE_SERVER) {
+        reconfigure = true;
+        firstUpdate = true;
+      }
     }
 
     int oldSpan = opts.span;
@@ -1432,6 +1434,14 @@ public class CConn extends CConnection implements UserPasswdGetter,
         sizeWindow(true);
       else
         reconfigureViewport(false);
+    }
+    // Force a framebuffer update if we're initiating a manual or auto remote
+    // desktop resize.  Otherwise, it won't occur until the mouse is moved or
+    // something changes on the server (manual) or until the window is resized
+    // (auto.)
+    if (firstUpdate) {
+      forceNonincremental = true;
+      requestNewUpdate();
     }
   }
 

commit 5838048097008092e554cdc007114c2f7620aadd

Java: Fix NPE when dismissing Options dialog
Regression introduced by 5e7d286dd70f05df72465297a5aca0e706db7b95
If the Options dialog is popped up from the New TurboVNC Connection
dialog, then we can't request a new framebuffer update when the dialog
is dismissed, or else we'll encounter a NullPointerException in line
1297.  Thus, we need to ensure that the RFB state is "NORMAL" before
requesting a new update.

java/com/turbovnc/vncviewer/CConn.java
@@ -1440,7 +1440,7 @@ public class CConn extends CConnection implements UserPasswdGetter,
     // desktop resize.  Otherwise, it won't occur until the mouse is moved or
     // something changes on the server (manual) or until the window is resized
     // (auto.)
-    if (firstUpdate) {
+    if (firstUpdate && state() == RFBSTATE_NORMAL) {
       forceNonincremental = true;
       requestNewUpdate();
     }