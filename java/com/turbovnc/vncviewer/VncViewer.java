TurboVNC/turbovnc

commit 535110388555035e9838d67803318de22485cf20

Java: Remove the ability to run as an applet
It's time.  Refer to change log for details.

java/com/turbovnc/vncviewer/VncViewer.java
@@ -37,8 +37,8 @@ import com.turbovnc.rdr.*;
 import com.turbovnc.rfb.*;
 import com.turbovnc.network.*;
 
-public class VncViewer extends javax.swing.JApplet
-  implements Runnable, ActionListener, OptionsDialogCallback {
+public class VncViewer implements Runnable, ActionListener,
+  OptionsDialogCallback {
   public static final String PRODUCT_NAME = "TurboVNC Viewer";
   public static String copyrightYear = null;
   public static String copyright = null;
@@ -245,35 +245,33 @@ public class VncViewer extends javax.swing.JApplet
       //    subsequent calls to getInsets().
       // Thus, we have to compute the insets globally using a dummy JFrame.
       // Dear Swing, eff ewe.
-      if (!embed.getValue()) {
-        final JFrame frame = new JFrame();
-        // Under certain Linux WM's, the insets aren't valid until
-        // componentResized() is called (see above.)
-        if (isX11()) {
-          frame.addComponentListener(new ComponentAdapter() {
-            public void componentResized(ComponentEvent e) {
-              synchronized(frame) {
-                if (frame.isVisible() && frame.getExtendedState() == JFrame.NORMAL) {
-                  insets = frame.getInsets();
-                  frame.notifyAll();
-                }
+      final JFrame frame = new JFrame();
+      // Under certain Linux WM's, the insets aren't valid until
+      // componentResized() is called (see above.)
+      if (isX11()) {
+        frame.addComponentListener(new ComponentAdapter() {
+          public void componentResized(ComponentEvent e) {
+            synchronized(frame) {
+              if (frame.isVisible() && frame.getExtendedState() == JFrame.NORMAL) {
+                insets = frame.getInsets();
+                frame.notifyAll();
               }
             }
-          });
-          frame.setExtendedState(JFrame.NORMAL);
-          frame.setVisible(true);
-          synchronized(frame) {
-            while (insets == null)
-              frame.wait();
           }
-          frame.setVisible(false);
-        } else {
-          frame.setVisible(true);
-          insets = frame.getInsets();
-          frame.setVisible(false);
+        });
+        frame.setExtendedState(JFrame.NORMAL);
+        frame.setVisible(true);
+        synchronized(frame) {
+          while (insets == null)
+            frame.wait();
         }
-        frame.dispose();
+        frame.setVisible(false);
+      } else {
+        frame.setVisible(true);
+        insets = frame.getInsets();
+        frame.setVisible(false);
       }
+      frame.dispose();
     } catch (Exception e) {
       vlog.error("Could not set insets:");
       vlog.error("  " + e.toString());
@@ -345,8 +343,6 @@ public class VncViewer extends javax.swing.JApplet
   }
 
   public VncViewer(String[] argv) {
-    applet = false;
-
     UserPreferences.load("global");
 
     setVersion();
@@ -445,8 +441,6 @@ public class VncViewer extends javax.swing.JApplet
     }
 
     setGlobalOptions();
-
-    embed.setParam(false);
   }
 
   public static void usage() {
@@ -479,13 +473,6 @@ public class VncViewer extends javax.swing.JApplet
     System.exit(1);
   }
 
-  public VncViewer() {
-    applet = true;
-    UserPreferences.load("global");
-    setVersion();
-    setGlobalOptions();
-  }
-
   public VncViewer(Socket sock_) {
     sock = sock_;
     UserPreferences.load("global");
@@ -506,35 +493,9 @@ public class VncViewer extends javax.swing.JApplet
       newViewer(oldViewer, null, false);
   }
 
-  public void init() {
-    vlog.debug("init called");
-    Container parent = getParent();
-    while (!parent.isFocusCycleRoot()) {
-      parent = parent.getParent();
-    }
-    parent.setFocusable(false);
-    parent.setFocusTraversalKeysEnabled(false);
-    setLookAndFeel();
-    setBackground(Color.white);
-  }
-
   public void start() {
     vlog.debug("start called");
-    String host = null;
-    if (applet && nViewers == 0) {
-      Configuration.readAppletParams(this);
-      if (embed.getValue()) {
-        fullScreen.setParam(false);
-        noNewConn.setParam(true);
-        scalingFactor.setParam("100");
-      }
-      String str = getParameter("LogLevel");
-      if (str != null)
-        LogWriter.setLogParams(str);
-      setGlobalOptions();
-      host = opts.serverName;
-    } else if (!applet)
-      host = opts.serverName;
+    String host = opts.serverName;
     if (host != null && host.indexOf(':') < 0 &&
         opts.port > 0) {
       opts.serverName = host + ((opts.port >= 5900 && opts.port <= 5999) ?
@@ -546,20 +507,15 @@ public class VncViewer extends javax.swing.JApplet
   }
 
   public void exit(int n) {
-    if (nViewers > 0)
+    if (nViewers > 0) {
       nViewers--;
-    if (nViewers > 0 || embed.getValue())
       return;
-    if (applet) {
-      destroy();
-    } else {
-      System.exit(n);
     }
+    System.exit(n);
   }
 
   // If "Reconnect" button is pressed
   public void actionPerformed(ActionEvent e) {
-    getContentPane().removeAll();
     start();
   }
 
@@ -582,39 +538,20 @@ public class VncViewer extends javax.swing.JApplet
       title = "TurboVNC Viewer : Unexpected Error";
       e.printStackTrace();
     }
-    if (embed.getValue()) {
-      getContentPane().removeAll();
-      JLabel label = new JLabel("<html><center><b>" + title + "</b><p><i>" +
-                                msg + "</i></center></html>", JLabel.CENTER);
-      label.setFont(new Font("Helvetica", Font.PLAIN, 24));
-      label.setMaximumSize(new Dimension(getSize().width, 100));
-      label.setVerticalAlignment(JLabel.CENTER);
-      label.setAlignmentX(Component.CENTER_ALIGNMENT);
-      JButton button = new JButton("Reconnect");
-      button.addActionListener(this);
-      button.setMaximumSize(new Dimension(200, 30));
-      button.setAlignmentX(Component.CENTER_ALIGNMENT);
-      setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
-      add(label);
-      add(button);
-      validate();
-      repaint();
-    } else {
-      JOptionPane pane;
-      Object[] options = { UIManager.getString("OptionPane.yesButtonText"),
-                           UIManager.getString("OptionPane.noButtonText") };
-      if (reconnect)
-        pane = new JOptionPane(msg + "\nReconnect?", msgType,
-                               JOptionPane.YES_NO_OPTION, null, options,
-                               options[1]);
-      else
-        pane = new JOptionPane(msg, msgType);
-      JDialog dlg = pane.createDialog(null, title);
-      dlg.setAlwaysOnTop(true);
-      dlg.setVisible(true);
-      if (reconnect && pane.getValue() == options[0])
-        start();
-    }
+    JOptionPane pane;
+    Object[] options = { UIManager.getString("OptionPane.yesButtonText"),
+                         UIManager.getString("OptionPane.noButtonText") };
+    if (reconnect)
+      pane = new JOptionPane(msg + "\nReconnect?", msgType,
+                             JOptionPane.YES_NO_OPTION, null, options,
+                             options[1]);
+    else
+      pane = new JOptionPane(msg, msgType);
+    JDialog dlg = pane.createDialog(null, title);
+    dlg.setAlwaysOnTop(true);
+    dlg.setVisible(true);
+    if (reconnect && pane.getValue() == options[0])
+      start();
   }
 
   static void showAbout(Component comp) {
@@ -630,8 +567,6 @@ public class VncViewer extends javax.swing.JApplet
       VncViewer.url, JOptionPane.INFORMATION_MESSAGE);
     pane.setIcon(VncViewer.logoIcon128);
     JDialog dlg = pane.createDialog(comp, "About TurboVNC Viewer");
-    if (VncViewer.embed.getValue())
-      dlg.setAlwaysOnTop(true);
     dlg.setVisible(true);
   }
 
@@ -682,11 +617,11 @@ public class VncViewer extends javax.swing.JApplet
       options.scalingFactor.setEnabled(false);
     } else if (opts.desktopSize.mode == Options.SIZE_SERVER) {
       options.desktopSize.setSelectedItem("Server");
-      options.scalingFactor.setEnabled(!VncViewer.embed.getValue());
+      options.scalingFactor.setEnabled(true);
     } else {
       options.desktopSize.setSelectedItem(opts.desktopSize.width + "x" +
                                           opts.desktopSize.height);
-      options.scalingFactor.setEnabled(!VncViewer.embed.getValue());
+      options.scalingFactor.setEnabled(true);
     }
 
     options.gateway.setEnabled(false);
@@ -846,8 +781,6 @@ public class VncViewer extends javax.swing.JApplet
                           !VncViewer.noReconnect.getValue());
           exitStatus = 1;
           if (cc != null) cc.deleteWindow();
-        } else if (cc.shuttingDown && embed.getValue()) {
-          reportException(new WarningException("Connection closed"));
         } else {
           cc = null;
         }
@@ -997,10 +930,9 @@ public class VncViewer extends javax.swing.JApplet
   static BoolParameter alwaysShowConnectionDialog
   = new BoolParameter("AlwaysShowConnectionDialog",
   "Always show the \"New TurboVNC Connection\" dialog even if the server " +
-  "has been specified in an applet parameter or on the command line.  This " +
-  "defaults to 1 if the viewer is being run as an applet.  This parameter " +
-  "has no effect if SSH tunneling is enabled (the \"New TurboVNC " +
-  "Connection\" dialog is never shown in that case.)", false);
+  "has been specified on the command line.  This parameter has no effect " +
+  "if SSH tunneling is enabled (the \"New TurboVNC Connection\" dialog is " +
+  "never shown in that case.)", false);
 
   static BoolParameter clientRedirect
   = new BoolParameter("ClientRedirect", null, false);
@@ -1215,13 +1147,6 @@ public class VncViewer extends javax.swing.JApplet
   "default behavior.)  Setting this parameter to \"Server\" or \"0\" " +
   "disables remote desktop resizing and uses the desktop size set by the " +
   "server.", "Auto", "WxH, Auto, or Server");
-
-  static BoolParameter embed
-  = new BoolParameter("Embed",
-  "If the TurboVNC Viewer is being run as an applet, display its output to " +
-  "an embedded frame in the browser window rather than to a dedicated " +
-  "window.  This also has the effect of setting FullScreen=0, " +
-  "NoNewConn=1, and Scale=100.", false);
 
   static BoolParameter fsAltEnter
   = new BoolParameter("FSAltEnter",
@@ -1511,7 +1436,6 @@ public class VncViewer extends javax.swing.JApplet
 
   Thread thread;
   Socket sock;
-  static boolean applet;
   static int nViewers;
   static LogWriter vlog = new LogWriter("main");
   FileInStream benchFile;

doc/index.html
@@ -3,7 +3,7 @@
 <head>
 <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
 <meta name="language" content="en">
-<meta name="date" content="2017-08-05T11:28:36">
+<meta name="date" content="2017-08-05T11:36:19">
 <meta name="generator" content="deplate.rb 0.8.5">
 <title>User&rsquo;s Guide for TurboVNC 2.2</title>
 <link rel="start" href="index.html" title="Frontpage">
@@ -922,17 +922,15 @@ server machine, type the following command:</p>
 <p><a name="Zero_Install"></a></p>
 
 <p>When a TurboVNC session is created, it automatically starts a miniature 
-web server that serves up the Java TurboVNC Viewer as either an applet 
-or a Java Web Start app.  This allows you to easily connect to the 
-TurboVNC session from a machine that does not have the TurboVNC Viewer 
-installed locally.  If one of the official TurboVNC binary packages is 
-installed on the server, then it will automatically send the appropriate 
+web server that serves up the Java TurboVNC Viewer as a Java Web Start 
+app.  This allows you to easily connect to the TurboVNC session from a 
+machine that does not have the TurboVNC Viewer installed locally.  If 
+one of the official TurboVNC binary packages is installed on the server, 
+then the miniature web server will automatically send the appropriate 
 x86 or x86-64 libjpeg-turbo JNI library for Linux, OS X, or Windows 
-clients when launching the TurboVNC Viewer using Java Web Start.  If 
-using the Java TurboVNC Viewer as an applet, then you can install 
-<span class="remote"><a href="http://www.sourceforge.net/projects/libjpeg-turbo/files" class="remote">one 
-of the official libjpeg-turbo packages</a></span><a name="idx0033"></a> 
-on the client machine to accelerate JPEG decoding.</p>
+clients when launching the TurboVNC Viewer. This provides most of the 
+advantages of the standalone TurboVNC viewers, including native levels 
+of performance on popular client platforms.</p>
 
 <p>To launch the Java TurboVNC Viewer from a web browser, point your web 
 browser to:</p>
@@ -950,29 +948,21 @@ connect.</p>
 <p><code>http://my_server:5801</code></p>
 
 <p>This will download a JNLP file to your computer, which you can open in 
-Java Web Start.  Add <code>/applet</code> to the URL to launch the 
-viewer as a Java applet instead (as of this writing, browsers are 
-starting to do away with Java plugins, so running the viewer as an 
-applet is more of a legacy feature.)</p>
+Java Web Start.</p>
 
 <p>You can add viewer parameters to the URL using the following format:</p>
 
 <p><code>http://</code><em><code>{turbovnc_server}</code></em><code>:{5800+</code><em><code>n</code></em><code>}?</code><em><code>{param1}</code></em><code>=</code><em><code>{value1}</code></em><code>&amp;</code><em><code>{param2}</code></em><code>=</code><em><code>{value2}</code></em></p>
 
-<p>Examples:</p>
-
-<p><code>http://my_server:5801/applet?embed=1&amp;tunnel=1</code></p>
-
-<p>will run the viewer as an applet in the browser window and tunnel the 
-VNC connection through SSH.</p>
+<p>Example:</p>
 
 <p><code>http://my_server:5801?tunnel=1&amp;samp=2x&amp;quality=80</code></p>
 
-<p>will run the viewer as a JWS app, tunnel the VNC connection through SSH, 
-and enable Medium-Quality JPEG.</p>
+<p>will tunnel the VNC connection through SSH and enable Medium-Quality 
+JPEG.</p>
 
 <div class="important"><p class="important">
-NOTE: As of Java 7 Update 51, self-signed JARs are not allowed to run in the Java browser plug-in or JWS by default.  This is not an issue if you are using the official TurboVNC binary packages, but if you are building a self-signed version of the Java TurboVNC Viewer for development purposes, then you will need to add <code>http://{turbovnc_server}:{http_port}</code> (for example, <code>http://my_server:5801</code>) to Java&rsquo;s Exception Site List, which can be found under the &ldquo;Security&rdquo; tab in the Java Control Panel.
+NOTE: As of Java 7 Update 51, self-signed JARs are not allowed to run in Java Web Start by default.  This is not an issue if you are using the official TurboVNC binary packages, but if you are building a self-signed version of the Java TurboVNC Viewer for development purposes, then you will need to add <code>http://{turbovnc_server}:{http_port}</code> (for example, <code>http://my_server:5801</code>) to Java&rsquo;s Exception Site List, which can be found under the &ldquo;Security&rdquo; tab in the Java Control Panel.
 </p></div>
 
 <div class="important"><p class="important">
@@ -991,11 +981,8 @@ TurboVNC Viewer on machines that don&rsquo;t already have a VNC viewer
 installed (for instance, for the purpose of collaborating with 
 colleagues who don&rsquo;t normally use TurboVNC.)</p>
 
-<p>To set up a large-scale zero-install deployment of the Java TurboVNC 
-Viewer, it is desirable to serve up the JAR files from a dedicated web 
-server.  When deployed using JWS, the Java TurboVNC Viewer provides all 
-of the advantages of a standalone native viewer, including native levels 
-of performance on most platforms.</p>
+<p>To set up a large-scale deployment of the Java TurboVNC Viewer, however, 
+it is desirable to serve up the JAR files from a dedicated web server.</p>
 
 <p>For the purposes of this guide, it is assumed that the reader has some 
 knowledge of web server administration.</p>
@@ -1011,7 +998,7 @@ knowledge of web server administration.</p>
         packages for Linux, or you can download 
         <code>libjpeg-turbo-{version}-jws.zip</code> from libjpeg-turbo 1.4.0 or 
         later (available at 
-        <span class="remote"><a href="http://sourceforge.net/projects/libjpeg-turbo/files" class="remote">http://sourceforge.net/projects/libjpeg-turbo/files</a></span><a name="idx0034"></a>.)  
+        <span class="remote"><a href="http://sourceforge.net/projects/libjpeg-turbo/files" class="remote">http://sourceforge.net/projects/libjpeg-turbo/files</a></span><a name="idx0033"></a>.)  
         Note that only the JARs included in the official TurboVNC packages are 
         signed using an official code signing certificate. <br /><br />
     </li>
@@ -1076,7 +1063,7 @@ knowledge of web server administration.</p>
         </p></div>
         This is just a minimal example.  Refer to the 
         <span class="remote"><a href="http://docs.oracle.com/javase/6/docs/technotes/guides/javaws/" class="remote">Java 
-        Web Start documentation</a></span><a name="idx0035"></a> for additional 
+        Web Start documentation</a></span><a name="idx0034"></a> for additional 
         fields that you might want to add. <br /><br />
     </li>
     <li class="Itemize-1 Itemize asterisk">
@@ -1199,7 +1186,7 @@ environment variables.  See Section
 <a href="#VNC_VIA_CMD" class="ref">11.2</a> for more details.</p>
 
 <div class="important"><p class="important">
-NOTE: In this release of TurboVNC, the TurboVNC Viewer for Linux/Un*x and Mac platforms is actually the Java TurboVNC Viewer, which is packaged along with the libjpeg-turbo JNI library and necessary glueware to make the viewer behave and perform like a standalone native viewer.  Since the Java TurboVNC Viewer contains an embedded SSH client, the <code>via</code> and <code>tunnel</code> parameters can also be used when the viewer is run as an applet or deployed using Java Web Start.
+NOTE: In this release of TurboVNC, the TurboVNC Viewer for Linux/Un*x and Mac platforms is actually the Java TurboVNC Viewer, which is packaged along with the libjpeg-turbo JNI library and necessary glueware to make the viewer behave and perform like a standalone native viewer.  Since the Java TurboVNC Viewer contains an embedded SSH client, the <code>via</code> and <code>tunnel</code> parameters can also be used when the viewer is deployed using Java Web Start.
 </p></div>
 
 <div class="important"><p class="important">
@@ -1340,7 +1327,7 @@ TurboVNC compresses images:</p>
         </li>
         <li class="Ordered-0">
             whether or not <a href="#InterframeComparison">interframe 
-            comparison</a><a name="idx0036"></a> should be used
+            comparison</a><a name="idx0035"></a> should be used
         </li></ol> See Section 
         <a href="#AdvancedCompression" class="ref">7.2</a> below for more 
         details.
@@ -1392,7 +1379,7 @@ options described above:</p>
     <td class="standard">30</td>
     <td class="standard">4x</td>
     <td class="standard">7</td>
-    <td class="standard">For subrectangles that have a high number of unique colors, this encoding method produces very noticeable image compression artifacts.  However, it performs optimally on low-bandwidth connections.  If image quality is more critical than performance, then use one of the other encoding methods or take advantage of the <a href="#LR">Lossless Refresh feature</a><a name="idx0037"></a>.  In addition to reducing the JPEG quality to a &ldquo;minimum usable&rdquo; level, this encoding method also enables interframe comparison and Compression Level 2 (CL 7 = CL 2 + interframe comparison.)  Compression Level 2 can reduce bandwidth for low-color application workloads that are not good candidates for JPEG compression.</td>
+    <td class="standard">For subrectangles that have a high number of unique colors, this encoding method produces very noticeable image compression artifacts.  However, it performs optimally on low-bandwidth connections.  If image quality is more critical than performance, then use one of the other encoding methods or take advantage of the <a href="#LR">Lossless Refresh feature</a><a name="idx0036"></a>.  In addition to reducing the JPEG quality to a &ldquo;minimum usable&rdquo; level, this encoding method also enables interframe comparison and Compression Level 2 (CL 7 = CL 2 + interframe comparison.)  Compression Level 2 can reduce bandwidth for low-color application workloads that are not good candidates for JPEG compression.</td>
   </tr>
   <tr class="standard">
     <td class="standard">&ldquo;Lossless Tight&rdquo;</td>
@@ -1743,15 +1730,15 @@ the <code>vncviewer</code> script (Linux/Un*x), the
 <code>vncviewer-java.bat</code> script or the Start Menu shortcut 
 (Windows), or the standalone TurboVNC Viewer app (Mac).  If the viewer 
 is launched with Java Web Start using the <a href="#JWS">recommended 
-procedure</a><a name="idx0038"></a> or the 
+procedure</a><a name="idx0037"></a> or the 
 <a href="#Zero_Install">TurboVNC Server&rsquo;s built-in web 
-server</a><a name="idx0039"></a>, then the appropriate libjpeg-turbo JNI 
+server</a><a name="idx0038"></a>, then the appropriate libjpeg-turbo JNI 
 library will be downloaded along with it, assuming that the client 
 machine is running Windows, OS X, or Linux.  For other deployment 
 scenarios, the Java TurboVNC Viewer will find the libjpeg-turbo JNI 
 library if 
 <span class="remote"><a href="http://www.sourceforge.net/projects/libjpeg-turbo/files" class="remote">one 
-of the official libjpeg-turbo packages</a></span><a name="idx0040"></a> 
+of the official libjpeg-turbo packages</a></span><a name="idx0039"></a> 
 is installed on the client machine.</p>
 
 <p>If you suspect for whatever reason that JPEG decoding is not being 
@@ -2404,7 +2391,7 @@ vglrun&nbsp;-d&nbsp;:0.1&nbsp;{3D&nbsp;application}
 <p>In order to realize the full performance benefits of TurboVNC, it is 
 necessary to use a TurboVNC server and a TurboVNC viewer in concert.  
 However, TurboVNC is fully compatible with 
-<span class="remote"><a href="http://www.tigervnc.com" class="remote">TigerVNC</a></span><a name="idx0041"></a>, 
+<span class="remote"><a href="http://www.tigervnc.com" class="remote">TigerVNC</a></span><a name="idx0040"></a>, 
 TightVNC, RealVNC, and other VNC flavors.  You can use the TurboVNC 
 Viewer to connect to a non-TurboVNC server (or vice versa), although 
 this will generally result in some decrease in performance.</p>
@@ -3040,10 +3027,9 @@ JAVA_TOOL_OPTIONS=-Dturbovnc.turbojpeg=0&nbsp;/opt/TurboVNC/bin/vncviewer
 
 <p>to start the TurboVNC Viewer without JPEG acceleration.</p>
 
-<p>Refer to the default <code>index.vnc</code> and 
-<code>VncViewer.jnlp</code> files installed with the TurboVNC Server for 
-an example of how to specify Java command-line arguments in an applet or 
-Java Web Start environment.</p>
+<p>Refer to the default <code>VncViewer.jnlp</code> file installed with the 
+TurboVNC Server for an example of how to specify Java command-line 
+arguments in a Java Web Start environment.</p>
 
 <p><a name="turbovnc.forcealpha"></a></p>
 

java/CMakeLists.txt
@@ -169,11 +169,6 @@ endif()
 
 if(TVNC_BUILDSERVER)
 
-add_custom_target(index.vnc ALL
-	COMMAND ${CMAKE_COMMAND} -E copy_if_different
-		${SRCDIR}/${CLASSPATH}/index.vnc ${BINDIR}/index.vnc
-	DEPENDS ${SRCDIR}/${CLASSPATH}/index.vnc)
-
 set(TVNCJNIOPENCOMMENT "<!--")
 set(TVNCJNICLOSECOMMENT "-->")
 if(TVNC_INCLUDEJNIJARS)
@@ -283,8 +278,6 @@ install(FILES ${BINDIR}/VncViewer.jar DESTINATION ${CMAKE_INSTALL_JAVADIR})
 install(FILES ${SRCDIR}/${CLASSPATH}/README.txt
 	DESTINATION ${CMAKE_INSTALL_JAVADIR})
 if(TVNC_BUILDSERVER)
-	install(FILES ${SRCDIR}/${CLASSPATH}/index.vnc
-		DESTINATION ${CMAKE_INSTALL_JAVADIR})
 	install(FILES ${BINDIR}/VncViewer.jnlp DESTINATION ${CMAKE_INSTALL_JAVADIR})
 	install(FILES ${SRCDIR}/${CLASSPATH}/favicon.ico
 		DESTINATION ${CMAKE_INSTALL_JAVADIR})

java/com/turbovnc/rfb/Configuration.java
@@ -142,16 +142,6 @@ public class Configuration {
     }
   }
 
-  public static void readAppletParams(java.applet.Applet applet) {
-    VoidParameter current = head;
-    while (current != null) {
-      String str = applet.getParameter(current.getName());
-      if (str != null)
-        current.setParam(str);
-      current = current.next;
-    }
-  }
-
   public static void load(String filename) {
     if (filename == null)
       return;

commit a95dfc5f81594f5d900da70d1f8c68264a42a259

Java: Fix hang when exiting due to error
Regression caused by 535110388555035e9838d67803318de22485cf20

This also removes some vestigial applet code that should have been
removed with 535110388555035e9838d67803318de22485cf20.

java/com/turbovnc/vncviewer/VncViewer.java
@@ -37,8 +37,7 @@ import com.turbovnc.rdr.*;
 import com.turbovnc.rfb.*;
 import com.turbovnc.network.*;
 
-public class VncViewer implements Runnable, ActionListener,
-  OptionsDialogCallback {
+public class VncViewer implements Runnable, OptionsDialogCallback {
   public static final String PRODUCT_NAME = "TurboVNC Viewer";
   public static String copyrightYear = null;
   public static String copyright = null;
@@ -507,16 +506,11 @@ public class VncViewer implements Runnable, ActionListener,
   }
 
   public void exit(int n) {
-    if (nViewers > 0) {
+    if (nViewers > 0)
       nViewers--;
+    if (nViewers > 0)
       return;
-    }
     System.exit(n);
-  }
-
-  // If "Reconnect" button is pressed
-  public void actionPerformed(ActionEvent e) {
-    start();
   }
 
   void reportException(Exception e) {