e4855dcfb446ae63a71635a4ddd832785ef89c8c
DRC <information@libjpeg-turbo.org>
2017-09-20 19:37:19 +0000

    Server: Compiler warning

--- unix/Xvnc/programs/Xserver/xkb/xkb.c
+++ unix/Xvnc/programs/Xserver/xkb/xkb.c
@@ -3974,13 +3974,11 @@ _XkbSetNamesCheck(ClientPtr client, DeviceIntPtr dev,
                   xkbSetNamesReq * stuff, CARD32 *data)
 {
     XkbDescRec *xkb;
-    XkbNamesRec *names;
     CARD32 *tmp;
     Atom bad = None;
 
     tmp = data;
     xkb = dev->key->xkbInfo->desc;
-    names = xkb->names;
 
     if (stuff->which & XkbKeyTypeNamesMask) {
         int i;
