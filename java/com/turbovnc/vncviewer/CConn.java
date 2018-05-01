TurboVNC/turbovnc

commit 296404be205d608eb0fca3c7182987b757524fe2

Java: Fix multi-connection KB grabbing issues
A description of a Gordian knot:

This started with an observation that, when opening a new viewer
connection, the old connection window would detect that it lost focus,
and it would ungrab the keyboard.  Because ungrabbing the keyboard
applies to the whole process, this led to a race condition whereby
the keyboard was often ungrabbed in the new connection window, despite
the viewer thinking that it was grabbed.

Step 1: When a connection window loses focus, only ungrab the keyboard
if focus was lost to a non-TurboVNC window.

Easy enough, but this led to another problem: if multiple connection
windows were active and focus switched between them, the keyboard
grabbing state would remain unchanged, even if the grab mode differed
between the connections.  On Un*x/X11 systems, keyboard grabbing applies
to the whole process, so

Step 2: When a connection window regains focus, grab or ungrab the
keyboard to match the connection's chosen grab mode (if the mode is
Always), full-screen mode (if the mode is FS), or grab state (if the
mode is Manual.)

Easy enough, but this led to another problem: if Manual grab mode was
selected, there was no way to determine whether grabbing was enabled
before the connection window lost focus.

Step 3: Decouple the actual grab state from the selected grab state.

The actual grab state is now stored more robustly, in a synchronized
static variable that allows all viewer connection threads to see whether
grabbing is active on any other viewer connection.  The selected grab
state is simply read from the F8 menu object.

All of this could have been much more easily solved by forking a new
JVM for the second and subsequent connections, but that involves some
rather hackish things that are likely to interfere with Java Web Start.

java/com/turbovnc/vncviewer/CConn.java
@@ -939,7 +939,6 @@ public class CConn extends CConnection implements UserPasswdGetter,
   private void recreateViewport() { recreateViewport(false); }
 
   private void recreateViewport(boolean restore) {
-    boolean keyboardTempUngrabbed = false;
     if (VncViewer.embed.getValue())
       return;
     if (viewport != null) {
@@ -950,11 +949,7 @@ public class CConn extends CConnection implements UserPasswdGetter,
       }
       if (viewport.timer != null)
         viewport.timer.stop();
-      if (VncViewer.osGrab() && keyboardGrabbed) {
-        viewport.grabKeyboardHelper(false);
-        if (opts.grabKeyboard == Options.GRAB_MANUAL)
-          keyboardTempUngrabbed = true;
-      }
+      viewport.grabKeyboardHelper(false);
       if (VncViewer.currentMonitorIsPrimary.getValue())
         oldViewportBounds = viewport.getBounds();
       viewport.dispose();
@@ -973,12 +968,9 @@ public class CConn extends CConnection implements UserPasswdGetter,
     if (opts.fullScreen && viewport.lionFSSupported())
       viewport.toggleLionFS();
     desktop.requestFocusInWindow();
-    if (VncViewer.osGrab()) {
-      if (opts.grabKeyboard == Options.GRAB_ALWAYS ||
-          (opts.grabKeyboard == Options.GRAB_MANUAL && keyboardTempUngrabbed) ||
-          (opts.grabKeyboard == Options.GRAB_FS && opts.fullScreen))
-        viewport.grabKeyboardHelper(true);
-    }
+    if (shouldGrab())
+      viewport.grabKeyboardHelper(true);
+    selectGrab(VncViewer.isKeyboardGrabbed(viewport));
     if (VncViewer.osEID())
       viewport.setupExtInputHelper();
   }
@@ -1596,12 +1588,13 @@ public class CConn extends CConnection implements UserPasswdGetter,
 
     if (VncViewer.osGrab() && Viewport.isHelperAvailable()) {
       opts.grabKeyboard = options.grabKeyboard.getSelectedIndex();
+      boolean isGrabbed = VncViewer.isKeyboardGrabbed(viewport);
       if (viewport != null &&
-          ((opts.grabKeyboard == Options.GRAB_ALWAYS &&
-           !viewport.keyboardTempUngrabbed) ||
-          (opts.grabKeyboard == Options.GRAB_FS &&
-           opts.fullScreen != viewport.keyboardTempUngrabbed))) {
-        viewport.keyboardTempUngrabbed = !viewport.keyboardTempUngrabbed;
+          ((opts.grabKeyboard == Options.GRAB_ALWAYS && !isGrabbed) ||
+           (opts.grabKeyboard == Options.GRAB_FS &&
+            opts.fullScreen != isGrabbed))) {
+        viewport.grabKeyboardHelper(!isGrabbed);
+        selectGrab(!isGrabbed);
       }
     }
 
@@ -1688,12 +1681,22 @@ public class CConn extends CConnection implements UserPasswdGetter,
     }
   }
 
-  // EDT
-  public void toggleKeyboardGrab() {
-    if (VncViewer.embed.getValue() || !VncViewer.osGrab())
-      return;
-    if (viewport != null)
-      viewport.grabKeyboardHelper(!keyboardGrabbed);
+  public boolean shouldGrab() {
+    return VncViewer.osGrab() &&
+           (opts.grabKeyboard == Options.GRAB_ALWAYS ||
+            (opts.grabKeyboard == Options.GRAB_MANUAL && isGrabSelected()) ||
+            (opts.grabKeyboard == Options.GRAB_FS && opts.fullScreen));
+  }
+
+  public void selectGrab(boolean on) {
+    if (VncViewer.osGrab())
+      menu.grabKeyboard.setSelected(on);
+  }
+
+  public boolean isGrabSelected() {
+    if (VncViewer.osGrab())
+      return menu.grabKeyboard.isSelected();
+    return false;
   }
 
   // EDT

java/com/turbovnc/vncviewer/DesktopWindow.java
@@ -568,7 +568,10 @@ class DesktopWindow extends JPanel implements Runnable, MouseListener,
           cc.toggleFullScreen();
           return;
         case KeyEvent.VK_G:
-          cc.toggleKeyboardGrab();
+          if (cc.viewport != null) {
+            cc.viewport.grabKeyboardHelper(!cc.isGrabSelected());
+            cc.selectGrab(!cc.isGrabSelected());
+          }
           return;
         case KeyEvent.VK_I:
           cc.showInfo();

java/com/turbovnc/vncviewer/F8Menu.java
@@ -1,6 +1,6 @@
 /* Copyright (C) 2002-2005 RealVNC Ltd.  All Rights Reserved.
  * Copyright (C) 2011, 2013 Brian P. Hinz
- * Copyright (C) 2012-2015 D. R. Commander.  All Rights Reserved.
+ * Copyright (C) 2012-2015, 2017 D. R. Commander.  All Rights Reserved.
  *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -69,7 +69,7 @@ public class F8Menu extends JPopupMenu implements ActionListener {
         Viewport.isHelperAvailable()) {
       grabKeyboard = new JCheckBoxMenuItem("Grab Keyboard   (Ctrl-Alt-Shift-G)");
       grabKeyboard.setMnemonic(KeyEvent.VK_G);
-      grabKeyboard.setSelected(cc.keyboardGrabbed);
+      grabKeyboard.setSelected(VncViewer.isKeyboardGrabbed());
       grabKeyboard.addActionListener(this);
       add(grabKeyboard);
     }
@@ -96,7 +96,7 @@ public class F8Menu extends JPopupMenu implements ActionListener {
     if (VncViewer.osGrab())
       addPopupMenuListener(new PopupMenuListener() {
         public void popupMenuCanceled(PopupMenuEvent e) {
-          if (cc.keyboardGrabbed)
+          if (cc.isGrabSelected())
             cc.viewport.grabKeyboardHelper(true, true);
         }
         public void popupMenuWillBecomeInvisible(PopupMenuEvent e) {}
@@ -144,7 +144,8 @@ public class F8Menu extends JPopupMenu implements ActionListener {
     } else if (actionMatch(ev, clipboard)) {
       cc.clipboardDialog.showDialog(cc.viewport);
     } else if (actionMatch(ev, grabKeyboard)) {
-      cc.toggleKeyboardGrab();
+      if (cc.viewport != null)
+        cc.viewport.grabKeyboardHelper(grabKeyboard.isSelected());
     } else if (actionMatch(ev, f8)) {
       cc.writeKeyEvent(MenuKey.getMenuKeySym(), true);
       cc.writeKeyEvent(MenuKey.getMenuKeySym(), false);

java/com/turbovnc/vncviewer/Viewport.java
@@ -80,21 +80,25 @@ public class Viewport extends JFrame {
     // full-screen state.
     showToolbar(cc.showToolbar, canDoLionFS);
 
+    final Viewport vp = this;
     addWindowFocusListener(new WindowAdapter() {
       public void windowGainedFocus(WindowEvent e) {
         if (sp.getViewport().getView() != null)
           sp.getViewport().getView().requestFocusInWindow();
-        if (isVisible() && keyboardTempUngrabbed) {
-          vlog.info("Keyboard focus regained. Re-grabbing keyboard.");
-          grabKeyboardHelper(true);
-          keyboardTempUngrabbed = false;
+        if (isVisible()) {
+          if (cc.shouldGrab() && !VncViewer.isKeyboardGrabbed(vp))
+            vlog.info("Keyboard focus regained. Re-grabbing keyboard.");
+          else if (!cc.shouldGrab() && VncViewer.isKeyboardGrabbed())
+            vlog.info("Keyboard focus regained. Ungrabbing keyboard.");
+          grabKeyboardHelper(cc.shouldGrab());
+          cc.selectGrab(cc.shouldGrab());
         }
       }
       public void windowLostFocus(WindowEvent e) {
-        if (cc.keyboardGrabbed && isVisible()) {
+        if (VncViewer.isKeyboardGrabbed() && isVisible() &&
+            e.getOppositeWindow() == null) {
           vlog.info("Keyboard focus lost. Temporarily ungrabbing keyboard.");
           grabKeyboardHelper(false);
-          keyboardTempUngrabbed = true;
         }
       }
     });
@@ -396,13 +400,14 @@ public class Viewport extends JFrame {
   }
 
   public void grabKeyboardHelper(boolean on, boolean force) {
-    if (isHelperAvailable()) {
+    if (VncViewer.osGrab() && !VncViewer.embed.getValue() &&
+        isHelperAvailable()) {
       try {
-        if (cc.keyboardGrabbed == on && !force)
+        if (((on && VncViewer.isKeyboardGrabbed(this)) ||
+             (!on && !VncViewer.isKeyboardGrabbed())) && !force)
           return;
         grabKeyboard(on, VncViewer.grabPointer.getValue());
-        cc.keyboardGrabbed = on;
-        cc.menu.grabKeyboard.setSelected(cc.keyboardGrabbed);
+        VncViewer.setGrabOwner(on ? this : null);
       } catch (java.lang.UnsatisfiedLinkError e) {
         vlog.info("WARNING: Could not invoke grabKeyboard() from TurboVNC Helper.");
         vlog.info("  Keyboard grabbing will be disabled.");
@@ -524,7 +529,6 @@ public class Viewport extends JFrame {
   public int dx, dy = 0, adjustWidth, adjustHeight;
   MacMenuBar macMenu;
   boolean canDoLionFS;
-  public boolean keyboardTempUngrabbed;
   static boolean triedHelperInit, helperAvailable;
   Timer timer;
   private long x11dpy, x11win;

java/com/turbovnc/vncviewer/VncViewer.java
@@ -1008,6 +1008,26 @@ public class VncViewer extends javax.swing.JApplet
     setGlobalInsets();
   }
 
+  // Is the keyboard grabbed by any TurboVNC Viewer window?
+  static public boolean isKeyboardGrabbed() {
+    synchronized(VncViewer.class) {
+      return grabOwner != null;
+    }
+  }
+
+  // Is the keyboard grabbed by a specific TurboVNC Viewer window?
+  static public boolean isKeyboardGrabbed(Viewport viewport) {
+    synchronized(VncViewer.class) {
+      return grabOwner == viewport;
+    }
+  }
+
+  static public void setGrabOwner(Viewport viewport) {
+    synchronized(VncViewer.class) {
+      grabOwner = viewport;
+    }
+  }
+
   // CONNECTION PARAMETERS
 
   static HeaderParameter connHeader
@@ -1125,13 +1145,13 @@ public class VncViewer extends javax.swing.JApplet
   osGrab() ? "When the keyboard is grabbed, special key sequences (such as " +
   "Alt-Tab) that are used to switch windows and perform other window " +
   "management functions are passed to the VNC server instead of being " +
-  "handled by the local window manager.  The default is to grab the " +
-  "keyboard when switching to full-screen mode and ungrab it when exiting " +
-  "full-screen mode.  Setting this parameter to \"Always\" grabs the " +
-  "keyboard when the viewer starts up and does not automatically ungrab it. " +
-  "When this parameter is set to \"Manual\", the keyboard is only grabbed " +
-  "or ungrabbed when the \"Grab Keyboard\" option is selected in the F8 " +
-  "menu, or when the Ctrl-Alt-Shift-G hotkey is pressed.  Regardless of the " +
+  "handled by the local window manager.  The default is to automatically " +
+  "grab the keyboard in full-screen mode and to ungrab it in windowed " +
+  "mode.  Setting this parameter to \"Always\" automatically grabs the " +
+  "keyboard in both full-screen mode and windowed mode.  When this " +
+  "parameter is set to \"Manual\", the keyboard is only grabbed or " +
+  "ungrabbed when the \"Grab Keyboard\" option is selected in the F8 menu, " +
+  "or when the Ctrl-Alt-Shift-G hotkey is pressed.  Regardless of the " +
   "grabbing mode, the F8 menu option and hotkey can always be used to " +
   "grab or ungrab the keyboard." : null, "FS", "Always, FS, Manual");
 
@@ -1543,4 +1563,5 @@ public class VncViewer extends javax.swing.JApplet
   Thread listenThread;
   static ArrayList<CConn> conns = new ArrayList<CConn>();
   static Insets insets;
+  static Viewport grabOwner;
 }

commit fe8e9f20fdf9d257c2c7f2161de8f107c6aae65d

Java Win/Linux: Fix NPE when run w/o TVNC Helper
Caused by 296404be205d608eb0fca3c7182987b757524fe2

java/com/turbovnc/vncviewer/CConn.java
@@ -1,6 +1,6 @@
 /* Copyright (C) 2002-2005 RealVNC Ltd.  All Rights Reserved.
  * Copyright 2009-2011 Pierre Ossman <ossman@cendio.se> for Cendio AB
- * Copyright (C) 2011-2017 D. R. Commander.  All Rights Reserved.
+ * Copyright (C) 2011-2018 D. R. Commander.  All Rights Reserved.
  * Copyright (C) 2011-2015 Brian P. Hinz
  *
  * This is free software; you can redistribute it and/or modify
@@ -1689,12 +1689,12 @@ public class CConn extends CConnection implements UserPasswdGetter,
   }
 
   public void selectGrab(boolean on) {
-    if (VncViewer.osGrab())
+    if (menu.grabKeyboard != null)
       menu.grabKeyboard.setSelected(on);
   }
 
   public boolean isGrabSelected() {
-    if (VncViewer.osGrab())
+    if (menu.grabKeyboard != null)
       return menu.grabKeyboard.isSelected();
     return false;
   }