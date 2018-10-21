TurboVNC/turbovnc

commit ab88ece377516835f633d0de6aac7e1c98f11579

Create a separate Options class for storing global and connection-specific options.  This new class is populated from the applet/command-line parameters, then it is duplicated for each connection, so modifying the connection-specific instance of it does not affect other connections.  This solves a couple of long-standing architectural problems with the viewer.  Previously, the parameters were instantiated with VncViewer, but since Configuration is a singleton, its linked list contained references only to the first VncViewer's parameters.  Thus, second and subsequent viewers were not receiving any parameters modified by loading the global defaults, nor were they receiving any parameters overridden on the command line.  Further, the previous change that enabled modal dialogs tried to set the server host/port using Configuration, so this had the effect of causing an exception to be thrown when the user tried to establish a second connection.
The right thing to do here was to make the parameters static and, for the most part, treat them as an immutable global state.  If a parameter is meant to be global, it can still be used to store the state of the corresponding option.  Otherwise, the state of the option is stored and modified in the Options instance.



git-svn-id: svn+ssh://svn.code.sf.net/p/turbovnc/code/trunk@2218 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

java/com/turbovnc/rfb/Options.java
@@ -1,0 +1,155 @@
+/* Copyright (C) 2012 D. R. Commander.  All Rights Reserved.
+ * 
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+ * USA.
+ */
+
+package com.turbovnc.rfb;
+
+public class Options
+{
+  public static final int SCALE_AUTO = -1;
+  public static final int SCALE_FIXEDRATIO = -2;
+
+  public static final int SPAN_PRIMARY = 0;
+  public static final int SPAN_ALL = 1;
+  public static final int SPAN_AUTO = 2;
+
+  public static final int NUMSUBSAMPOPT = 4;
+  public static final int SUBSAMP_NONE = 0;
+  public static final int SUBSAMP_4X = 1;
+  public static final int SUBSAMP_2X = 2;
+  public static final int SUBSAMP_GRAY = 3;
+
+  public final static int DEFQUAL = 95;
+
+  public Options() {}
+
+  public Options(Options old) {
+    if (old.serverName != null) serverName = new String(old.serverName);
+    port = old.port;
+    shared = old.shared;
+    viewOnly = old.viewOnly;
+    fullScreen = old.fullScreen;
+    span = old.span;
+    scalingFactor = old.scalingFactor;
+    acceptClipboard = old.acceptClipboard;
+    sendClipboard = old.sendClipboard;
+    acceptBell = old.acceptBell;
+    preferredEncoding = old.preferredEncoding;
+    allowJpeg = old.allowJpeg;
+    quality = old.quality;
+    subsampling = old.subsampling;
+    compressLevel = old.compressLevel;
+    colors = old.colors;
+    cursorShape = old.cursorShape;
+    if (old.user != null) user = new String(old.user);
+    noUnixLogin = old.noUnixLogin;
+    sendLocalUsername = old.sendLocalUsername;
+    if (old.via != null) via = new String(old.via);
+    tunnel = old.tunnel;
+  }
+
+  public void setScalingFactor(String scaleString) {
+    if (scaleString.toLowerCase().startsWith("a"))
+      scalingFactor = SCALE_AUTO;
+    else if(scaleString.toLowerCase().startsWith("f"))
+      scalingFactor = SCALE_FIXEDRATIO;
+    else {
+      scaleString = scaleString.replaceAll("[^\\d]", "");
+      int sf = -1;
+      try {
+        sf = Integer.parseInt(scaleString);
+      } catch(NumberFormatException e) {};
+      if (sf >= 1 && sf <= 1000) {
+        scalingFactor = sf;
+      }
+    }
+  }
+
+  public int getSubsamplingOrdinal() {
+    switch (subsampling) {
+      case SUBSAMP_2X:
+        return 1;
+      case SUBSAMP_4X:
+        return 2;
+      case SUBSAMP_GRAY:
+        return 3;
+    }
+    return 0;
+  }
+
+  void printOpt(String name, boolean val) {
+    System.out.println(name + " = " + (val ? "true":"false"));
+  }
+
+  void printOpt(String name, int val) {
+    System.out.println(name + " = " + val);
+  }
+
+  void printOpt(String name, String val) {
+    System.out.println(name + " = " + val);
+  }
+
+  public void print() {
+    printOpt("serverName", serverName);
+    printOpt("port", port);
+    printOpt("shared", shared);
+    printOpt("viewOnly", viewOnly);
+    printOpt("fullScreen", fullScreen);
+    printOpt("span", span);
+    printOpt("scalingFactor", scalingFactor);
+    printOpt("acceptClipboard", acceptClipboard);
+    printOpt("sendClipboard", sendClipboard);
+    printOpt("acceptBell", acceptBell);
+    printOpt("preferredEncoding", preferredEncoding);
+    printOpt("copyRect", copyRect);
+    printOpt("allowJpeg", allowJpeg);
+    printOpt("quality", quality);
+    printOpt("subsampling", subsampling);
+    printOpt("compressLevel", compressLevel);
+    printOpt("colors", colors);
+    printOpt("cursorShape", cursorShape);
+    printOpt("user", user);
+    printOpt("noUnixLogin", noUnixLogin);
+    printOpt("sendLocalUsername", sendLocalUsername);
+    printOpt("via", via);
+    printOpt("tunnel", tunnel);
+  }
+
+  public String serverName;
+  public int port;
+  public boolean shared;
+  public boolean viewOnly;
+  public boolean fullScreen;
+  public int span;
+  public int scalingFactor;
+  public boolean acceptClipboard;
+  public boolean sendClipboard;
+  public boolean acceptBell;
+  public int preferredEncoding;
+  public boolean copyRect = true;
+  public boolean allowJpeg;
+  public int quality;
+  public int subsampling;
+  public int compressLevel;
+  public int colors;
+  public boolean cursorShape;
+  public String user;
+  public boolean noUnixLogin;
+  public boolean sendLocalUsername;
+  public String via;
+  public boolean tunnel;
+}

java/com/turbovnc/rfb/CMsgWriter.java
@@ -48,11 +48,12 @@ abstract public class CMsgWriter {
   // Ask for encodings based on which decoders are supported.  Assumes higher
   // encoding numbers are more desirable.
 
-  synchronized public void writeSetEncodings(int preferredEncoding, boolean useCopyRect) 
+  synchronized public void writeSetEncodings(int preferredEncoding,
+                                             Options opts)
   {
     int nEncodings = 0;
     int[] encodings = new int[Encodings.encodingMax+3];
-    if (cp.supportsLocalCursor)
+    if (opts.cursorShape)
       encodings[nEncodings++] = Encodings.pseudoEncodingCursor;
     if (cp.supportsDesktopResize)
       encodings[nEncodings++] = Encodings.pseudoEncodingDesktopSize;
@@ -72,7 +73,7 @@ abstract public class CMsgWriter {
       encodings[nEncodings++] = preferredEncoding;
     }
 
-    if (useCopyRect) {
+    if (opts.copyRect) {
       encodings[nEncodings++] = Encodings.encodingCopyRect;
     }
 
@@ -108,24 +109,18 @@ abstract public class CMsgWriter {
     }
 
     encodings[nEncodings++] = Encodings.pseudoEncodingLastRect;
-    if (cp.compressLevel >= 0 && cp.compressLevel <= 9)
+    if (opts.compressLevel >= 0 && opts.compressLevel <= 9)
       encodings[nEncodings++] = Encodings.pseudoEncodingCompressLevel0
-        + cp.compressLevel;
-    if (cp.allowJpeg && preferredEncoding == Encodings.encodingTight) {
-      int quality = cp.quality;
-      if (quality < 1 || quality > 100)
-        quality = ConnParams.DEFQUAL;
-      int qualityLevel = quality / 10;
+        + opts.compressLevel;
+    if (opts.allowJpeg && opts.preferredEncoding == Encodings.encodingTight) {
+      int qualityLevel = opts.quality / 10;
       if (qualityLevel > 9) qualityLevel = 9;
       encodings[nEncodings++] = Encodings.pseudoEncodingQualityLevel0
         + qualityLevel;
       encodings[nEncodings++] = Encodings.pseudoEncodingFineQualityLevel0
-        + quality;
-      int subsampling = cp.subsampling;
-      if (subsampling < 0 || subsampling >= ConnParams.NUMSUBSAMPOPT)
-        subsampling = ConnParams.DEFSUBSAMP;
+        + opts.quality;
       encodings[nEncodings++] = Encodings.pseudoEncodingSubsamp1X
-        + subsampling;
+        + opts.subsampling;
     }
 
     writeSetEncodings(nEncodings, encodings);
@@ -188,9 +183,13 @@ abstract public class CMsgWriter {
   ConnParams getConnParams() { return cp; }
   OutStream getOutStream() { return os; }
 
-  protected CMsgWriter(ConnParams cp_, OutStream os_) {cp = cp_; os = os_;}
+  protected CMsgWriter(ConnParams cp_, OutStream os_) {
+    cp = cp_;  os = os_;
+  }
 
   ConnParams cp;
+  Options opts;
   OutStream os;
+
   static LogWriter vlog = new LogWriter("CMsgWriter");
 }

java/com/turbovnc/rfb/ConnParams.java
@@ -24,29 +24,18 @@ import com.turbovnc.rdr.*;
 
 public class ConnParams {
 
-  public final static int NUMSUBSAMPOPT = 4;
-  public final static int SUBSAMP_NONE  = 0;
-  public final static int SUBSAMP_420   = 1;
-  public final static int SUBSAMP_422   = 2;
-  public final static int SUBSAMP_GRAY  = 3;
-
-  public final static int DEFQUAL       = 95;
-  public final static int DEFSUBSAMP    = SUBSAMP_NONE;
-
   static LogWriter vlog = new LogWriter("ConnParams");
 
   public ConnParams() {
     majorVersion = 0; minorVersion = 0;
-    width = 0; height = 0; useCopyRect = false;
-    supportsLocalCursor = false; supportsLocalXCursor = false;
-    supportsDesktopResize = false; supportsExtendedDesktopSize = false;
-    supportsDesktopRename = false; supportsLastRect = false;
-    supportsSetDesktopSize = false; supportsFence = false;
+    width = 0;  height = 0;
+    supportsDesktopResize = false;  supportsExtendedDesktopSize = false;
+    supportsDesktopRename = false;  supportsLastRect = false;
+    supportsSetDesktopSize = false;  supportsFence = false;
     supportsContinuousUpdates = false;
     supportsClientRedirect = false;
-    compressLevel = 1;  allowJpeg = true;  quality = -1;  subsampling = -1;
-    name_ = null; nEncodings_ = 0; encodings_ = null;
-    currentEncoding_ = Encodings.encodingRaw; verStrPos = 0;
+    name_ = null;  nEncodings_ = 0;  encodings_ = null;
+    verStrPos = 0;
     screenLayout = new ScreenSet();
 
     setName("");
@@ -117,27 +106,11 @@ public class ConnParams {
     name_ = name;
   }
 
-  public int getSubsamplingOrdinal() {
-    switch (subsampling) {
-      case ConnParams.SUBSAMP_422:
-        return 1;
-      case ConnParams.SUBSAMP_420:
-        return 2;
-      case ConnParams.SUBSAMP_GRAY:
-        return 3;
-    }
-    return 0;
-  }
-
-  public int currentEncoding() { return currentEncoding_; }
   public int nEncodings() { return nEncodings_; }
   public int[] encodings() { return encodings_; }
 
   public boolean done;
-  public boolean useCopyRect;
 
-  public boolean supportsLocalCursor;
-  public boolean supportsLocalXCursor;
   public boolean supportsDesktopResize;
   public boolean supportsExtendedDesktopSize;
   public boolean supportsDesktopRename;
@@ -148,16 +121,10 @@ public class ConnParams {
 
   public boolean supportsSetDesktopSize;
 
-  public int compressLevel;
-  public boolean allowJpeg;
-  public int quality;
-  public int subsampling;
-
   private PixelFormat pf_;
   private String name_;
   private int nEncodings_;
   private int[] encodings_;
-  private int currentEncoding_;
   private StringBuilder verStr;
   private int verStrPos;
 }

java/com/turbovnc/rfb/IntParameter.java
@@ -22,7 +22,10 @@ package com.turbovnc.rfb;
 public class IntParameter extends VoidParameter {
 
   public IntParameter(String name_, String desc_, int v) {
-    this(name_, desc_, v, 0, -1);
+    super(name_, desc_);
+    value = v;
+    defValue = v;
+    useMinMax = false;
   }
 
   public IntParameter(String name_, String desc_, int v, int minValue_,
@@ -32,14 +35,23 @@ public class IntParameter extends VoidParameter {
     defValue = v;
     maxValue = maxValue_;
     minValue = minValue_;
+    useMinMax = true;
   }
 
   public boolean setParam(String v) {
+    int i;
     try {
-      value = Integer.parseInt(v);
+      i = Integer.parseInt(v);
     } catch (NumberFormatException e) {
       return false;
     }
+    return setValue(i);
+  }
+
+  public boolean setValue(int v) {
+    if (useMinMax && (v < minValue || v > maxValue))
+      return false;
+    value = v;
     return true;
   }
 
@@ -50,7 +62,7 @@ public class IntParameter extends VoidParameter {
   }
   public String getValueStr() { return Integer.toString(value); }
   public String getValues() {
-    if (maxValue >= minValue) {
+    if (useMinMax) {
       return minValue + "-" + maxValue;
     }
     return null;
@@ -62,4 +74,5 @@ public class IntParameter extends VoidParameter {
   protected int defValue;
   protected int minValue;
   protected int maxValue;
+  boolean useMinMax;
 }

commit 29efef4b3f1ec68081b3fa5f5313976ea5a5b433

Java: Fix undocumented CopyRect parameter
Broken by ab88ece377516835f633d0de6aac7e1c98f11579

java/com/turbovnc/rfb/Options.java
@@ -67,6 +67,7 @@ public class Options {
     sendClipboard = old.sendClipboard;
     acceptBell = old.acceptBell;
     preferredEncoding = old.preferredEncoding;
+    copyRect = old.copyRect;
     allowJpeg = old.allowJpeg;
     quality = old.quality;
     subsampling = old.subsampling;