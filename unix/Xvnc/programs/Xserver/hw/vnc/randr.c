TurboVNC/turbovnc

commit 5cb50b8f8732d0d79689e207fd67bc9692c38e69

Server: Don't send EDS message until FBUR received
This new behavior is inspired by the TigerVNC Server code.  Our previous
behavior was problematic, because our server (as is the case with
similar VNC servers) sometimes tries to send a framebuffer update (FBU)
before the SetEncodings message has been received.  Normally, these
premature FBUs will not result in any data being sent to the viewer,
because the server has not yet received a framebuffer update request
(FBUR) from the viewer, and continuous updates (CU) has not yet been
enabled via the SetEncodings message.  However, the TurboVNC Server
erroneously attempted to send an ExtendedDesktopSize (EDS) message as
part of the first FBU.  If that FBU was premature, then sending the EDS
message failed, because cl->enableDesktopSize was FALSE.  However,
cl->pendingDesktopSize was reset regardless.  Thus, when a FBU
containing pixel data was later sent, it arrived at the viewer without a
prior EDS message, and thus the viewer assumed that the server didn't
support remote desktop resizing.

unix/Xvnc/programs/Xserver/hw/vnc/rfbserver.c
@@ -435,7 +435,6 @@ static rfbClientPtr rfbNewClient(int sock)
   /* The TigerVNC Viewer won't enable remote desktop resize until it receives
      a desktop resize message from the server, so we give it one with the
      first FBU. */
-  cl->pendingDesktopResize = TRUE;
   cl->reason = rfbEDSReasonServer;
   cl->result = rfbEDSResultSuccess;
 
@@ -1184,6 +1183,7 @@ static void rfbProcessClientNormalMessage(rfbClientPtr cl)
                      &tmpRegion);
         REGION_SUBTRACT(pScreen, &cl->copyRegion, &cl->copyRegion, &tmpRegion);
         REGION_UNION(pScreen, &cl->ifRegion, &cl->ifRegion, &tmpRegion);
+        cl->pendingDesktopResize = TRUE;
       }
 
       if (FB_UPDATE_PENDING(cl) &&

commit 944b77935ecf6183019175b4acdc42053ca3c718

Server: Fix flickering w/ UltraVNC Viewer
Regression caused by 5cb50b8f8732d0d79689e207fd67bc9692c38e69

The UltraVNC Viewer seems to not send incremental FBURs, so we need to
separate the pending resize logic for "normal" and extended desktop size
messages and trigger a pending resize for the "normal" flavor only in
response to a server-side desktop resize.  This mimics the behavior of
the TigerVNC Server.

Fixes #165

unix/Xvnc/programs/Xserver/hw/vnc/randr.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C)2013-2018 D. R. Commander.  All Rights Reserved.
+ *  Copyright (C)2013-2019 D. R. Commander.  All Rights Reserved.
  *  Copyright 2012-2015 Pierre Ossman for Cendio AB
  *
  *  This is free software; you can redistribute it and/or modify
@@ -568,7 +568,7 @@ static Bool rfbSendDesktopSizeAll(rfbClientPtr reqClient, int reason)
 
   for (cl = rfbClientHead; cl; cl = nextCl) {
     nextCl = cl->next;
-    cl->pendingDesktopResize = TRUE;
+    cl->pendingDesktopResize = cl->pendingExtDesktopResize = TRUE;
     if (cl != reqClient && reason == rfbEDSReasonClient)
       cl->reason = rfbEDSReasonOtherClient;
     else

unix/Xvnc/programs/Xserver/hw/vnc/rfb.h
@@ -3,7 +3,7 @@
  */
 
 /*
- *  Copyright (C) 2010-2018 D. R. Commander.  All Rights Reserved.
+ *  Copyright (C) 2010-2019 D. R. Commander.  All Rights Reserved.
  *  Copyright (C) 2010 University Corporation for Atmospheric Research.
  *                     All Rights Reserved.
  *  Copyright (C) 2000-2004 Const Kaplinsky.  All Rights Reserved.
@@ -427,7 +427,7 @@ typedef struct rfbClientRec {
   Bool congestionTimerRunning;
   struct timeval lastWrite;
 
-  Bool pendingDesktopResize;
+  Bool pendingDesktopResize, pendingExtDesktopResize;
   int reason, result;
 
 #if USETLS

unix/Xvnc/programs/Xserver/hw/vnc/rfbserver.c
@@ -3,7 +3,7 @@
  */
 
 /*
- *  Copyright (C) 2009-2018 D. R. Commander.  All Rights Reserved.
+ *  Copyright (C) 2009-2019 D. R. Commander.  All Rights Reserved.
  *  Copyright (C) 2010 University Corporation for Atmospheric Research.
  *                     All Rights Reserved.
  *  Copyright (C) 2005-2008 Sun Microsystems, Inc.  All Rights Reserved.
@@ -78,6 +78,7 @@ static void rfbProcessClientNormalMessage(rfbClientPtr cl);
 static Bool rfbSendCopyRegion(rfbClientPtr cl, RegionPtr reg, int dx, int dy);
 static Bool rfbSendLastRectMarker(rfbClientPtr cl);
 Bool rfbSendDesktopSize(rfbClientPtr cl);
+Bool rfbSendExtDesktopSize(rfbClientPtr cl);
 
 
 /*
@@ -1181,7 +1182,7 @@ static void rfbProcessClientNormalMessage(rfbClientPtr cl)
                      &tmpRegion);
         REGION_SUBTRACT(pScreen, &cl->copyRegion, &cl->copyRegion, &tmpRegion);
         REGION_UNION(pScreen, &cl->ifRegion, &cl->ifRegion, &tmpRegion);
-        cl->pendingDesktopResize = TRUE;
+        cl->pendingExtDesktopResize = TRUE;
       }
 
       if (FB_UPDATE_PENDING(cl) &&
@@ -1401,7 +1402,7 @@ static void rfbProcessClientNormalMessage(rfbClientPtr cl)
          an error in ResizeDesktop(). */
       for (cl2 = rfbClientHead; cl2; cl2 = cl2->next) {
         if (cl2 == cl) {
-          cl2->pendingDesktopResize = TRUE;
+          cl2->pendingExtDesktopResize = TRUE;
           cl2->reason = rfbEDSReasonClient;
           cl2->result = result;
           rfbSendFramebufferUpdate(cl2);
@@ -1804,6 +1805,11 @@ Bool rfbSendFramebufferUpdate(rfbClientPtr cl)
      congestion window. */
 
   rfbCorkSock(cl->sock);
+
+  if (cl->pendingExtDesktopResize) {
+    if (!rfbSendExtDesktopSize(cl)) return FALSE;
+    cl->pendingExtDesktopResize = FALSE;
+  }
 
   if (cl->pendingDesktopResize) {
     if (!rfbSendDesktopSize(cl)) return FALSE;
@@ -2633,10 +2639,7 @@ Bool rfbSendDesktopSize(rfbClientPtr cl)
   rfbFramebufferUpdateRectHeader rh;
   rfbFramebufferUpdateMsg fu;
 
-  if (!cl->enableDesktopSize && !cl->enableExtDesktopSize)
-    return TRUE;
-  /* Error messages can only be sent with the EDS extension */
-  if (!cl->enableExtDesktopSize && cl->result != rfbEDSResultSuccess)
+  if (!cl->enableDesktopSize)
     return TRUE;
 
   memset(&fu, 0, sz_rfbFramebufferUpdateMsg);
@@ -2648,17 +2651,8 @@ Bool rfbSendDesktopSize(rfbClientPtr cl)
     return FALSE;
   }
 
-  if (cl->enableExtDesktopSize) {
-    /* Send the ExtendedDesktopSize message, if the client supports it.
-       The TigerVNC Viewer, in particular, requires this, or it won't
-       enable remote desktop resize. */
-    rh.encoding = Swap32IfLE(rfbEncodingExtendedDesktopSize);
-    rh.r.x = Swap16IfLE(cl->reason);
-    rh.r.y = Swap16IfLE(cl->result);
-  } else {
-    rh.encoding = Swap32IfLE(rfbEncodingNewFBSize);
-    rh.r.x = rh.r.y = 0;
-  }
+  rh.encoding = Swap32IfLE(rfbEncodingNewFBSize);
+  rh.r.x = rh.r.y = 0;
   rh.r.w = Swap16IfLE(rfbFB.width);
   rh.r.h = Swap16IfLE(rfbFB.height);
   if (WriteExact(cl, (char *)&rh, sz_rfbFramebufferUpdateRectHeader) < 0) {
@@ -2667,55 +2661,96 @@ Bool rfbSendDesktopSize(rfbClientPtr cl)
     return FALSE;
   }
 
-  if (cl->enableExtDesktopSize) {
-    CARD8 numScreens[4] = { 0, 0, 0, 0 };
-    rfbScreenInfo *iter;
-    BOOL fakeScreen = FALSE;
+  return TRUE;
+}
 
-    xorg_list_for_each_entry(iter, &rfbScreens, entry) {
-      if (iter->output->crtc && iter->output->crtc->mode)
-        numScreens[0]++;
-    }
-    if (numScreens[0] < 1) {
-      numScreens[0] = 1;
-      fakeScreen = TRUE;
-    }
 
-    if (WriteExact(cl, (char *)numScreens, 4) < 0) {
-      rfbLogPerror("rfbSendDesktopSize: write");
+/*
+ * rfbSendExtDesktopSize sends an extended desktop size message to a specific
+ * client.
+ */
+
+Bool rfbSendExtDesktopSize(rfbClientPtr cl)
+{
+  rfbFramebufferUpdateRectHeader rh;
+  rfbFramebufferUpdateMsg fu;
+
+  if (!cl->enableExtDesktopSize)
+    return TRUE;
+  /* Error messages can only be sent with the EDS extension */
+  if (!cl->enableExtDesktopSize && cl->result != rfbEDSResultSuccess)
+    return TRUE;
+
+  memset(&fu, 0, sz_rfbFramebufferUpdateMsg);
+  fu.type = rfbFramebufferUpdate;
+  fu.nRects = Swap16IfLE(1);
+  if (WriteExact(cl, (char *)&fu, sz_rfbFramebufferUpdateMsg) < 0) {
+    rfbLogPerror("rfbSendExtDesktopSize: write");
+    rfbCloseClient(cl);
+    return FALSE;
+  }
+
+  /* Send the ExtendedDesktopSize message, if the client supports it.
+     The TigerVNC Viewer, in particular, requires this, or it won't
+     enable remote desktop resize. */
+  rh.encoding = Swap32IfLE(rfbEncodingExtendedDesktopSize);
+  rh.r.x = Swap16IfLE(cl->reason);
+  rh.r.y = Swap16IfLE(cl->result);
+  rh.r.w = Swap16IfLE(rfbFB.width);
+  rh.r.h = Swap16IfLE(rfbFB.height);
+  if (WriteExact(cl, (char *)&rh, sz_rfbFramebufferUpdateRectHeader) < 0) {
+    rfbLogPerror("rfbSendExtDesktopSize: write");
+    rfbCloseClient(cl);
+    return FALSE;
+  }
+
+  CARD8 numScreens[4] = { 0, 0, 0, 0 };
+  rfbScreenInfo *iter;
+  BOOL fakeScreen = FALSE;
+
+  xorg_list_for_each_entry(iter, &rfbScreens, entry) {
+    if (iter->output->crtc && iter->output->crtc->mode)
+      numScreens[0]++;
+  }
+  if (numScreens[0] < 1) {
+    numScreens[0] = 1;
+    fakeScreen = TRUE;
+  }
+
+  if (WriteExact(cl, (char *)numScreens, 4) < 0) {
+    rfbLogPerror("rfbSendExtDesktopSize: write");
+    rfbCloseClient(cl);
+    return FALSE;
+  }
+
+  if (fakeScreen) {
+    rfbScreenInfo screen = *xorg_list_first_entry(&rfbScreens, rfbScreenInfo,
+                                                  entry);
+    screen.s.id = Swap32IfLE(screen.s.id);
+    screen.s.x = screen.s.y = 0;
+    screen.s.w = Swap16IfLE(rfbFB.width);
+    screen.s.h = Swap16IfLE(rfbFB.height);
+    screen.s.flags = Swap32IfLE(screen.s.flags);
+    if (WriteExact(cl, (char *)&screen.s, sz_rfbScreenDesc) < 0) {
+      rfbLogPerror("rfbSendExtDesktopSize: write");
       rfbCloseClient(cl);
       return FALSE;
     }
+  } else {
+    xorg_list_for_each_entry(iter, &rfbScreens, entry) {
+      rfbScreenInfo screen = *iter;
 
-    if (fakeScreen) {
-      rfbScreenInfo screen = *xorg_list_first_entry(&rfbScreens, rfbScreenInfo,
-                                                    entry);
-      screen.s.id = Swap32IfLE(screen.s.id);
-      screen.s.x = screen.s.y = 0;
-      screen.s.w = Swap16IfLE(rfbFB.width);
-      screen.s.h = Swap16IfLE(rfbFB.height);
-      screen.s.flags = Swap32IfLE(screen.s.flags);
-      if (WriteExact(cl, (char *)&screen.s, sz_rfbScreenDesc) < 0) {
-        rfbLogPerror("rfbSendDesktopSize: write");
-        rfbCloseClient(cl);
-        return FALSE;
-      }
-    } else {
-      xorg_list_for_each_entry(iter, &rfbScreens, entry) {
-        rfbScreenInfo screen = *iter;
-
-        if (screen.output->crtc && screen.output->crtc->mode) {
-          screen.s.id = Swap32IfLE(screen.s.id);
-          screen.s.x = Swap16IfLE(screen.s.x);
-          screen.s.y = Swap16IfLE(screen.s.y);
-          screen.s.w = Swap16IfLE(screen.s.w);
-          screen.s.h = Swap16IfLE(screen.s.h);
-          screen.s.flags = Swap32IfLE(screen.s.flags);
-          if (WriteExact(cl, (char *)&screen.s, sz_rfbScreenDesc) < 0) {
-            rfbLogPerror("rfbSendDesktopSize: write");
-            rfbCloseClient(cl);
-            return FALSE;
-          }
+      if (screen.output->crtc && screen.output->crtc->mode) {
+        screen.s.id = Swap32IfLE(screen.s.id);
+        screen.s.x = Swap16IfLE(screen.s.x);
+        screen.s.y = Swap16IfLE(screen.s.y);
+        screen.s.w = Swap16IfLE(screen.s.w);
+        screen.s.h = Swap16IfLE(screen.s.h);
+        screen.s.flags = Swap32IfLE(screen.s.flags);
+        if (WriteExact(cl, (char *)&screen.s, sz_rfbScreenDesc) < 0) {
+          rfbLogPerror("rfbSendExtDesktopSize: write");
+          rfbCloseClient(cl);
+          return FALSE;
         }
       }
     }