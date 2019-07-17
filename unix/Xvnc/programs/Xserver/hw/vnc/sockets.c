TurboVNC/turbovnc

commit a726b947d40e6994744e40051f3e7a28bdbeaa7e

Server: Guard against DoS attack ...
... caused by many RFB connections being made to a TurboVNC Server
session and never dropped.  This exposed two vulnerabilities:

1) If enough RFB connections were made to the session that its allotment
of file descriptors (usually 1024 on Linux) was exhausted, then the
accept() call in rfbSockNotify() would fail with errno == EMFILE, but
since the connection failed, rfbSockNotify() would continue to be called
ad infinitum.  Any existing RFB connections and running X11 applications
continued to function, but since Xvnc could not open any new file
descriptors, no new X11 applications could be launched, and no new X11
connections could be made from existing applications.  Furthermore, the
infinite loop caused the CPU usage for Xvnc to go to 100%, and the
continuous flood of "Too many open files" error messages in the session
log grew the log file by multiple megabytes/second until disk space was
exhausted.

It is possible for the server to work around this by temporarily
disabling the RFB listener if accept() fails, but this just kicks the
can down the road and exposes a very similar infinite loop in
EstablishNewConnections(), which is part of the X.Org code and thus out
of the purview of TurboVNC.  The only reasonable solution was to
introduce an RFB connection limit, which is set to 100 by default and
which cannot be set to a higher number than 500.  This should provide
plenty of headroom to avoid the aforementioned infinite loop.  Note that
there is already a similar limit on X11 connections, which seems to
have an upper bound of 512-- probably also to provide enough headroom to
avoid file descriptor exhaustion.

2) Since the TurboVNC Server printed a list of all active RFB clients
when it received a new inbound RFB connection, this list could grow
quite long, approaching tens of kilobytes in length, before the server
stopped accepting connections due to file descriptor exhaustion.  Thus,

unix/Xvnc/programs/Xserver/hw/vnc/sockets.c
@@ -19,7 +19,7 @@
  */
 
 /*
- *  Copyright (C) 2012-2018 D. R. Commander.  All Rights Reserved.
+ *  Copyright (C) 2012-2019 D. R. Commander.  All Rights Reserved.
  *  Copyright (C) 2011 Gernot Tenchio
  *  Copyright (C) 1999 AT&T Laboratories Cambridge.  All Rights Reserved.
  *
@@ -72,6 +72,7 @@ int rfbMaxClientWait = 20000;   /* time (ms) after which we decide client has
 
 int rfbPort = 0;
 int rfbListenSock = -1;
+int rfbMaxClientConnections = DEFAULT_MAX_CONNECTIONS;
 
 int udpPort = 0;
 int udpSock = -1;
@@ -170,7 +171,7 @@ static void rfbSockNotify(int fd, int ready, void *data)
   char addrStr[INET6_ADDRSTRLEN];
   char buf[6];
   const int one = 1;
-  int sock;
+  int sock, numClientConnections = 0;
   rfbClientPtr cl, nextCl;
 
   if (rfbListenSock != -1 && fd == rfbListenSock) {
@@ -205,6 +206,32 @@ static void rfbSockNotify(int fd, int ready, void *data)
       return;
     }
 #endif
+
+    for (cl = rfbClientHead; cl; cl = cl->next)
+      numClientConnections++;
+    if (numClientConnections >= rfbMaxClientConnections) {
+      rfbClientRec tempCl;
+      rfbProtocolVersionMsg pv;
+      const char *errMsg = "Connection limit reached";
+      CARD32 secType = rfbSecTypeInvalid;
+      CARD32 errMsgLen = strlen(errMsg), errMsgLenWire = Swap32IfLE(errMsgLen);
+
+      memset(&tempCl, 0, sizeof(rfbClientRec));
+      tempCl.sock = sock;
+      getpeername(sock, &addr.u.sa, &addrlen);
+      tempCl.host = strdup(sockaddr_string(&addr, addrStr, INET6_ADDRSTRLEN));
+      rfbLog("Limit of %d connections reached-- rejecting %s\n",
+             rfbMaxClientConnections, tempCl.host);
+
+      sprintf(pv, rfbProtocolVersionFormat, 3, 3);
+      if (WriteExact(&tempCl, pv, sz_rfbProtocolVersionMsg) >= 0 &&
+          WriteExact(&tempCl, (char *)&secType, sizeof(CARD32)) >= 0 &&
+          WriteExact(&tempCl, (char *)&errMsgLenWire, sizeof(CARD32)) >= 0 &&
+          WriteExact(&tempCl, (char *)errMsg, errMsgLen) >= 0) { }
+
+      close(sock);
+      return;
+    }
 
     rfbLog("Got connection from client %s\n",
            sockaddr_string(&addr, addrStr, INET6_ADDRSTRLEN));

unix/Xvnc/programs/Xserver/Xvnc.man.in
@@ -6,14 +6,14 @@
 .\" Copyright (C) 2000, 2001 Red Hat, Inc.
 .\" Copyright (C) 2001, 2002 Constantin Kaplinsky
 .\" Copyright (C) 2005-2008 Sun Microsystems, Inc.
-.\" Copyright (C) 2010, 2012, 2014-2018 D. R. Commander
+.\" Copyright (C) 2010, 2012, 2014-2019 D. R. Commander
 .\" Copyright (C) 2010 University Corporation for Atmospheric Research
 .\"
 .\" You may distribute under the terms of the GNU General Public
 .\" License as specified in the file LICENCE.TXT that comes with the
 .\" TightVNC distribution.
 .\"
-.TH Xvnc 1 "November 2018" "" "TurboVNC"
+.TH Xvnc 1 "April 2019" "" "TurboVNC"
 .SH NAME
 Xvnc \- the TurboVNC X server
 .SH SYNOPSIS
@@ -30,6 +30,7 @@ Xvnc \- the TurboVNC X server
 [\-securitytypes\ \%\fItype-list\fR] [\-x509cert\ \fIcert\fR]
 [\-x509key\ \fIkey\fR] [\-pamsession] [\-noreverse] [\-noclipboardsend]
 [\-noclipboardrecv] [\-maxclipboard\ \fIbytes\fR]
+[\-maxconnections \fIconnection-count\fR]
 [\-idletimeout\ \fItime\fR] [\-httpd\ \fIdir\fR]
 [\-httpport\ \fIport\fR] [\-deferupdate\ \fItime\fR] [\-noflowcontrol]
 [\-alr\ \fItime\fR]
@@ -159,6 +160,10 @@ CONFIGURATION FILE section for more details.
 .TP
 \fB-maxclipboard\fR \fIbytes\fR
 Set the maximum clipboard transfer size to \fIbytes\fR.
+.TP
+\fB-maxconnections\fR \fIconnection-count\fR
+Allow no more than \fIconnection-count\fR simultaneous VNC viewer connections,
+where 1 <= \fIconnection-count\fR <= 500 (default: 100.)
 .TP
 \fB\-idletimeout\fR \fItime\fR
 Time, in seconds, which the TurboVNC session can sit idle (with no VNC viewer

unix/Xvnc/programs/Xserver/hw/vnc/init.c
@@ -317,6 +317,15 @@ int ddxProcessArgument(int argc, char *argv[], int i)
     return 2;
   }
 
+  if (strcasecmp(argv[i], "-maxconnections") == 0) {
+    if (i + 1 >= argc) UseMsg();
+    rfbMaxClientConnections = atoi(argv[i + 1]);
+    if (rfbMaxClientConnections < 1 ||
+        rfbMaxClientConnections > MAX_MAX_CONNECTIONS)
+      UseMsg();
+    return 2;
+  }
+
   if (strcasecmp(argv[i], "-nevershared") == 0) {
     rfbNeverShared = TRUE;
     return 1;
@@ -1593,6 +1602,10 @@ void ddxUseMsg(void)
   ErrorF("-localhost             only allow connections from localhost\n");
   ErrorF("-maxclipboard B        set max. clipboard transfer size to B bytes\n");
   ErrorF("                       (default: %d)\n", rfbMaxClipboard);
+  ErrorF("-maxconnections N      allow no more than N (1 <= N <= %d) simultaneous VNC\n",
+         MAX_MAX_CONNECTIONS);
+  ErrorF("                       viewer connections (default: %d)\n",
+         DEFAULT_MAX_CONNECTIONS);
   ErrorF("-nevershared           never treat new clients as shared\n");
   ErrorF("-noclipboardrecv       disable client->server clipboard synchronization\n");
   ErrorF("-noclipboardsend       disable server->client clipboard synchronization\n");

unix/Xvnc/programs/Xserver/hw/vnc/rfb.h
@@ -81,6 +81,13 @@
    the CPU count */
 #define MAX_ENCODING_THREADS 8
 
+/* Maximum number of client connections.  The default of 100 should be more
+   than enough for most use cases.  The ceiling is set to 500 to give us plenty
+   of room to avoid exceeding the Xvnc process's allotment of file descriptors,
+   which is usually 1024 on Linux systems. */
+#define DEFAULT_MAX_CONNECTIONS 100
+#define MAX_MAX_CONNECTIONS 500
+
 
 /*
  * Per-screen (framebuffer) structure.  There is only one of these, since we
@@ -896,6 +903,7 @@ extern Bool rfbSendRectEncodingRRE(rfbClientPtr cl, int x, int y, int w,
 
 /* sockets.c */
 
+extern int rfbMaxClientConnections;
 extern int rfbMaxClientWait;
 
 extern int udpPort;

unix/Xvnc/programs/Xserver/hw/vnc/rfbserver.c
@@ -345,16 +345,9 @@ static rfbClientPtr rfbNewClient(int sock)
   char *env = NULL;
   int np = sysconf(_SC_NPROCESSORS_CONF);
 
-  if (rfbClientHead == NULL) {
+  if (rfbClientHead == NULL)
     /* no other clients - make sure we don't think any keys are pressed */
     KbdReleaseAllKeys();
-  } else {
-    rfbLog("  (other clients");
-    for (cl = rfbClientHead; cl; cl = cl->next) {
-      fprintf(stderr, " %s", cl->host);
-    }
-    fprintf(stderr, ")\n");
-  }
 
   cl = (rfbClientPtr)rfbAlloc0(sizeof(rfbClientRec));
 

commit ca4baed79606fb7de9bc0753845b747b1dd7ed60

Server: Fix very minor memory leak
(introduced by a726b947d40e6994744e40051f3e7a28bdbeaa7e)

unix/Xvnc/programs/Xserver/hw/vnc/sockets.c
@@ -229,6 +229,7 @@ static void rfbSockNotify(int fd, int ready, void *data)
           WriteExact(&tempCl, (char *)&errMsgLenWire, sizeof(CARD32)) >= 0 &&
           WriteExact(&tempCl, (char *)errMsg, errMsgLen) >= 0) { }
 
+      free(tempCl.host);
       close(sock);
       return;
     }