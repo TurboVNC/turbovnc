15a4d40e7cef636cc3c711e2aded91a82d54a969
Arthur Williams <taaparthur@gmail.com>
2019-10-06 18:55:35 +0000

    dix: Check for NULL spriteInfo in GetPairedDevice
    There is a race when reseting the XServer that causes spriteInfo to be
    NULL in GetPairedDevice resulting a segfault and subsequent crash. The
    problem was noticed when opening a connection, creating master devices,
    destroying master devices and closing the connection during testing.
    
    Signed-off-by: Arthur Williams <taaparthur@gmail.com>
    
    (cherry picked from commit e693c9657f98c334e9921ca2f8ebf710497c0c6a)

--- unix/Xvnc/programs/Xserver/dix/devices.c
+++ unix/Xvnc/programs/Xserver/dix/devices.c
@@ -2665,7 +2665,7 @@ GetPairedDevice(DeviceIntPtr dev)
     if (!IsMaster(dev) && !IsFloating(dev))
         dev = GetMaster(dev, MASTER_ATTACHED);
 
-    return dev->spriteInfo->paired;
+    return dev->spriteInfo? dev->spriteInfo->paired: NULL;
 }
 
 /**
