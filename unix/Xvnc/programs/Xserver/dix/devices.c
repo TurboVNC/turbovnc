TurboVNC/turbovnc

commit 3550ff3cf3f9dc0b15ca2f156fab918cbc453178

Upgrade X.org infrastructure
FreeType 2.9.1 --> 2.10.1
Mesa 17.3.9 --> 19.2.8
bigreqsproto 1.1.2 --> xorgproto 2019.2
compositeproto 0.4.2 --> xorgproto 2019.2
damageproto 1.2.1 --> xorgproto 2019.2
fixesproto 5.0 --> xorgproto 2019.2
fontsproto 2.1.3 --> xorgproto 2019.2
glproto 1.4.17 --> xorgproto 2019.2
inputproto 2.3.2 --> xorgproto 2019.2
kbproto 1.0.7 --> xorgproto 2019.2
libXext 1.3.3 --> 1.3.4
libxkbfile 1.0.9 --> 1.1.0
presentproto 1.1 --> xorgproto 2019.2
randrproto 1.5.0 --> xorgproto 2019.2
recordproto 1.14.2 --> xorgproto 2019.2
renderproto 0.11.1 --> xorgproto 2019.2
resourceproto 1.2.0 --> xorgproto 2019.2
scrnsaverproto 1.2.2 --> xorgproto 2019.2
videoproto 2.3.3 --> xorgproto 2019.2
xcmiscproto 1.2.2 --> xorgproto 2019.2
xextproto 7.3.0 --> xorgproto 2019.2
xf86bigfontproto 1.2.0 --> xorgproto 2019.2
xineramaproto 1.2.1 --> xorgproto 2019.2
xproto 7.0.31 --> xorgproto 2019.2
libX11 1.6.7 --> 1.6.9
libXau 1.0.8 --> 1.0.9
libXdmcp 1.1.2 --> 1.1.3
libXfont2 2.0.3 --> 2.0.4
libfontenc 1.1.3 --> 1.1.4
libxcb 1.13 --> 1.13.1
pixman 0.34.0 --> 0.38.4
xorg-xserver 1.19.7 --> 1.20.7

unix/Xvnc/programs/Xserver/CMakeLists.txt
@@ -20,7 +20,7 @@ if(BITS EQUAL 64)
 endif()
 
 set(VENDOR_NAME "The X.Org Foundation")
-set(VENDOR_RELEASE "11906000")
+set(VENDOR_RELEASE "12007000")
 configure_file(include/version-config.h.in include/version-config.h)
 
 set(CMAKE_REQUIRED_LIBRARIES m)
@@ -28,11 +28,9 @@ check_symbol_exists(cbrt math.h HAVE_CBRT)
 set(CMAKE_REQUIRED_LIBRARIES)
 include(CheckIncludeFiles)
 check_include_files(dlfcn.h HAVE_DLFCN_H)
-check_include_files(ieeefp.h HAVE_IEEEFP_H)
 if(FOUND_GLIBC)
 	set(CMAKE_REQUIRED_DEFINITIONS -D_GNU_SOURCE)
 endif()
-check_symbol_exists(ffs strings.h HAVE_FFS)
 check_symbol_exists(getdtablesize unistd.h HAVE_GETDTABLESIZE)
 check_symbol_exists(getifaddrs ifaddrs.h HAVE_GETIFADDRS)
 check_symbol_exists(getpeereid unistd.h HAVE_GETPEEREID)

unix/Xvnc/programs/Xserver/dix/devices.c
@@ -76,11 +76,6 @@ SOFTWARE.
 #include <X11/extensions/XI2.h>
 #include <X11/extensions/XIproto.h>
 #include <math.h>
-#ifdef TURBOVNC
-#ifdef HAVE_IEEEFP_H
-#include <ieeefp.h>
-#endif
-#endif
 #include <pixman.h>
 #include "exglobals.h"
 #include "exevents.h"
@@ -179,11 +174,7 @@ DeviceSetProperty(DeviceIntPtr dev, Atom property, XIPropertyValuePtr prop,
             return BadValue;
 
         for (i = 0; i < 9; i++)
-#ifdef TURBOVNC
-            if (!finite(f[i]))
-#else
             if (!isfinite(f[i]))
-#endif
                 return BadValue;
 
         if (!dev->valuator)
@@ -1186,6 +1177,7 @@ RemoveDevice(DeviceIntPtr dev, BOOL sendevent)
             flags[tmp->id] = IsMaster(tmp) ? XIMasterRemoved : XISlaveRemoved;
             CloseDevice(tmp);
             ret = Success;
+            break;
         }
     }
 
@@ -1202,6 +1194,7 @@ RemoveDevice(DeviceIntPtr dev, BOOL sendevent)
                 prev->next = next;
 
             ret = Success;
+            break;
         }
     }
 

unix/Xvnc/programs/Xserver/include/dix-config.h.in
@@ -9,9 +9,6 @@
 /* Define to 1 if you have the <dlfcn.h> header file. */
 #cmakedefine HAVE_DLFCN_H
 
-/* Define to 1 if you have the `ffs' function. */
-#cmakedefine HAVE_FFS
-
 /* Define to 1 if you have the `getdtablesize' function. */
 #cmakedefine HAVE_GETDTABLESIZE
 
@@ -26,9 +23,6 @@
 
 /* Define to 1 if you have the `getzoneid' function. */
 #cmakedefine HAVE_GETZONEID
-
-/* Define to 1 if you have the <ieeefp.h> header file. */
-#cmakedefine HAVE_IEEEFP_H
 
 /* Define to 1 if you have the `reallocarray' function. */
 #cmakedefine HAVE_REALLOCARRAY
@@ -82,13 +76,13 @@
 #define LISTEN_UNIX
 
 /* Have setitimer support */
-#cmakedefine01 HAVE_SETITIMER
+#cmakedefine HAVE_SETITIMER
 
 /* Have poll() */
-#cmakedefine01 HAVE_POLL
+#cmakedefine HAVE_POLL
 
 /* Have epoll_create1() */
-#cmakedefine01 HAVE_EPOLL_CREATE1
+#cmakedefine HAVE_EPOLL_CREATE1
 
 #define CMAKE_INSTALL_FULL_SYSCONFDIR "@CMAKE_INSTALL_FULL_SYSCONFDIR@"
 

unix/Xvnc/CMakeLists.txt
@@ -62,33 +62,6 @@ if(NOT TVNC_SYSTEMX11)
 	endfunction()
 endif()
 
-function(handle_type_puns)
-	# NOTE: For whatever reason, Clang doesn't need this
-	if(CMAKE_COMPILER_IS_GNUCC)
-		if(ARGC EQUAL 0)
-			set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fno-strict-aliasing" PARENT_SCOPE)
-		else()
-			foreach(file ${ARGV})
-				set_property(SOURCE ${file} APPEND_STRING PROPERTY COMPILE_FLAGS
-					" -fno-strict-aliasing")
-			endforeach()
-		endif()
-	endif()
-endfunction()
-
-function(handle_unused)
-	if(CMAKE_COMPILER_IS_GNUCC OR CMAKE_C_COMPILER_ID STREQUAL "Clang")
-		if(ARGC EQUAL 0)
-			set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-unused" PARENT_SCOPE)
-		else()
-			foreach(file ${ARGV})
-				set_property(SOURCE ${file} APPEND_STRING PROPERTY COMPILE_FLAGS
-					" -Wno-unused")
-			endforeach()
-		endif()
-	endif()
-endfunction()
-
 if(TVNC_SYSTEMX11)
 	include_directories(${X11_X11_INCLUDE_PATH} ${X11_Xau_INCLUDE_PATH}
 		${X11_Xdmcp_INCLUDE_PATH} ${X11_Xkbfile_INCLUDE_PATH})

unix/Xvnc/extras/Mesa/CMakeLists.txt
@@ -7,18 +7,14 @@ foreach(h ${HEADERS})
 endforeach()
 
 copy_X_header(include/GL/internal/dri_interface.h GL/internal)
+copy_X_header(include/KHR/khrplatform.h KHR)
 
 include_directories(include src src/mapi src/mapi/glapi)
 
-add_definitions(-DGLX_USE_TLS -DHAVE_PTHREAD -DMAPI_MODE_UTIL
-	-DGLX_INDIRECT_RENDERING)
+add_definitions(-DUSE_ELF_TLS -DHAVE_PTHREAD -DHAVE_TIMESPEC_GET
+	-DMAPI_MODE_UTIL -DGLX_INDIRECT_RENDERING)
 
 disable_compiler_warnings()
-handle_type_puns(src/mapi/u_current.c)
-handle_unused(
-	src/glx/glx_pbuffer.c
-	src/glx/glxcmds.c
-	src/glx/indirect.c)
 
 add_library(glapi-server STATIC
 	src/mapi/glapi/glapi_dispatch.c

unix/Xvnc/extras/Mesa/turbovnc.patch
@@ -1,6 +1,6 @@
-diff --git a/mesa-17.3.9/include/c99_math.h b/Mesa/include/c99_math.h
+diff --git a/mesa-19.2.8/include/c99_math.h b/Mesa/include/c99_math.h
 index e906c26..e14c6d9 100644
---- a/mesa-17.3.9/include/c99_math.h
+--- a/mesa-19.2.8/include/c99_math.h
 +++ b/Mesa/include/c99_math.h
 @@ -44,6 +44,7 @@
  #endif 
@@ -34,36 +34,39 @@ index e906c26..e14c6d9 100644
  
  
  /* Since C++11, the following functions are part of the std namespace. Their C
-diff --git a/mesa-17.3.9/src/util/debug.c b/Mesa/src/util/debug.c
-index 98b1853..867c833 100644
---- a/mesa-17.3.9/src/util/debug.c
+diff --git a/mesa-19.2.8/src/util/debug.c b/Mesa/src/util/debug.c
+index 7079c61..c3686bf 100644
+--- a/mesa-19.2.8/src/util/debug.c
 +++ b/Mesa/src/util/debug.c
-@@ -22,9 +22,12 @@
-  */
+@@ -23,10 +23,15 @@
  
+ #include <errno.h>
  #include <string.h>
 +#if 0 /* TURBOVNC */
  #include "main/macros.h"
 +#endif /* TURBOVNC */
  #include "debug.h"
++#if 0 /* TURBOVNC */
+ #include "u_string.h"
++#endif /* TURBOVNC */
  
 +#if 0 /* TURBOVNC */
  uint64_t
  parse_debug_string(const char *debug,
                     const struct debug_control *control)
-@@ -51,6 +54,7 @@ parse_debug_string(const char *debug,
+@@ -67,6 +72,7 @@ comma_separated_list_contains(const char *list, const char *s)
  
-    return flag;
+    return false;
  }
 +#endif /* TURBOVNC */
  
  /**
   * Reads an environment variable and interprets its value as a boolean.
-diff --git a/mesa-17.3.9/src/util/debug.h b/Mesa/src/util/debug.h
-index 75ebc2e..3c12cd8 100644
---- a/mesa-17.3.9/src/util/debug.h
+diff --git a/mesa-19.2.8/src/util/debug.h b/Mesa/src/util/debug.h
+index bbcc197..c91ac3c 100644
+--- a/mesa-19.2.8/src/util/debug.h
 +++ b/Mesa/src/util/debug.h
-@@ -36,9 +36,11 @@ struct debug_control {
+@@ -36,11 +36,13 @@ struct debug_control {
      uint64_t     flag;
  };
  
@@ -71,7 +74,9 @@ index 75ebc2e..3c12cd8 100644
  uint64_t
  parse_debug_string(const char *debug,
                     const struct debug_control *control);
+ bool
+ comma_separated_list_contains(const char *list, const char *s);
 +#endif /* TURBOVNC */
  bool
  env_var_as_boolean(const char *var_name, bool default_value);
- 
+ unsigned

unix/Xvnc/extras/freetype2/CMakeLists.txt
@@ -8,7 +8,6 @@ endif()
 add_definitions(-DFT2_BUILD_LIBRARY)
 
 disable_compiler_warnings()
-handle_type_puns(src/psaux/psaux.c)
 
 add_library(freetype2 STATIC
 	src/base/ftsystem.c

commit 7c55a04cb1226e4aabb7f547984c2ddc26629d55

Server: Fix Solaris/GCC build
(broken by 3550ff3cf3f9dc0b15ca2f156fab918cbc453178)

unix/Xvnc/programs/Xserver/CMakeLists.txt
@@ -28,6 +28,7 @@ check_symbol_exists(cbrt math.h HAVE_CBRT)
 set(CMAKE_REQUIRED_LIBRARIES)
 include(CheckIncludeFiles)
 check_include_files(dlfcn.h HAVE_DLFCN_H)
+check_include_files(ieeefp.h HAVE_IEEEFP_H)
 if(FOUND_GLIBC)
 	set(CMAKE_REQUIRED_DEFINITIONS -D_GNU_SOURCE)
 endif()

unix/Xvnc/programs/Xserver/dix/devices.c
@@ -76,6 +76,11 @@ SOFTWARE.
 #include <X11/extensions/XI2.h>
 #include <X11/extensions/XIproto.h>
 #include <math.h>
+#ifdef TURBOVNC
+#ifdef HAVE_IEEEFP_H
+#include <ieeefp.h>
+#endif
+#endif
 #include <pixman.h>
 #include "exglobals.h"
 #include "exevents.h"
@@ -174,7 +179,11 @@ DeviceSetProperty(DeviceIntPtr dev, Atom property, XIPropertyValuePtr prop,
             return BadValue;
 
         for (i = 0; i < 9; i++)
+#ifdef TURBOVNC
+            if (!finite(f[i]))
+#else
             if (!isfinite(f[i]))
+#endif
                 return BadValue;
 
         if (!dev->valuator)

unix/Xvnc/programs/Xserver/include/dix-config.h.in
@@ -24,6 +24,9 @@
 /* Define to 1 if you have the `getzoneid' function. */
 #cmakedefine HAVE_GETZONEID
 
+/* Define to 1 if you have the <ieeefp.h> header file. */
+#cmakedefine HAVE_IEEEFP_H
+
 /* Define to 1 if you have the `reallocarray' function. */
 #cmakedefine HAVE_REALLOCARRAY
 

unix/Xvnc/programs/Xserver/turbovnc.patch
@@ -69,6 +69,34 @@ index cbdd912..371f881 100644
          XIBarrierRemoveMasterDevice(clients[i], ptr->id);
  
      /* disable the remove the devices, XTest devices must be done first
+diff --git a/xorg-server-1.20.7/dix/devices.c b/Xserver/dix/devices.c
+index 1b18b16..cf59f4a 100644
+--- a/xorg-server-1.20.7/dix/devices.c
++++ b/Xserver/dix/devices.c
+@@ -76,6 +76,11 @@ SOFTWARE.
+ #include <X11/extensions/XI2.h>
+ #include <X11/extensions/XIproto.h>
+ #include <math.h>
++#ifdef TURBOVNC
++#ifdef HAVE_IEEEFP_H
++#include <ieeefp.h>
++#endif
++#endif
+ #include <pixman.h>
+ #include "exglobals.h"
+ #include "exevents.h"
+@@ -174,7 +179,11 @@ DeviceSetProperty(DeviceIntPtr dev, Atom property, XIPropertyValuePtr prop,
+             return BadValue;
+ 
+         for (i = 0; i < 9; i++)
++#ifdef TURBOVNC
++            if (!finite(f[i]))
++#else
+             if (!isfinite(f[i]))
++#endif
+                 return BadValue;
+ 
+         if (!dev->valuator)
 diff --git a/xorg-server-1.20.7/dix/events.c b/Xserver/dix/events.c
 index c02a059..f78973b 100644
 --- a/xorg-server-1.20.7/dix/events.c