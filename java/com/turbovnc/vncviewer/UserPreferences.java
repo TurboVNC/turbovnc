TurboVNC/turbovnc

commit 23a5f12431314e37b93e491f76ce9e5c27ba3d71

Re-implement and fix the "Save defaults" feature by using the Java Preferences API.  This lays the groundwork for future expansion, including a possible implementation of connection-specific preferences (a la the Windows viewer.)  This patch also fixes a NullPointerException that occurred on some platforms when entering a blank string in the connection dialog, as well as fixing and generally improving the X509 certificate load dialogs.
git-svn-id: svn+ssh://svn.code.sf.net/p/turbovnc/code/trunk@2204 799e4f7b-5fd2-41f6-823c-2ecc41bc7f0b

java/com/turbovnc/vncviewer/UserPreferences.java
@@ -1,0 +1,251 @@
+/* Copyright (C) 2012 Brian P. Hinz
+ * 
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+ * USA.
+ */
+
+//
+// For storing user preferences not specified as configuration parameters.
+//
+
+package com.turbovnc.vncviewer;
+
+import java.util.prefs.Preferences;
+import java.util.prefs.BackingStoreException;
+
+import com.turbovnc.rfb.*;
+
+public class UserPreferences {
+
+  private static Preferences root = Preferences.userRoot().node("TurboVNC");
+
+  public static void set(String nName, String key, String val) {
+    Preferences node = root.node(nName);
+    node.put(key, val);
+  }
+
+  public static void set(String nName, String key, int val) {
+    Preferences node = root.node(nName);
+    node.putInt(key, val);
+  }
+
+  public static void set(String nName, String key, boolean val) {
+    Preferences node = root.node(nName);
+    node.putBoolean(key, val);
+  }
+
+  public static String get(String nName, String key) {
+    Preferences node = root.node(nName);
+    return node.get(key, null);
+  }
+
+  public static boolean getBool(String nName, String key, boolean defval) {
+    Preferences node = root.node(nName);
+    return node.getBoolean(key, defval);
+  }
+
+  public static int getInt(String nName, String key, int defval) {
+    Preferences node = root.node(nName);
+    return node.getInt(key, defval);
+  }
+
+  public static void save() {
+    try {
+      root.sync();
+			System.out.println(root.absolutePath());
+      String[] keys = root.keys();
+      for (int i = 0; i < keys.length; i++)
+        vlog.debug(keys[i] + " = " + root.get(keys[i], null));
+    } catch (BackingStoreException e) {
+      vlog.error(e.getMessage());
+    }
+  }
+
+  public static void save(String nName) {
+    try {
+      Preferences node = root.node(nName);
+			System.out.println(node.absolutePath());
+      node.sync();
+      String[] keys = root.keys();
+      for (int i = 0; i < keys.length; i++)
+        vlog.debug(keys[i] + " = " + node.get(keys[i], null));
+    } catch (BackingStoreException e) {
+      vlog.error(e.getMessage());
+    }
+  }
+
+  public static void clear() {
+    try {
+      root.clear();
+      String[] children = root.childrenNames();
+      for (int i = 0; i < children.length; i++) {
+        Preferences node = root.node(children[i]);
+        node.removeNode();
+      }
+      root.sync();
+    } catch (BackingStoreException e) {
+      vlog.error(e.getMessage());
+    }
+  }
+
+  public static void load(String nName) {
+    // Sets the value of any corresponding Configuration parameters
+    boolean secVeNCrypt = true, setSecVeNCrypt = false;
+    boolean encNone = true, setEncNone = false;
+    boolean encTLS = true, setEncTLS = false;
+    boolean encX509 = true, setEncX509 = false;
+    boolean secPlain = true, setSecPlain = false;
+    boolean secIdent = true, setSecIdent = false;
+    boolean secNone = true, setSecNone = false;
+    boolean secVnc = true, setSecVnc = false;
+    try {
+      Preferences node = root.node(nName);
+      String[] keys = node.keys();
+      for (int i = 0; i < keys.length; i++) {
+        String key = keys[i];
+        VoidParameter p = Configuration.getParam(key);
+        if (p == null) {
+          if (key.equalsIgnoreCase("secVeNCrypt")) {
+            setSecVeNCrypt = true;
+            secVeNCrypt = node.getBoolean(key, true);
+          } else if (key.equalsIgnoreCase("encNone")) {
+            setEncNone = true;
+            encNone = node.getBoolean(key, true);
+          } else if (key.equalsIgnoreCase("encTLS")) {
+            setEncTLS = true;
+            encTLS = node.getBoolean(key, true);
+          } else if (key.equalsIgnoreCase("encX509")) {
+            setEncX509 = true;
+            encX509 = node.getBoolean(key, true);
+          } else if (key.equalsIgnoreCase("secPlain")) {
+            setSecPlain = true;
+            secPlain = node.getBoolean(key, true);
+          } else if (key.equalsIgnoreCase("secIdent")) {
+            setSecIdent = true;
+            secIdent = node.getBoolean(key, true);
+          } else if (key.equalsIgnoreCase("secNone")) {
+            setSecNone = true;
+            secNone = node.getBoolean(key, true);
+          } else if (key.equalsIgnoreCase("secVnc")) {
+            setSecVnc = true;
+            secVnc = node.getBoolean(key, true);
+          } else if (!key.equalsIgnoreCase("x509ca")) {
+            String valueStr = node.get(key, null);
+            if (valueStr != null)
+              CSecurityTLS.x509ca.setParam(valueStr);
+          } else if (!key.equalsIgnoreCase("x509crl")) {
+            String valueStr = node.get(key, null);
+            if (valueStr != null)
+              CSecurityTLS.x509crl.setParam(valueStr);
+          } else
+            continue;
+        }
+
+        String valueStr = node.get(key, null);
+        if (valueStr != null)
+          Configuration.setParam(key, valueStr);
+      }
+    } catch (BackingStoreException e) {
+      vlog.error(e.getMessage());
+    }
+    if ((setEncX509 || setEncTLS || setEncNone) &&
+        (setSecPlain || setSecIdent || setSecVnc || setSecNone)) {
+      Security.DisableSecType(Security.secTypeNone);
+      Security.DisableSecType(Security.secTypeVncAuth);
+      Security.DisableSecType(Security.secTypePlain);
+      Security.DisableSecType(Security.secTypeIdent);
+      Security.DisableSecType(Security.secTypeTLSNone);
+      Security.DisableSecType(Security.secTypeTLSVnc);
+      Security.DisableSecType(Security.secTypeTLSPlain);
+      Security.DisableSecType(Security.secTypeTLSIdent);
+      Security.DisableSecType(Security.secTypeX509None);
+      Security.DisableSecType(Security.secTypeX509Vnc);
+      Security.DisableSecType(Security.secTypeX509Plain);
+      Security.DisableSecType(Security.secTypeX509Ident);
+    }
+
+    if (setSecVeNCrypt) {
+      if (secVeNCrypt) Security.EnableSecType(Security.secTypeVeNCrypt);
+      else {
+        Security.DisableSecType(Security.secTypeVeNCrypt);
+        encX509 = encTLS = secPlain = secIdent = false;
+      }
+      Security.setInUserPrefs = true;
+    }
+    if (setEncX509 && setSecPlain) {
+      if (encX509 && secPlain) Security.EnableSecType(Security.secTypeX509Plain);
+      else Security.DisableSecType(Security.secTypeX509Plain);
+      Security.setInUserPrefs = true;
+    }
+    if (setEncX509 && setSecIdent) {
+      if (encX509 && secIdent) Security.EnableSecType(Security.secTypeX509Ident);
+      else Security.DisableSecType(Security.secTypeX509Ident);
+      Security.setInUserPrefs = true;
+    }
+    if (setEncX509 && setSecVnc) {
+      if (encX509 && secVnc) Security.EnableSecType(Security.secTypeX509Vnc);
+      else Security.DisableSecType(Security.secTypeX509Vnc);
+      Security.setInUserPrefs = true;
+    }
+    if (setEncX509 && setSecNone) {
+      if (encX509 && secNone) Security.EnableSecType(Security.secTypeX509None);
+      else Security.DisableSecType(Security.secTypeX509None);
+      Security.setInUserPrefs = true;
+    }
+    if (setEncTLS && setSecPlain) {
+      if (encTLS && secPlain) Security.EnableSecType(Security.secTypeTLSPlain);
+      else Security.DisableSecType(Security.secTypeTLSPlain);
+      Security.setInUserPrefs = true;
+    }
+    if (setEncTLS && setSecIdent) {
+      if (encTLS && secIdent) Security.EnableSecType(Security.secTypeTLSIdent);
+      else Security.DisableSecType(Security.secTypeTLSIdent);
+      Security.setInUserPrefs = true;
+    }
+    if (setEncTLS && setSecVnc) {
+      if (encTLS && secVnc) Security.EnableSecType(Security.secTypeTLSVnc);
+      else Security.DisableSecType(Security.secTypeTLSVnc);
+      Security.setInUserPrefs = true;
+    }
+    if (setEncTLS && setSecNone) {
+      if (encTLS && secNone) Security.EnableSecType(Security.secTypeTLSNone);
+      else Security.DisableSecType(Security.secTypeTLSNone);
+      Security.setInUserPrefs = true;
+    }
+    if (setEncNone && setSecPlain) {
+      if (encNone && secPlain) Security.EnableSecType(Security.secTypePlain);
+      else Security.DisableSecType(Security.secTypePlain);
+      Security.setInUserPrefs = true;
+    }
+    if (setEncNone && setSecIdent) {
+      if (encNone && secIdent) Security.EnableSecType(Security.secTypeIdent);
+      else Security.DisableSecType(Security.secTypeIdent);
+      Security.setInUserPrefs = true;
+    }
+    if (setSecVnc) {
+      if (secVnc) Security.EnableSecType(Security.secTypeVncAuth);
+      else Security.DisableSecType(Security.secTypeVncAuth);
+      Security.setInUserPrefs = true;
+    }
+    if (setSecNone) {
+      if (secNone) Security.EnableSecType(Security.secTypeNone);
+      else Security.DisableSecType(Security.secTypeNone);
+      Security.setInUserPrefs = true;
+    }
+  }
+
+  static LogWriter vlog = new LogWriter("UserPreferences");
+}
+

java/com/turbovnc/rfb/CSecurityTLS.java
@@ -35,20 +35,18 @@ import java.util.ArrayList;
 import java.util.Collection;
 import javax.swing.JOptionPane;
 
-import com.turbovnc.vncviewer.UserPrefs;
 import com.turbovnc.rdr.*;
 import com.turbovnc.network.*;
-import java.util.concurrent.Executors;
-import java.nio.channels.SelectionKey;
+import com.turbovnc.vncviewer.*;
 
 public class CSecurityTLS extends CSecurity {
 
   public static StringParameter x509ca
   = new StringParameter("x509ca",
-                        "X509 CA certificate", "");
+    "X509 CA certificate", FileUtils.getVncHomeDir() + "x509_ca.pem");
   public static StringParameter x509crl
   = new StringParameter("x509crl",
-                        "X509 CRL file", "");
+    "X509 CRL file", FileUtils.getVncHomeDir() + "x509_crl.pem");
 
   private void initGlobal() 
   {
@@ -69,30 +67,6 @@ public class CSecurityTLS extends CSecurity {
   {
     anon = _anon;
     session = null;
-    
-    setDefaults();
-    cafile = x509ca.getData(); 
-    crlfile = x509crl.getData(); 
-  }
-
-  public static void setDefaults()
-  {
-    String homeDir = null;
-    
-    if ((homeDir=UserPrefs.getHomeDir()) == null) {
-      vlog.error("Could not obtain VNC home directory path");
-      return;
-    }
-
-    String vnchomedir = homeDir+UserPrefs.getFileSeparator()+".vnc"+
-                        UserPrefs.getFileSeparator();
-    String caDefault = new String(vnchomedir+"x509_ca.pem");
-    String crlDefault = new String(vnchomedir+"x509_crl.pem");
-
-    if (new File(caDefault).exists())
-      x509ca.setDefaultStr(caDefault);
-    if (new File(crlDefault).exists())
-      x509crl.setDefaultStr(crlDefault);
   }
 
 // FIXME:
@@ -184,7 +158,7 @@ public class CSecurityTLS extends CSecurity {
       engine.setEnabledCipherSuites(engine.getSupportedCipherSuites());
     }
 
-    engine.setEnabledProtocols(new String[]{"SSLv3","TLSv1"});
+    engine.setEnabledProtocols(new String[]{"SSLv3", "TLSv1"});
   }
 
   class MyHandshakeListener implements HandshakeCompletedListener {
@@ -207,18 +181,19 @@ public class CSecurityTLS extends CSecurity {
       CertificateFactory cf = CertificateFactory.getInstance("X.509");
       try {
         ks.load(null, null);
-        File cacert = new File(cafile);
+        File cacert = new File(x509ca.getValue());
         if (!cacert.exists() || !cacert.canRead())
           return;
-        InputStream caStream = new FileInputStream(cafile);
+        InputStream caStream = new FileInputStream(x509ca.getValue());
         X509Certificate ca = (X509Certificate)cf.generateCertificate(caStream);
         ks.setCertificateEntry("CA", ca);
-        PKIXBuilderParameters params = new PKIXBuilderParameters(ks, new X509CertSelector());
-        File crlcert = new File(crlfile);
+        PKIXBuilderParameters params =
+          new PKIXBuilderParameters(ks, new X509CertSelector());
+        File crlcert = new File(x509crl.getValue());
         if (!crlcert.exists() || !crlcert.canRead()) {
           params.setRevocationEnabled(false);
         } else {
-          InputStream crlStream = new FileInputStream(crlfile);
+          InputStream crlStream = new FileInputStream(x509crl.getValue());
           Collection<? extends CRL> crls = cf.generateCRLs(crlStream);
           CertStoreParameters csp = new CollectionCertStoreParameters(crls);
           CertStore store = CertStore.getInstance("Collection", csp);
@@ -248,7 +223,7 @@ public class CSecurityTLS extends CSecurity {
       } catch (CertificateException e) {
         Object[] answer = {"Proceed", "Exit"};
         int ret = JOptionPane.showOptionDialog(null,
-          e.getCause().getLocalizedMessage()+"\n"+
+          e.getCause().getLocalizedMessage()+"\n" +
           "Continue connecting to this host?",
           "Confirm certificate exception?",
           JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE,
@@ -266,14 +241,15 @@ public class CSecurityTLS extends CSecurity {
     }
   }
 
-  public final int getType() { return anon ? Security.secTypeTLSNone : Security.secTypeX509None; }
+  public final int getType() {
+    return anon ? Security.secTypeTLSNone : Security.secTypeX509None;
+  }
+
   public final String description() 
     { return anon ? "TLSNone" : "X509None"; }
 
   //protected void checkSession();
   protected CConnection client;
-
-
 
   private SSLContext ctx;
   private SSLSession session;
@@ -281,7 +257,6 @@ public class CSecurityTLS extends CSecurity {
   private SSLEngineManager manager;
   private boolean anon;
 
-  private String cafile, crlfile;
   private FdInStream is;
   private FdOutStream os;
 

java/com/turbovnc/rfb/Security.java
@@ -63,6 +63,9 @@ public class Security {
 
   public Security(StringParameter secTypes)
   {
+    if (setInUserPrefs && secTypes.isDefault) return;
+
+    System.out.println("Setting security types from parameter");
     String secTypesStr;
 
     secTypesStr = secTypes.getData();
@@ -126,7 +129,9 @@ public class Security {
     return false;
   }
 
-  public static void DisableSecType(int secType) { enabledSecTypes.remove((Object)secType); }
+  public static void DisableSecType(int secType) {
+    enabledSecTypes.remove((Object)secType);
+  }
 
   public static int secTypeNum(String name) {
     if (name.equalsIgnoreCase("None"))      return secTypeNone;
@@ -195,7 +200,11 @@ public class Security {
     return (result);
   }
 
-  public final void SetSecTypes(List<Integer> secTypes) { enabledSecTypes = secTypes; }
+  public final void SetSecTypes(List<Integer> secTypes) {
+    enabledSecTypes = secTypes;
+  }
+
+  public static boolean setInUserPrefs;
 
   static LogWriter vlog = new LogWriter("Security");
 }

java/com/turbovnc/rfb/SecurityClient.java
@@ -72,11 +72,6 @@ public class SecurityClient extends Security {
   
   }
 
-  public static void setDefaults()
-  {
-      CSecurityTLS.setDefaults();
-  }
-
   String msg = null;
 
   public static StringParameter secTypes 

java/com/turbovnc/rfb/StringParameter.java
@@ -34,6 +34,7 @@ public class StringParameter extends VoidParameter {
 
   public boolean setParam(String v) {
     value = v;
+    isDefault = false;
     return value != null;
   }
 
@@ -48,6 +49,8 @@ public class StringParameter extends VoidParameter {
 
   public String getValue() { return value; }
   public String getData() { return value; }
+
+  public boolean isDefault = true;
 
   protected String value;
   protected String defValue;

java/com/turbovnc/vncviewer/CConn.java
@@ -156,12 +156,14 @@ public class CConn extends CConnection
         serverPort = Hostname.getPort(vncServerName);
       } else {
         ServerDialog dlg = new ServerDialog(options, vncServerName, this);
-        if (!dlg.showDialog() || dlg.server.getSelectedItem().equals("")) {
+        boolean status = dlg.showDialog();
+        Object item = dlg.server.getSelectedItem();
+        if (!status || item == null || item.toString().equals("")) {
           vlog.info("No server name specified!");
           close();
           return;
         }
-        vncServerName = (String)dlg.server.getSelectedItem();
+        vncServerName = (String)item;
         serverHost = Hostname.getHost(vncServerName);
         serverPort = Hostname.getPort(vncServerName);
       }
@@ -920,14 +922,15 @@ public class CConn extends CConnection
       options.shared.setSelected(viewer.shared.getValue());
       options.sendLocalUsername.setSelected(viewer.sendLocalUsername.getValue());
       options.secUnixLogin.setSelected(!viewer.noUnixLogin.getValue());
-      
+
       /* Process non-VeNCrypt sectypes */
       java.util.List<Integer> secTypes = new ArrayList<Integer>();
       secTypes = Security.GetEnabledSecTypes();
+      boolean enableVeNCrypt = false;
       for (Iterator i = secTypes.iterator(); i.hasNext();) {
         switch ((Integer)i.next()) {
         case Security.secTypeVeNCrypt:
-          options.secVeNCrypt.setSelected(true);
+          enableVeNCrypt = true;
           break;
         case Security.secTypeNone:
           options.secNone.setSelected(true);
@@ -939,54 +942,65 @@ public class CConn extends CConnection
       }
 
       /* Process VeNCrypt subtypes */
-      if (options.secVeNCrypt.isSelected()) {
+      if (enableVeNCrypt) {
         java.util.List<Integer> secTypesExt = new ArrayList<Integer>();
         secTypesExt = Security.GetEnabledExtSecTypes();
         for (Iterator iext = secTypesExt.iterator(); iext.hasNext();) {
           switch ((Integer)iext.next()) {
           case Security.secTypePlain:
+            options.secVeNCrypt.setSelected(true);
             options.encNone.setSelected(true);
             options.secPlain.setSelected(true);
             break;
           case Security.secTypeIdent:
+            options.secVeNCrypt.setSelected(true);
             options.encNone.setSelected(true);
             options.secIdent.setSelected(true);
             break;
           case Security.secTypeTLSNone:
+            options.secVeNCrypt.setSelected(true);
             options.encTLS.setSelected(true);
             options.secNone.setSelected(true);
             break;
           case Security.secTypeTLSVnc:
+            options.secVeNCrypt.setSelected(true);
             options.encTLS.setSelected(true);
             options.secVnc.setSelected(true);
             break;
           case Security.secTypeTLSPlain:
+            options.secVeNCrypt.setSelected(true);
             options.encTLS.setSelected(true);
             options.secPlain.setSelected(true);
             break;
           case Security.secTypeTLSIdent:
+            options.secVeNCrypt.setSelected(true);
             options.encTLS.setSelected(true);
             options.secIdent.setSelected(true);
             break;
           case Security.secTypeX509None:
+            options.secVeNCrypt.setSelected(true);
             options.encX509.setSelected(true);
             options.secNone.setSelected(true);
             break;
           case Security.secTypeX509Vnc:
+            options.secVeNCrypt.setSelected(true);
             options.encX509.setSelected(true);
             options.secVnc.setSelected(true);
             break;
           case Security.secTypeX509Plain:
+            options.secVeNCrypt.setSelected(true);
             options.encX509.setSelected(true);
             options.secPlain.setSelected(true);
             break;
           case Security.secTypeX509Ident:
+            options.secVeNCrypt.setSelected(true);
             options.encX509.setSelected(true);
             options.secIdent.setSelected(true);
             break;
           }
         }
-      } else {
+      }
+      if (!options.secVeNCrypt.isSelected()) {
         options.encNone.setEnabled(false);
         options.encTLS.setEnabled(false);
         options.encX509.setEnabled(false);

java/com/turbovnc/vncviewer/FileUtils.java
@@ -1,0 +1,79 @@
+/* Copyright (C) 2012 Brian P. Hinz
+ * 
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
+ * USA.
+ */
+
+package com.turbovnc.vncviewer;
+
+import javax.swing.filechooser.FileSystemView;
+
+import com.turbovnc.rfb.LogWriter;
+
+public class FileUtils {
+
+  public static final String getHomeDir() {
+    String homeDir = null;
+    try {
+      String os = System.getProperty("os.name").toLowerCase();
+      try {
+        if (os.startsWith("windows")) {
+          // JRE prior to 1.5 cannot reliably determine USERPROFILE.
+          // Return user.home and hope it's right.
+          if (Integer.parseInt(System.getProperty("java.version").split("\\.")[1]) < 5) {
+            try {
+              homeDir = System.getProperty("user.home");
+            } catch(java.security.AccessControlException e) {
+              vlog.error("Cannot access user.home system property:" + 
+                         e.getMessage());
+            }
+          } else {
+            homeDir = System.getenv("USERPROFILE");
+          }
+        } else {
+          try {
+            homeDir = FileSystemView.getFileSystemView().
+                      getDefaultDirectory().getCanonicalPath();
+          } catch(java.security.AccessControlException e) {
+            vlog.error("Cannot access system property:" + e.getMessage());
+          }
+        }
+      } catch (java.lang.Exception e) {
+        e.printStackTrace();
+      }
+    } catch(java.security.AccessControlException e) {
+      vlog.error("Cannot access os.name system property:" + e.getMessage());
+    }
+
+    return homeDir + getFileSeparator();
+  }
+
+  public static final String getVncHomeDir() {
+    return getHomeDir() + ".vnc" + getFileSeparator();
+  }
+
+  public static final String getFileSeparator() {
+    String separator = null;
+    try {
+      separator = Character.toString(java.io.File.separatorChar);
+    } catch(java.security.AccessControlException e) {
+      vlog.error("Cannot access file.separator system property:" + 
+                 e.getMessage());
+    }
+    return separator;
+  }
+
+  static LogWriter vlog = new LogWriter("FileUtils");
+}

commit c421b400acc6c70efc569e6e5b37a8ad41ebe5f1

UserPreferences.java: Remove unused/incorrect code
The code in question was introduced in
23a5f12431314e37b93e491f76ce9e5c27ba3d71.  It would only ever be invoked
if the viewer stored a Preferences key, other than the enc* and sec*
keys, that didn't match a configuration parameter name.  However, the
viewer never does that.  If the viewer did do that, then a logic error
in the code in question would cause the key's value to overwrite the
value of the X509CA and X509CRL parameters, which is incorrect behavior.

java/com/turbovnc/vncviewer/UserPreferences.java
@@ -143,14 +143,6 @@ public final class UserPreferences {
           } else if (key.equalsIgnoreCase("secUnixLogin")) {
             setSecUnixLogin = true;
             secUnixLogin = node.getBoolean(key, true);
-          } else if (!key.equalsIgnoreCase("x509ca")) {
-            String valueStr = node.get(key, null);
-            if (valueStr != null)
-              SecurityClient.x509ca.setParam(valueStr);
-          } else if (!key.equalsIgnoreCase("x509crl")) {
-            String valueStr = node.get(key, null);
-            if (valueStr != null)
-              SecurityClient.x509crl.setParam(valueStr);
           } else
             continue;
         }