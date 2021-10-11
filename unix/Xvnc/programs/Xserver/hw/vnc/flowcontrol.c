TurboVNC/turbovnc

commit a0f5670ecc42538f95f56ee81a885c6ba32916f1

Server: Refine congestion control algorithms
Based on:
https://github.com/tigervnc/tigervnc/commit/a99d14d1939cb2338b6268d9aebe3850df66daed
https://github.com/tigervnc/tigervnc/commit/da8904cb0a6867ce676a4b60334ce2be3746cf60
https://github.com/tigervnc/tigervnc/commit/c05fba745ee42661b54bb4454b270e43232c1bcc
https://github.com/tigervnc/tigervnc/commit/95c46d8b7b03b1e0fce5169e78474cb2163ccde4

unix/Xvnc/programs/Xserver/hw/vnc/flowcontrol.c
@@ -5,6 +5,7 @@
 /*
  *  Copyright (C) 2012, 2014, 2017-2018, 2021 D. R. Commander.
  *                                            All Rights Reserved.
+ *  Copyright (C) 2018 Peter Åstrand for Cendio AB.  All Rights Reserved.
  *  Copyright (C) 2011, 2015 Pierre Ossman for Cendio AB.  All Rights Reserved.
  *
  *  This is free software; you can redistribute it and/or modify
@@ -31,7 +32,13 @@
  * The basic principle is that described in RFC 5681 (TCP Congestion Control),
  * with the addition of using the TCP Vegas algorithm.  The reason we use Vegas
  * is that we run on top of a reliable transport, so we need a latency-based
- * algorithm rather than a loss-based one.
+ * algorithm rather than a loss-based one.  There is also a lot of
+ * interpolation in our algorithm, because our measurements have poor
+ * granularity.
+ *
+ * We use a simplistic form of slow start in order to ramp up quickly from an
+ * idle state.  We do not have any persistent threshold, though, as there is
+ * too much noise for it to be reliable.
  */
 
 #include "rfb.h"
@@ -46,40 +53,124 @@
    low. */
 static const unsigned INITIAL_WINDOW = 16384;
 
-/* TCP's minimal window is 3 * MSS, but since we don't know the MSS, we
-   make a guess at 4 KB (it's probaly a bit higher). */
+/* TCP's minimum window is 3 * MSS, but since we don't know the MSS, we
+   make a guess at 4 KB (it's probably a bit higher.) */
 static const unsigned MINIMUM_WINDOW = 4096;
 
-/* The current default maximum window for Linux (4 MB).  Should be a good
+/* The current default maximum window for Linux (4 MB.)  This should be a good
    limit for now... */
 static const unsigned MAXIMUM_WINDOW = 4194304;
 
 
-static void HandleRTTPong(rfbClientPtr);
+static int GetUncongestedETA(rfbClientPtr);
+static unsigned GetExtraBuffer(rfbClientPtr);
+static unsigned GetInFlight(rfbClientPtr);
 static CARD32 congestionCallback(OsTimerPtr, CARD32, pointer);
 static void UpdateCongestion(rfbClientPtr);
 
 
+static time_t msBetween(const struct timeval *first,
+                        const struct timeval *second)
+{
+  time_t diff;
+
+  diff = (second->tv_sec - first->tv_sec) * 1000;
+
+  diff += second->tv_usec / 1000;
+  diff -= first->tv_usec / 1000;
+
+  return diff;
+}
+
 static time_t msSince(const struct timeval *then)
 {
   struct timeval now;
-  time_t diff;
 
   gettimeofday(&now, NULL);
 
-  diff = (now.tv_sec - then->tv_sec) * 1000;
+  return msBetween(then, &now);
+}
 
-  diff += now.tv_usec / 1000;
-  diff -= then->tv_usec / 1000;
+static Bool isBefore(const struct timeval *first, const struct timeval *second)
+{
+  if (first->tv_sec < second->tv_sec)
+    return TRUE;
+  if (first->tv_sec > second->tv_sec)
+    return FALSE;
+  if (first->tv_usec < second->tv_usec)
+    return TRUE;
+  return FALSE;
+}
 
-  return diff;
+/* Compare two positions, even if integer wraparound has occurred. */
+static _X_INLINE Bool isAfter(unsigned a, unsigned b)
+{
+  return a != b && a - b <= UINT_MAX / 2;
 }
 
 
 void rfbInitFlowControl(rfbClientPtr cl)
 {
-  cl->ackedOffset = cl->sockOffset;
   cl->congWindow = INITIAL_WINDOW;
+  cl->inSlowStart = TRUE;
+  gettimeofday(&cl->lastUpdate, NULL);
+  gettimeofday(&cl->lastSent, NULL);
+  gettimeofday(&cl->lastPongArrival, NULL);
+  gettimeofday(&cl->lastAdjustment, NULL);
+}
+
+
+/*
+ * rfbUpdatePosition() registers the current stream position.  It can and
+ * should be called often.
+ */
+
+void rfbUpdatePosition(rfbClientPtr cl, unsigned pos)
+{
+  struct timeval now;
+  unsigned delta, consumed;
+
+  gettimeofday(&now, NULL);
+
+  delta = pos - cl->lastPosition;
+  if ((delta > 0) || (cl->extraBuffer > 0))
+    cl->lastSent = now;
+
+  /* Idle for too long?
+     We use a very crude RTO calculation in order to keep things simple.
+
+     FIXME: Implement RFC 2861. */
+  if (msBetween(&cl->lastSent, &now) > max(cl->baseRTT * 2, 100)) {
+
+#ifdef CONGESTION_DEBUG
+    rfbLog("Connection idle for %d ms.  Resetting congestion control.\n",
+           msBetween(&cl->lastSent, &now));
+#endif
+
+    /* Close congestion window and redo wire latency measurement. */
+    cl->congWindow = min(INITIAL_WINDOW, cl->congWindow);
+    cl->baseRTT = (unsigned)-1;
+    cl->measurements = 0;
+    gettimeofday(&cl->lastAdjustment, NULL);
+    cl->minRTT = cl->minCongestedRTT = (unsigned)-1;
+    cl->inSlowStart = TRUE;
+  }
+
+  /* Commonly we will be in a state of overbuffering.  We need to estimate the
+     extra delay that this causes, so we can separate it from the delay caused
+     by an incorrect congestion window.  (We cannot do this until we have a RTT
+     measurement, though.) */
+  if (cl->baseRTT != (unsigned)-1) {
+    cl->extraBuffer += delta;
+    consumed = msBetween(&cl->lastUpdate, &now) * cl->congWindow / cl->baseRTT;
+    if (cl->extraBuffer < consumed)
+      cl->extraBuffer = 0;
+    else
+      cl->extraBuffer -= consumed;
+  }
+
+  cl->lastPosition = pos;
+  cl->lastUpdate = now;
 }
 
 
@@ -90,6 +181,8 @@ Bool rfbSendRTTPing(rfbClientPtr cl)
 
   if (!cl->enableFence)
     return TRUE;
+
+  rfbUpdatePosition(cl, cl->sockOffset);
 
   /* We need to make sure that any old updates are already processed by the
      time we get the response back.  This allows us to reliably throttle
@@ -102,125 +195,256 @@ Bool rfbSendRTTPing(rfbClientPtr cl)
   rttInfo = rfbAlloc0(sizeof(struct RTTInfo));
 
   gettimeofday(&rttInfo->tv, NULL);
-  rttInfo->offset = cl->sockOffset;
-  rttInfo->inFlight = rttInfo->offset - cl->ackedOffset;
+  rttInfo->pos = cl->lastPosition;
+  rttInfo->extra = GetExtraBuffer(cl);
+  rttInfo->congested = rfbIsCongested(cl);
 
   xorg_list_append(&rttInfo->entry, &cl->pings);
 
-  cl->sentOffset = rttInfo->offset;
-
-  /* Let some data flow before we adjust the settings */
-  if (!cl->congestionTimerRunning) {
-    cl->congestionTimer = TimerSet(cl->congestionTimer, 0,
-                                   min(cl->baseRTT * 2, 100),
-                                   congestionCallback, cl);
-    cl->congestionTimerRunning = TRUE;
-  }
   return TRUE;
 }
 
 
 static void HandleRTTPong(rfbClientPtr cl)
 {
+  struct timeval now;
   struct RTTInfo *rttInfo;
   unsigned rtt, delay;
 
   if (xorg_list_is_empty(&cl->pings))
     return;
 
+  gettimeofday(&now, NULL);
+
   rttInfo = xorg_list_first_entry(&cl->pings, struct RTTInfo, entry);
   xorg_list_del(&rttInfo->entry);
 
-  rtt = msSince(&rttInfo->tv);
+  cl->lastPong = *rttInfo;
+  cl->lastPongArrival = now;
+
+  rtt = msBetween(&rttInfo->tv, &now);
   if (rtt < 1)
     rtt = 1;
 
-  cl->ackedOffset = rttInfo->offset;
-
-  /* Try to estimate wire latency by tracking lowest latency seen */
+  /* Try to estimate wire latency by tracking the lowest observed latency. */
   if (rtt < cl->baseRTT)
     cl->baseRTT = rtt;
 
-  if (rttInfo->inFlight > cl->congWindow) {
-    cl->seenCongestion = TRUE;
+  /* Pings sent before the last adjustment aren't interesting, as they aren't a
+     measure of the current congestion window. */
+  if (isBefore(&rttInfo->tv, &cl->lastAdjustment))
+    return;
 
-    /* Estimate added delay because of overtaxed buffers */
-    delay = (rttInfo->inFlight - cl->congWindow) *
-            cl->baseRTT / cl->congWindow;
+  /* Estimate added delay because of overtaxed buffers (see above.) */
+  delay = rttInfo->extra * cl->baseRTT / cl->congWindow;
+  if (delay < rtt)
+    rtt -= delay;
+  else
+    rtt = 1;
 
-    if (delay < rtt)
-      rtt -= delay;
-    else
-      rtt = 1;
+  /* An observed latency less than the wire latency means that we've
+     understimated the congestion window.  We can't really determine by how
+     much, though, so we pretend that we observed no buffer latency at all. */
+  if (rtt < cl->baseRTT)
+    rtt = cl->baseRTT;
 
-    /* If we underestimate the congestion window, then we'll get a latency
-       that's less than the wire latency, which will confuse other portions
-       of the code. */
-    if (rtt < cl->baseRTT)
-      rtt = cl->baseRTT;
-  }
+  /* Record the minimum observed delay (hopefully ignoring jitter), and let the
+     congestion control algorithm do its thing.
 
-  /* We only keep track of the minimum latency seen (for a given interval)
-     because we want to avoid issuing buffers continously, but we don't mind
-     (and even approve of) bursts. */
+     NOTE: Our algorithm is delay-based rather than loss-based, which means
+     that we need to look at pongs even if they weren't limited by the current
+     window ("congested").  Otherwise we will fail to detect increasing
+     congestion until the application exceeds the congestion window. */
   if (rtt < cl->minRTT)
     cl->minRTT = rtt;
+  if (rttInfo->congested) {
+    if (rtt < cl->minCongestedRTT)
+      cl->minCongestedRTT = rtt;
+  }
+
+  cl->measurements++;
+  UpdateCongestion(cl);
 
   free(rttInfo);
 }
 
 
+/*
+ * rfbIsCongested() determines if the transport is currently congested or if
+ * more data can be sent.
+ */
+
 Bool rfbIsCongested(rfbClientPtr cl)
 {
-  int offset, count = 0;  time_t sockIdleTime;
-  struct RTTInfo *rttInfo;
+  int eta;
 
   if (!cl->enableFence)
     return FALSE;
 
-  sockIdleTime = msSince(&cl->lastWrite);
+  TimerCancel(cl->congestionTimer);
 
-  /* Idle for too long? (and no data on the wire)
-
-     FIXME: This should really just be one baseRTT, but we're getting
-            problems with triggering the idle timeout on each update.
-            Maybe we need to use a moving average for the wire latency
-            instead of baseRTT. */
-  if ((cl->sentOffset == cl->ackedOffset) &&
-      (sockIdleTime > 2 * cl->baseRTT)) {
-
-#ifdef CONGESTION_DEBUG
-    if (cl->congWindow > INITIAL_WINDOW)
-      rfbLog("Reverting to initial window (%d KB) after %d ms\n",
-             INITIAL_WINDOW / 1024, sockIdleTime);
-#endif
-
-    /* Close congestion window and allow a transfer
-       FIXME: Reset baseRTT like Linux Vegas? */
-    cl->congWindow = min(INITIAL_WINDOW, cl->congWindow);
-    return FALSE;
-  }
-
-  offset = cl->sockOffset;
-
-  /* FIXME: Should we compensate for non-update data?
-            (i.e. use sentOffset instead of offset) */
-  if ((offset - cl->ackedOffset) < cl->congWindow)
+  rfbUpdatePosition(cl, cl->sockOffset);
+  if (GetInFlight(cl) < cl->congWindow)
     return FALSE;
 
-  /* If we just have one outstanding "ping", then that means the client has
-     started receiving our update.  In order to provide backward
-     compatibility with viewers that don't support the flow control
-     extensions, we allow another update here.  This could further clog up
-     the tubes, but if we reach this point, congestion control isn't really
-     working properly anyhow, as the wire would otherwise be idle for at
-     least RTT/2. */
-  xorg_list_for_each_entry(rttInfo, &cl->pings, entry)
-    count++;
-  if (count == 1)
-    return FALSE;
+  eta = GetUncongestedETA(cl);
+  if (eta >= 0)
+    cl->congestionTimer = TimerSet(cl->congestionTimer, 0, eta,
+                                   congestionCallback, cl);
 
   return TRUE;
+}
+
+
+/*
+ * GetUncongestedETA() estimates the number of milliseconds until the transport
+ * will no longer be congested.  It returns 0 if there is no congestion and -1
+ * if it is unknown when the transport will no longer be congested.
+ */
+
+static int GetUncongestedETA(rfbClientPtr cl)
+{
+  unsigned targetAcked;
+
+  const struct RTTInfo *prevPing;
+  unsigned eta, elapsed;
+  unsigned etaNext, delay;
+
+  struct RTTInfo *iter;
+
+  targetAcked = cl->lastPosition - cl->congWindow;
+
+  /* Simple case? */
+  if (isAfter(cl->lastPong.pos, targetAcked))
+    return 0;
+
+  /* No measurements yet? */
+  if (cl->baseRTT == (unsigned)-1)
+    return -1;
+
+  prevPing = &cl->lastPong;
+  eta = 0;
+  elapsed = msSince(&cl->lastPongArrival);
+
+  /* Walk the ping queue and figure out which ping we are waiting for in order
+     to get to an uncongested state. */
+  xorg_list_for_each_entry(iter, &cl->pings, entry) {
+    struct RTTInfo curPing;
+
+    /* If we aren't waiting for a pong that will clear the congested state,
+       then we have to estimate the final bit by pretending that we had a ping
+       just after the last position update. */
+    if (iter == xorg_list_last_entry(&cl->pings, struct RTTInfo, entry)) {
+      curPing.tv = cl->lastUpdate;
+      curPing.pos = cl->lastPosition;
+      curPing.extra = cl->extraBuffer;
+    } else {
+      curPing = *iter;
+    }
+
+    etaNext = msBetween(&prevPing->tv, &curPing.tv);
+    /* Compensate for buffering delays. */
+    delay = curPing.extra * cl->baseRTT / cl->congWindow;
+    etaNext += delay;
+    delay = prevPing->extra * cl->baseRTT / cl->congWindow;
+    if (delay >= etaNext)
+      etaNext = 0;
+    else
+      etaNext -= delay;
+
+    /* Found it? */
+    if (isAfter(curPing.pos, targetAcked)) {
+      eta += etaNext * (curPing.pos - targetAcked) /
+                       (curPing.pos - prevPing->pos);
+      if (elapsed > eta)
+        return 0;
+      else
+        return eta - elapsed;
+    }
+
+    eta += etaNext;
+    prevPing = &*iter;
+  }
+
+  return -1;
+}
+
+
+static unsigned GetExtraBuffer(rfbClientPtr cl)
+{
+  unsigned elapsed;
+  unsigned consumed;
+
+  if (cl->baseRTT == (unsigned)-1)
+    return 0;
+
+  elapsed = msSince(&cl->lastUpdate);
+  consumed = elapsed * cl->congWindow / cl->baseRTT;
+
+  if (consumed >= cl->extraBuffer)
+    return 0;
+  else
+    return cl->extraBuffer - consumed;
+}
+
+
+static unsigned GetInFlight(rfbClientPtr cl)
+{
+  struct RTTInfo nextPong;
+  unsigned etaNext, delay, elapsed, acked;
+
+  /* Simple case? */
+  if (cl->lastPosition == cl->lastPong.pos)
+    return 0;
+
+  /* No measurements yet? */
+  if (cl->baseRTT == (unsigned)-1) {
+    if (!xorg_list_is_empty(&cl->pings)) {
+      struct RTTInfo *rttInfo =
+        xorg_list_first_entry(&cl->pings, struct RTTInfo, entry);
+      return cl->lastPosition - rttInfo->pos;
+    }
+    return 0;
+  }
+
+  /* If we aren't waiting for a pong, then we have to estimate things by
+     pretending that we had a ping just after the last position update. */
+  if (xorg_list_is_empty(&cl->pings)) {
+    nextPong.tv = cl->lastUpdate;
+    nextPong.pos = cl->lastPosition;
+    nextPong.extra = cl->extraBuffer;
+  } else {
+    struct RTTInfo *rttInfo =
+      xorg_list_first_entry(&cl->pings, struct RTTInfo, entry);
+    nextPong = *rttInfo;
+  }
+
+  /* First, we need to estimate how many bytes have made it through completely.
+     To do this, we look at the next ping that should arrive, figure out how
+     far behind it should be, and interpolate the positions. */
+
+  etaNext = msBetween(&cl->lastPong.tv, &nextPong.tv);
+  /* Compensate for buffering delays. */
+  delay = nextPong.extra * cl->baseRTT / cl->congWindow;
+  etaNext += delay;
+  delay = cl->lastPong.extra * cl->baseRTT / cl->congWindow;
+  if (delay >= etaNext)
+    etaNext = 0;
+  else
+    etaNext -= delay;
+
+  elapsed = msSince(&cl->lastPongArrival);
+
+  /* The pong should be here very soon.  Be optimistic and assume we can
+     already use its value. */
+  if (etaNext <= elapsed)
+    acked = nextPong.pos;
+  else {
+    acked = cl->lastPong.pos;
+    acked += (nextPong.pos - cl->lastPong.pos) * elapsed / etaNext;
+  }
+
+  return cl->lastPosition - acked;
 }
 
 
@@ -228,8 +452,7 @@ static CARD32 congestionCallback(OsTimerPtr timer, CARD32 time, pointer arg)
 {
   rfbClientPtr cl = (rfbClientPtr)arg;
 
-  UpdateCongestion(cl);
-  cl->congestionTimerRunning = FALSE;
+  rfbSendFramebufferUpdate(cl);
   return 0;
 }
 
@@ -242,7 +465,8 @@ static void UpdateCongestion(rfbClientPtr cl)
   socklen_t tcp_info_length;
 #endif
 
-  if (!cl->seenCongestion)
+  /* In order to avoid noise, we want at least three measurements. */
+  if (cl->measurements < 3)
     return;
 
   /* The goal is to have a congestion window that is slightly too large, since
@@ -251,18 +475,54 @@ static void UpdateCongestion(rfbClientPtr cl)
 
   diff = cl->minRTT - cl->baseRTT;
 
-  if (diff > min(100, cl->baseRTT)) {
-    /* Way too fast */
+  if (diff > max(100, cl->baseRTT / 2)) {
+    /* We have no way of detecting loss, so assume that a massive latency spike
+       means packet loss.  Adjust the window and go directly to congestion
+       avoidance. */
+#ifdef CONGESTION_DEBUG
+    rfbLog("Latency spike!  Backing off...\n");
+#endif
     cl->congWindow = cl->congWindow * cl->baseRTT / cl->minRTT;
-  } else if (diff > min(50, cl->baseRTT / 2)) {
-    /* Slightly too fast */
-    cl->congWindow -= 4096;
-  } else if (diff < 5) {
-    /* Way too slow */
-    cl->congWindow += 8192;
-  } else if (diff < 25) {
-    /* Too slow */
-    cl->congWindow += 4096;
+    cl->inSlowStart = FALSE;
+  }
+
+  if (cl->inSlowStart) {
+    /* Slow start-- aggressive growth until we see congestion */
+
+    if (diff > 25) {
+      /* If we observe increased latency, then we assume we've hit the limit
+         and it's time to leave slow start and switch to congestion
+         avoidance. */
+      cl->congWindow = cl->congWindow * cl->baseRTT / cl->minRTT;
+      cl->inSlowStart = FALSE;
+    } else {
+      /* It's not safe to increase the congestion window unless we actually
+         used all of it, so we look at minCongestedRTT and not minRTT. */
+
+      diff = cl->minCongestedRTT - cl->baseRTT;
+      if (diff < 25)
+        cl->congWindow *= 2;
+    }
+  } else {
+    /* Congestion avoidance (VEGAS) */
+
+    if (diff > 50) {
+      /* Slightly too fast */
+      cl->congWindow -= 4096;
+    } else {
+      /* Only the "congested" pongs are checked to see if the window is too
+         small. */
+
+      diff = cl->minCongestedRTT - cl->baseRTT;
+
+      if (diff < 5) {
+        /* Way too slow */
+        cl->congWindow += 8192;
+      } else if (diff < 25) {
+        /* Too slow */
+        cl->congWindow += 4096;
+      }
+    }
   }
 
   if (cl->congWindow < MINIMUM_WINDOW)
@@ -271,9 +531,10 @@ static void UpdateCongestion(rfbClientPtr cl)
     cl->congWindow = MAXIMUM_WINDOW;
 
 #ifdef CONGESTION_DEBUG
-  rfbLog("RTT: %d ms (%d ms), Window: %d KB, Offset: %d KB, Bandwidth: %g Mbps\n",
-         cl->minRTT, cl->baseRTT, cl->congWindow / 1024, cl->sockOffset / 1024,
-         cl->congWindow * 8.0 / cl->baseRTT / 1000.0);
+  rfbLog("RTT: %d/%d ms (%d ms), Window: %d KB, Offset: %d KB, Bandwidth: %g Mbps%s\n",
+         cl->minRTT, cl->minCongestedRTT, cl->baseRTT, cl->congWindow / 1024,
+         cl->sockOffset / 1024, cl->congWindow * 8.0 / cl->baseRTT / 1000.0,
+         cl->inSlowStart ? " (slow start)" : "");
 
 #ifdef TCP_INFO
   tcp_info_length = sizeof(tcp_info);
@@ -287,8 +548,9 @@ static void UpdateCongestion(rfbClientPtr cl)
 
 #endif
 
-  cl->minRTT = -1;
-  cl->seenCongestion = FALSE;
+  cl->measurements = 0;
+  gettimeofday(&cl->lastAdjustment, NULL);
+  cl->minRTT = cl->minCongestedRTT = (unsigned)-1;
 }
 
 
@@ -324,10 +586,12 @@ Bool rfbSendFence(rfbClientPtr cl, CARD32 flags, unsigned len,
     return FALSE;
   }
 
-  if (WriteExact(cl, (char *)data, len) < 0) {
-    rfbLogPerror("rfbSendFence: write");
-    rfbCloseClient(cl);
-    return FALSE;
+  if (len > 0) {
+    if (WriteExact(cl, (char *)data, len) < 0) {
+      rfbLogPerror("rfbSendFence: write");
+      rfbCloseClient(cl);
+      return FALSE;
+    }
   }
   return TRUE;
 }

unix/Xvnc/programs/Xserver/hw/vnc/rfb.h
@@ -4,6 +4,7 @@
 
 /*
  *  Copyright (C) 2010-2021 D. R. Commander.  All Rights Reserved.
+ *  Copyright (C) 2011, 2015 Pierre Ossman for Cendio AB.  All Rights Reserved.
  *  Copyright (C) 2011 Gernot Tenchio
  *  Copyright (C) 2011 Joel Martin
  *  Copyright (C) 2010 University Corporation for Atmospheric Research.
@@ -200,8 +201,8 @@ typedef struct {
 
 struct RTTInfo {
   struct timeval tv;
-  int offset;
-  unsigned inFlight;
+  unsigned pos, extra;
+  char congested;
   struct xorg_list entry;
 };
 
@@ -439,16 +440,18 @@ typedef struct rfbClientRec {
   unsigned fenceDataLen;
   char fenceData[64];
 
-  unsigned baseRTT;
-  unsigned congWindow;
-  int ackedOffset, sentOffset, sockOffset;
-  unsigned minRTT;
-  Bool seenCongestion;
+  unsigned lastPosition, extraBuffer;
+  struct timeval lastUpdate, lastSent;
+  unsigned baseRTT, congWindow;
+  Bool inSlowStart;
+  int sockOffset;
   struct xorg_list pings;
-  OsTimerPtr updateTimer;
   OsTimerPtr congestionTimer;
-  Bool congestionTimerRunning;
-  struct timeval lastWrite;
+  struct RTTInfo lastPong;
+  struct timeval lastPongArrival;
+  int measurements;
+  struct timeval lastAdjustment;
+  unsigned minRTT, minCongestedRTT;
 
   Bool pendingDesktopResize, pendingExtDesktopResize;
   int reason, result;
@@ -736,6 +739,7 @@ extern RegionPtr rfbRestoreAreas(WindowPtr, RegionPtr);
 /* flowcontrol.c */
 
 extern void rfbInitFlowControl(rfbClientPtr cl);
+extern void rfbUpdatePosition(rfbClientPtr cl, unsigned pos);
 extern Bool rfbSendRTTPing(rfbClientPtr cl);
 extern Bool rfbIsCongested(rfbClientPtr cl);
 extern Bool rfbSendFence(rfbClientPtr cl, CARD32 flags, unsigned len,

unix/Xvnc/programs/Xserver/hw/vnc/rfbserver.c
@@ -236,15 +236,6 @@ static CARD32 alrCallback(OsTimerPtr timer, CARD32 time, pointer arg)
 }
 
 
-static CARD32 updateCallback(OsTimerPtr timer, CARD32 time, pointer arg)
-{
-  rfbClientPtr cl = (rfbClientPtr)arg;
-
-  rfbSendFramebufferUpdate(cl);
-  return 0;
-}
-
-
 /*
  * Interframe comparison
  */
@@ -494,8 +485,7 @@ static rfbClientPtr rfbNewClient(int sock)
   if (rfbIdleTimeout > 0)
     IdleTimerCancel();
 
-  cl->baseRTT = cl->minRTT = (unsigned)-1;
-  gettimeofday(&cl->lastWrite, NULL);
+  cl->baseRTT = cl->minRTT = cl->minCongestedRTT = (unsigned)-1;
   REGION_INIT(pScreen, &cl->cuRegion, NullBox, 0);
   xorg_list_init(&cl->pings);
 
@@ -530,7 +520,6 @@ void rfbClientConnectionGone(rfbClientPtr cl)
 
   TimerFree(cl->alrTimer);
   TimerFree(cl->deferredUpdateTimer);
-  TimerFree(cl->updateTimer);
   TimerFree(cl->congestionTimer);
 
 #ifdef XVNC_AuthPAM
@@ -1819,7 +1808,7 @@ Bool rfbSendFramebufferUpdate(rfbClientPtr cl)
   Bool redundantUpdate = FALSE;
   double tUpdateStart = 0.0;
 
-  TimerCancel(cl->updateTimer);
+  rfbUpdatePosition(cl, cl->sockOffset);
 
   /*
    * We're in the middle of processing a command that's supposed to be
@@ -1839,10 +1828,8 @@ Bool rfbSendFramebufferUpdate(rfbClientPtr cl)
   /* Check that we actually have some space on the link and retry in a
      bit if things are congested. */
 
-  if (rfbCongestionControl && rfbIsCongested(cl)) {
-    cl->updateTimer = TimerSet(cl->updateTimer, 0, 50, updateCallback, cl);
+  if (rfbCongestionControl && rfbIsCongested(cl))
     return TRUE;
-  }
 
   /* In continuous mode, we will be outputting at least three distinct
      messages.  We need to aggregate these in order to not clog up TCP's
@@ -2283,6 +2270,7 @@ Bool rfbSendFramebufferUpdate(rfbClientPtr cl)
   }
 
   rfbUncorkSock(cl->sock);
+  rfbUpdatePosition(cl, cl->sockOffset);
   return TRUE;
 
   abort:

unix/Xvnc/programs/Xserver/hw/vnc/sockets.c
@@ -590,7 +590,6 @@ int WriteExact(rfbClientPtr cl, char *buf, int len)
     }
   }
 
-  gettimeofday(&cl->lastWrite, NULL);
   cl->sockOffset += bytesWritten;
 
   return 1;

commit ffdb526ab850352f1f759c7e84cb0ceba6c4db91

Server: Fix porting error in cong control overhaul
(introduced in a0f5670ecc42538f95f56ee81a885c6ba32916f1)

unix/Xvnc/programs/Xserver/hw/vnc/flowcontrol.c
@@ -62,6 +62,7 @@ static const unsigned MINIMUM_WINDOW = 4096;
 static const unsigned MAXIMUM_WINDOW = 4194304;
 
 
+static Bool IsCongested(rfbClientPtr);
 static int GetUncongestedETA(rfbClientPtr);
 static unsigned GetExtraBuffer(rfbClientPtr);
 static unsigned GetInFlight(rfbClientPtr);
@@ -197,7 +198,7 @@ Bool rfbSendRTTPing(rfbClientPtr cl)
   gettimeofday(&rttInfo->tv, NULL);
   rttInfo->pos = cl->lastPosition;
   rttInfo->extra = GetExtraBuffer(cl);
-  rttInfo->congested = rfbIsCongested(cl);
+  rttInfo->congested = IsCongested(cl);
 
   xorg_list_append(&rttInfo->entry, &cl->pings);
 
@@ -269,6 +270,15 @@ static void HandleRTTPong(rfbClientPtr cl)
 }
 
 
+static Bool IsCongested(rfbClientPtr cl)
+{
+  if (GetInFlight(cl) < cl->congWindow)
+    return FALSE;
+
+  return TRUE;
+}
+
+
 /*
  * rfbIsCongested() determines if the transport is currently congested or if
  * more data can be sent.
@@ -284,7 +294,7 @@ Bool rfbIsCongested(rfbClientPtr cl)
   TimerCancel(cl->congestionTimer);
 
   rfbUpdatePosition(cl, cl->sockOffset);
-  if (GetInFlight(cl) < cl->congWindow)
+  if (!IsCongested(cl))
     return FALSE;
 
   eta = GetUncongestedETA(cl);