TurboVNC/turbovnc

commit 8032b4e750fb448492bc4fecb7047302d9473983

Viewer: Overhaul TurboVNC Viewer Options dialog
In addition to improving the usability of the viewer, this also lays the
groundwork for specifying a different set of options for each host.

- Move the contents of the Security class into the Options class so
  that the Options class encapsulates all options that can be configured
  using the GUI.
- Refactor OptionsDialog.updatePreferences() as Options.save() so that
  any Options instance can be saved on command.  Default options are now
  saved after the options are retrieved from the OptionsDialog class,
  not before.
- Move UserPreferences from the com.turbovnc.vncviewer package into the
  com.turbovnc.rfb package so that the Options class can access it.
- In response to the NoUnixLogin parameter and the User and
  SendLocalUsername options, apply restrictions to the available
  security types in the Options class rather than in the CConn class.
- Consolidate the setting and getting of options in OptionsDialog into
  two new methods, OptionsDialog.setOptions()/getOptions(), which
  subsume OptionsDialog.setSecurityOptions()/getSecurityOptions().
  The OptionsDialogCallback.setOptions()/getOptions() interface is still
  used, but its implementations in CConn and VncViewer now invoke
  OptionsDialog.setOptions/getOptions().
- Make all components in OptionsDialog private, so they can only be
  set/get using OptionsDialog.setOptions()/getOptions().
- Redesign the Security tab in the TurboVNC Viewer Options dialog so
  that each individual security type has its own check box.  This fixes
  an issue whereby certain combinations of security types (e.g.
  Vnc,TLSPlain) could not be specified using the dialog.
- Add a text field to the Security tab of the TurboVNC Viewer Options
  dialog that allows the username to be specified.
- Automatically disable settings in the Security tab of the TurboVNC
  Viewer Options dialog when they are rendered irrelevant by other
  settings.

java/com/turbovnc/vncviewer/VncViewer.java
@@ -672,124 +672,13 @@ public class VncViewer implements Runnable, OptionsDialogCallback {
   }
 
   public void setOptions() {
-    options.allowJpeg.setSelected(opts.allowJpeg);
-    options.subsamplingLevel.setValue(opts.getSubsamplingOrdinal());
-    options.jpegQualityLevel.setValue(opts.quality);
-    options.setCompressionLevel(opts.compressLevel);
-
+    options.setOptions(opts, true, true, true, true);
     setTightOptions();
-
-    options.viewOnly.setSelected(opts.viewOnly);
-    options.reverseScroll.setSelected(opts.reverseScroll);
-    options.fsAltEnter.setSelected(opts.fsAltEnter);
-    options.recvClipboard.setSelected(opts.recvClipboard);
-    options.sendClipboard.setSelected(opts.sendClipboard);
-    options.menuKey.setSelectedItem(KeyEvent.getKeyText(opts.menuKeyCode));
-    if (Utils.osGrab() && Helper.isAvailable())
-      options.grabKeyboard.setSelectedIndex(opts.grabKeyboard);
-
-    options.shared.setSelected(opts.shared);
-    options.sendLocalUsername.setSelected(opts.sendLocalUsername);
-    SecurityClient security = new SecurityClient();
-    options.setSecurityOptions();
-
-    options.fullScreen.setSelected(opts.fullScreen);
-    options.span.setSelectedIndex(opts.span);
-    options.cursorShape.setSelected(opts.cursorShape);
-    options.acceptBell.setSelected(opts.acceptBell);
-    options.showToolbar.setSelected(opts.showToolbar);
-    if (opts.scalingFactor == Options.SCALE_AUTO) {
-      options.scalingFactor.setSelectedItem("Auto");
-    } else if (opts.scalingFactor == Options.SCALE_FIXEDRATIO) {
-      options.scalingFactor.setSelectedItem("Fixed Aspect Ratio");
-    } else {
-      options.scalingFactor.setSelectedItem(opts.scalingFactor + "%");
-    }
-    if (opts.desktopSize.mode == Options.SIZE_AUTO) {
-      options.desktopSize.setSelectedItem("Auto");
-      options.scalingFactor.setEnabled(false);
-    } else if (opts.desktopSize.mode == Options.SIZE_SERVER) {
-      options.desktopSize.setSelectedItem("Server");
-      options.scalingFactor.setEnabled(true);
-    } else {
-      options.desktopSize.setSelectedItem(opts.desktopSize.getString());
-      options.scalingFactor.setEnabled(true);
-    }
-
-    options.shared.setEnabled(false);
-    options.secVeNCrypt.setEnabled(false);
-    options.encNone.setEnabled(false);
-    options.encTLS.setEnabled(false);
-    options.encX509.setEnabled(false);
-    options.x509ca.setEnabled(false);
-    options.x509caButton.setEnabled(false);
-    options.x509caLabel.setEnabled(false);
-    options.x509crl.setEnabled(false);
-    options.x509crlButton.setEnabled(false);
-    options.x509crlLabel.setEnabled(false);
-    options.secIdent.setEnabled(false);
-    options.secNone.setEnabled(false);
-    options.secVnc.setEnabled(false);
-    options.secUnixLogin.setEnabled(false);
-    options.secPlain.setEnabled(false);
-    options.sendLocalUsername.setEnabled(false);
-    options.gateway.setEnabled(false);
-    options.gatewayLabel.setEnabled(false);
-    options.sshUser.setEnabled(false);
-    options.sshUserLabel.setEnabled(false);
-    options.tunnel.setEnabled(false);
-    if (opts.via != null)
-      options.gateway.setText(opts.via);
-    if (opts.sshUser != null)
-      options.sshUser.setText(opts.sshUser);
-    options.tunnel.setSelected(opts.tunnel);
-    if (Params.x509ca.getValue() != null)
-      options.x509ca.setText(Params.x509ca.getValue());
-    if (Params.x509crl.getValue() != null)
-      options.x509crl.setText(Params.x509crl.getValue());
   }
 
   public void getOptions() {
-    opts.allowJpeg = options.allowJpeg.isSelected();
-    opts.quality = options.jpegQualityLevel.getValue();
-    opts.compressLevel = options.getCompressionLevel();
-    opts.subsampling = options.getSubsamplingLevel();
-    opts.sendLocalUsername = options.sendLocalUsername.isSelected();
-    opts.viewOnly = options.viewOnly.isSelected();
-    opts.reverseScroll = options.reverseScroll.isSelected();
-    opts.fsAltEnter = options.fsAltEnter.isSelected();
-    opts.recvClipboard = options.recvClipboard.isSelected();
-    opts.sendClipboard = options.sendClipboard.isSelected();
-    opts.acceptBell = options.acceptBell.isSelected();
-    opts.showToolbar = options.showToolbar.isSelected();
-
-    opts.setScalingFactor(options.scalingFactor.getSelectedItem().toString());
-    opts.setDesktopSize(options.desktopSize.getSelectedItem().toString());
-    opts.fullScreen = options.fullScreen.isSelected();
-
-    int index = options.span.getSelectedIndex();
-    if (index >= 0 && index < Options.NUMSPANOPT)
-      opts.span = index;
-
-    opts.menuKeyCode =
-      MenuKey.getMenuKeySymbols()[options.menuKey.getSelectedIndex()].keycode;
-    opts.menuKeySym =
-      MenuKey.getMenuKeySymbols()[options.menuKey.getSelectedIndex()].keysym;
-
-    if (Utils.osGrab() && Helper.isAvailable())
-      opts.grabKeyboard = options.grabKeyboard.getSelectedIndex();
-
-    opts.shared = options.shared.isSelected();
-    opts.cursorShape = options.cursorShape.isSelected();
-
-    options.getSecurityOptions();
-    String gateway = options.gateway.getText();
-    opts.via = (gateway.isEmpty() ? null : gateway);
-    String sshUser = options.sshUser.getText();
-    opts.sshUser = (sshUser.isEmpty() ? null : sshUser);
-    opts.tunnel = options.tunnel.isSelected();
-    Params.x509ca.setParam(options.x509ca.getText());
-    Params.x509crl.setParam(options.x509crl.getText());
+    options.getOptions(opts);
+    opts.save();
     options = null;
   }
 
@@ -1030,6 +919,7 @@ public class VncViewer implements Runnable, OptionsDialogCallback {
       }
 
       // SECURITY AND AUTHENTICATION OPTIONS
+      opts.setSecurityTypes(Params.secTypes.getValue());
       opts.sendLocalUsername = Params.sendLocalUsername.getValue();
       opts.tunnel = Params.tunnel.getValue();
       if (Params.user.getValue() != null)
@@ -1050,6 +940,11 @@ public class VncViewer implements Runnable, OptionsDialogCallback {
             opts.via = null;
         }
       }
+
+      if (Params.x509ca.getValue() != null)
+        opts.x509ca = new String(Params.x509ca.getValue());
+      if (Params.x509crl.getValue() != null)
+        opts.x509crl = new String(Params.x509crl.getValue());
 
       String s = Params.vncServerName.getValue();
       if (s != null) {

java/com/turbovnc/rfb/CConnection.java
@@ -1,7 +1,7 @@
 /* Copyright (C) 2002-2005 RealVNC Ltd.  All Rights Reserved.
  * Copyright (C) 2011-2012 Brian P. Hinz
- * Copyright (C) 2012, 2014, 2016, 2018, 2020 D. R. Commander.
- *                                            All Rights Reserved.
+ * Copyright (C) 2012, 2014, 2016, 2018, 2020-2021 D. R. Commander.
+ *                                                 All Rights Reserved.
  *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -123,7 +123,7 @@ public abstract class CConnection extends CMsgHandler {
     int secType = RFB.SECTYPE_INVALID;
 
     List<Integer> secTypes = new ArrayList<Integer>();
-    secTypes = Security.getEnabledSecTypes();
+    secTypes = opts.getEnabledSecTypes();
 
     if (cp.isVersion(3, 3)) {
 
@@ -201,7 +201,7 @@ public abstract class CConnection extends CMsgHandler {
     }
 
     state = RFBSTATE_SECURITY;
-    csecurity = security.getCSecurity(secType);
+    csecurity = security.getCSecurity(opts, secType);
     processSecurityMsg();
   }
 
@@ -409,6 +409,9 @@ public abstract class CConnection extends CMsgHandler {
   int serverPort;
   boolean clientSecTypeOrder;
   protected Socket sock;
+  // CHECKSTYLE VisibilityModifier:OFF
+  public Options opts;
+  // CHECKSTYLE VisibilityModifier:ON
 
   static LogWriter vlog = new LogWriter("CConnection");
 }

java/com/turbovnc/rfb/CSecurityRFBTLS.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2002-2005 RealVNC Ltd.  All Rights Reserved.
  * Copyright (C) 2011 Brian P Hinz
- * Copyright (C) 2012, 2016-2018 D. R. Commander.  All Rights Reserved.
+ * Copyright (C) 2012, 2016-2018, 2021 D. R. Commander.  All Rights Reserved.
  *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -49,7 +49,7 @@ public class CSecurityRFBTLS extends CSecurityTLS {
     int secType = RFB.SECTYPE_INVALID;
 
     List<Integer> secTypes = new ArrayList<Integer>();
-    secTypes = Security.getEnabledExtSecTypes();
+    secTypes = cc.opts.getEnabledExtSecTypes();
 
     int nServerSecTypes = is.readU8();
     if (nServerSecTypes == 0)
@@ -91,7 +91,7 @@ public class CSecurityRFBTLS extends CSecurityTLS {
     vlog.debug("Choosing TLS security type " +
                RFB.secTypeName(secType) + "(" + secType + ")");
 
-    csecurity = security.getCSecurity(secType);
+    csecurity = security.getCSecurity(cc.opts, secType);
     return csecurity.processMsg(cc);
   }
 

java/com/turbovnc/rfb/CSecurityTight.java
@@ -1,6 +1,6 @@
 /* Copyright (C) 2012 Brian P. Hinz
- * Copyright (C) 2012, 2015, 2017-2018, 2020 D. R. Commander.
- *                                           All Rights Reserved.
+ * Copyright (C) 2012, 2015, 2017-2018, 2020-2021 D. R. Commander.
+ *                                                All Rights Reserved.
  *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -78,27 +78,28 @@ public class CSecurityTight extends CSecurity {
       is.readBytes(s, 0, 8);
       String signature = new String(s);
 
+      Options opts = cc.opts;
       switch (code) {
         case CODE_NOAUTH:
-          if (!Security.getEnabledSecTypes().contains(RFB.SECTYPE_NONE))
+          if (!opts.getEnabledSecTypes().contains(RFB.SECTYPE_NONE))
             break;
           if (vendor.equals(VENDOR_STDV) && signature.equals(SIG_NOAUTH))
             supportedAuthTypes.add(code);
           break;
         case CODE_VNCAUTH:
-          if (!Security.getEnabledSecTypes().contains(RFB.SECTYPE_VNCAUTH))
+          if (!opts.getEnabledSecTypes().contains(RFB.SECTYPE_VNCAUTH))
             break;
           if (vendor.equals(VENDOR_STDV) && signature.equals(SIG_VNCAUTH))
             supportedAuthTypes.add(code);
           break;
         case CODE_ULGNAUTH:
-          if (!Security.getEnabledTightSecTypes().contains(RFB.SECTYPE_UNIX_LOGIN))
+          if (!opts.getEnabledTightSecTypes().contains(RFB.SECTYPE_UNIX_LOGIN))
             break;
           if (vendor.equals(VENDOR_TGHT) && signature.equals(SIG_ULGNAUTH))
             supportedAuthTypes.add(code);
           break;
         case CODE_VENCRYPT:
-          if (!Security.getEnabledSecTypes().contains(RFB.SECTYPE_VENCRYPT))
+          if (!opts.getEnabledSecTypes().contains(RFB.SECTYPE_VENCRYPT))
             break;
           if (vendor.equals(VENDOR_VENC) && signature.equals(SIG_VENCRYPT))
             supportedAuthTypes.add(code);

java/com/turbovnc/rfb/CSecurityVeNCrypt.java
@@ -3,7 +3,7 @@
  * Copyright (C) 2006 OCCAM Financial Technology
  * Copyright (C) 2010 TigerVNC Team
  * Copyright (C) 2011 Brian P. Hinz
- * Copyright (C) 2012, 2017-2018 D. R. Commander.  All Rights Reserved.
+ * Copyright (C) 2012, 2017-2018, 2021 D. R. Commander.  All Rights Reserved.
  *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -135,7 +135,7 @@ public class CSecurityVeNCrypt extends CSecurity {
         Iterator<Integer> j;
         List<Integer> secTypes = new ArrayList<Integer>();
 
-        secTypes = Security.getEnabledExtSecTypes();
+        secTypes = cc.opts.getEnabledExtSecTypes();
 
         /* Honor server's security type order */
         for (i = 0; i < nAvailableTypes; i++) {
@@ -159,7 +159,7 @@ public class CSecurityVeNCrypt extends CSecurity {
             chosenType == RFB.SECTYPE_VENCRYPT)
           throw new AuthFailureException("No valid VeNCrypt sub-type");
 
-        csecurity = security.getCSecurity(chosenType);
+        csecurity = security.getCSecurity(cc.opts, chosenType);
 
         /* send chosen type to server */
         os.writeU32(chosenType);

java/com/turbovnc/rfb/Options.java
@@ -1,5 +1,8 @@
-/* Copyright (C) 2012-2013, 2015, 2017-2018, 2020 D. R. Commander.
- *                                                All Rights Reserved.
+/* Copyright (C) 2002-2005 RealVNC Ltd.  All Rights Reserved.
+ * Copyright (C) 2010 TigerVNC Team
+ * Copyright (C) 2011 Brian P. Hinz
+ * Copyright (C) 2012-2013, 2015-2018, 2020-2021 D. R. Commander.
+ *                                               All Rights Reserved.
  *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,6 +21,10 @@
  */
 
 package com.turbovnc.rfb;
+
+import java.util.*;
+
+import com.turbovnc.rdr.*;
 
 import com.jcraft.jsch.Session;
 
@@ -79,6 +86,7 @@ public class Options {
     quality = old.quality;
     subsampling = old.subsampling;
 
+    enabledSecTypes = new ArrayList<Integer>(old.enabledSecTypes);
     sendLocalUsername = old.sendLocalUsername;
     sshSession = old.sshSession;
     sshTunnelActive = old.sshTunnelActive;
@@ -86,6 +94,8 @@ public class Options {
     tunnel = old.tunnel;
     if (old.user != null) user = new String(old.user);
     if (old.via != null) via = new String(old.via);
+    if (old.x509ca != null) x509ca = new String(old.x509ca);
+    if (old.x509crl != null) x509crl = new String(old.x509crl);
   }
 
   public static int parseScalingFactor(String scaleString) {
@@ -181,6 +191,86 @@ public class Options {
     return 0;
   }
 
+  public void save() {
+    // CONNECTION OPTIONS
+    UserPreferences.set("global", "RecvClipboard", recvClipboard);
+    UserPreferences.set("global", "SendClipboard", sendClipboard);
+    UserPreferences.set("global", "Shared", shared);
+
+    // INPUT OPTIONS
+    UserPreferences.set("global", "FSAltEnter", fsAltEnter);
+    if (Utils.osGrab()) {
+      if (grabKeyboard == GRAB_ALWAYS)
+        UserPreferences.set("global", "GrabKeyboard", "Always");
+      else if (grabKeyboard == GRAB_MANUAL)
+        UserPreferences.set("global", "GrabKeyboard", "Manual");
+      else
+        UserPreferences.set("global", "GrabKeyboard", "FS");
+    }
+    for (MenuKey.MenuKeySymbol mks : MenuKey.getMenuKeySymbols()) {
+      if (mks.keycode == menuKeyCode && mks.keysym == menuKeySym)
+        UserPreferences.set("global", "MenuKey", mks.name);
+    }
+    UserPreferences.set("global", "ReverseScroll", reverseScroll);
+    UserPreferences.set("global", "ViewOnly", viewOnly);
+
+    // DISPLAY OPTIONS
+    UserPreferences.set("global", "AcceptBell", acceptBell);
+    UserPreferences.set("global", "CursorShape", cursorShape);
+    UserPreferences.set("global", "DesktopSize", desktopSize.getString());
+    UserPreferences.set("global", "FullScreen", fullScreen);
+
+    if (scalingFactor == SCALE_AUTO)
+      UserPreferences.set("global", "Scale", "Auto");
+    else if (scalingFactor == SCALE_FIXEDRATIO)
+      UserPreferences.set("global", "Scale", "FixedRatio");
+    else
+      UserPreferences.set("global", "Scale", scalingFactor);
+
+    if (span == SPAN_PRIMARY)
+      UserPreferences.set("global", "Span", "Primary");
+    else if (span == SPAN_ALL)
+      UserPreferences.set("global", "Span", "All");
+    else
+      UserPreferences.set("global", "Span", "Auto");
+
+    UserPreferences.set("global", "Toolbar", showToolbar);
+
+    // ENCODING OPTIONS
+    UserPreferences.set("global", "CompressLevel", compressLevel);
+    UserPreferences.set("global", "JPEG", allowJpeg);
+    UserPreferences.set("global", "Quality", quality);
+
+    if (subsampling == SUBSAMP_2X)
+      UserPreferences.set("global", "Subsampling", "2X");
+    else if (subsampling == SUBSAMP_4X)
+      UserPreferences.set("global", "Subsampling", "4X");
+    else if (subsampling == SUBSAMP_GRAY)
+      UserPreferences.set("global", "Subsampling", "Gray");
+    else
+      UserPreferences.set("global", "Subsampling", "1X");
+
+    // SECURITY AND AUTHENTICATION OPTIONS
+    //
+    // NOTE: We use key names of "SecTypes" rather than "SecurityTypes" and
+    // "Username" rather than "User".  This is to prevent older versions of the
+    // TurboVNC Viewer from automatically loading these values into parameters,
+    // since older versions of the TurboVNC Viewer did not provide a way to
+    // configure the values of those parameters using the GUI.
+    UserPreferences.set("global", "SecTypes", getSecTypesString());
+    UserPreferences.set("global", "SendLocalUsername", sendLocalUsername);
+    UserPreferences.set("global", "tunnel", tunnel);
+    UserPreferences.set("global", "Username", user);
+    if (via != null && sshUser != null)
+      UserPreferences.set("global", "via", sshUser + "@" + via);
+    else
+      UserPreferences.set("global", "via", via);
+    UserPreferences.set("global", "x509ca", x509ca);
+    UserPreferences.set("global", "x509crl", x509crl);
+
+    UserPreferences.save();
+  }
+
   void printOpt(String name, boolean val) {
     System.out.println(name + " = " + (val ? "true" : "false"));
   }
@@ -224,11 +314,141 @@ public class Options {
     printOpt("quality", quality);
     printOpt("subsampling", subsampling);
 
+    printOpt("secTypes", getSecTypesString());
     printOpt("sendLocalUsername", sendLocalUsername);
     printOpt("sshUser", sshUser);
     printOpt("tunnel", tunnel);
     printOpt("user", user);
     printOpt("via", via);
+    printOpt("x509ca", x509ca);
+    printOpt("x509crl", x509crl);
+  }
+
+  public void setSecurityTypes(String secTypesString) {
+    if (secTypesString == null)
+      enabledSecTypes = new ArrayList<Integer>();
+    else
+      enabledSecTypes = parseSecTypes(secTypesString);
+  }
+
+  public List<Integer> getEnabledSecTypes() {
+    List<Integer> result = new ArrayList<Integer>();
+
+    for (Iterator<Integer> i = enabledSecTypes.iterator(); i.hasNext();) {
+      int refType = (Integer)i.next();
+      if (refType >= 0x100) {
+        result.add(RFB.SECTYPE_VENCRYPT);
+        break;
+      }
+    }
+    result.add(RFB.SECTYPE_TIGHT);
+    result.add(RFB.SECTYPE_TLS);
+    for (Iterator<Integer> i = enabledSecTypes.iterator(); i.hasNext();) {
+      int refType = (Integer)i.next();
+      if (refType < 0x100 && refType != RFB.SECTYPE_TIGHT &&
+          refType != RFB.SECTYPE_TLS)
+        result.add(refType);
+    }
+
+    return (result);
+  }
+
+  public List<Integer> getEnabledExtSecTypes() {
+    List<Integer> result = new ArrayList<Integer>();
+
+    for (Iterator<Integer> i = enabledSecTypes.iterator(); i.hasNext();) {
+      int refType = (Integer)i.next();
+      if (refType != RFB.SECTYPE_VENCRYPT && refType != RFB.SECTYPE_UNIX_LOGIN)
+        /* ^^ Do not include VeNCrypt to avoid loops */
+        result.add(refType);
+    }
+
+    return (result);
+  }
+
+  public List<Integer> getEnabledTightSecTypes() {
+    List<Integer> result = new ArrayList<Integer>();
+
+    for (Iterator<Integer> i = enabledSecTypes.iterator(); i.hasNext();) {
+      int refType = (Integer)i.next();
+      if (refType < 0x100 && refType != RFB.SECTYPE_VENCRYPT &&
+          refType != RFB.SECTYPE_TIGHT && refType != RFB.SECTYPE_TLS)
+        result.add(refType);
+    }
+
+    return (result);
+  }
+
+  private String getSecTypesString() {
+    StringBuilder sb = new StringBuilder();
+    for (Iterator<Integer> i = enabledSecTypes.iterator(); i.hasNext();) {
+      int refType = ((Integer)i.next());
+      if (refType != RFB.SECTYPE_VENCRYPT && refType != RFB.SECTYPE_TIGHT &&
+          refType != RFB.SECTYPE_TLS) {
+        sb.append(RFB.secTypeName(refType));
+        if (i.hasNext()) sb.append(',');
+      }
+    }
+    return sb.toString();
+  }
+
+  public void enableSecType(int secType) {
+    for (Iterator<Integer> i = enabledSecTypes.iterator(); i.hasNext();)
+      if ((Integer)i.next() == secType)
+        return;
+
+    if (isSecTypeAllowed(secType))
+      enabledSecTypes.add(secType);
+  }
+
+  public boolean isSecTypeSupported(int secType) {
+    Iterator<Integer> i;
+
+    for (i = enabledSecTypes.iterator(); i.hasNext();)
+      if ((Integer)i.next() == secType)
+        return true;
+    if (secType == RFB.SECTYPE_VENCRYPT)
+      return true;
+    if (secType == RFB.SECTYPE_TIGHT)
+      return true;
+    if (secType == RFB.SECTYPE_TLS)
+      return true;
+
+    return false;
+  }
+
+  public void disableSecType(int secType) {
+    enabledSecTypes.remove((Object)secType);
+  }
+
+  private List<Integer> parseSecTypes(String types_) {
+    List<Integer> result = new ArrayList<Integer>();
+    String[] types = types_.split(",");
+    for (int i = 0; i < types.length; i++) {
+      if (types[i].length() < 1) continue;
+      int typeNum = RFB.secTypeNum(types[i]);
+      if (typeNum != RFB.SECTYPE_INVALID) {
+        if (isSecTypeAllowed(typeNum))
+          result.add(typeNum);
+      } else
+        throw new WarningException("Security type \'" + types[i] +
+                                   "\' is not valid");
+    }
+    return result;
+  }
+
+  private boolean isSecTypeAllowed(int secType) {
+    if (Params.noUnixLogin.getValue() &&
+        (secType == RFB.SECTYPE_PLAIN || secType == RFB.SECTYPE_TLS_PLAIN ||
+         secType == RFB.SECTYPE_X509_PLAIN ||
+         secType == RFB.SECTYPE_UNIX_LOGIN))
+      return false;
+    if ((user != null || sendLocalUsername) &&
+        (secType == RFB.SECTYPE_VNCAUTH || secType == RFB.SECTYPE_TLS_VNC ||
+         secType == RFB.SECTYPE_X509_VNC || secType == RFB.SECTYPE_NONE ||
+         secType == RFB.SECTYPE_TLS_NONE || secType == RFB.SECTYPE_X509_NONE))
+      return false;
+    return true;
   }
 
   public static class DesktopSize {
@@ -315,6 +535,7 @@ public class Options {
   public int quality;
   public int subsampling;
   // SECURITY AND AUTHENTICATION OPTIONS
+  private List<Integer> enabledSecTypes;
   public boolean sendLocalUsername;
   public Session sshSession;
   public boolean sshTunnelActive;
@@ -322,4 +543,6 @@ public class Options {
   public boolean tunnel;
   public String user;
   public String via;
+  public String x509ca;
+  public String x509crl;
 }

commit 2c5c8b5987cd1cc8e12a28e91d3437b02a2fca65

Viewer: Process *User* params b4 sec. types param
The set of enabled security types depends on the values of the User and
SendLocalUsername parameters as well as on the value of the
SecurityTypes parameter.

Regression introduced by 8032b4e750fb448492bc4fecb7047302d9473983

java/com/turbovnc/vncviewer/VncViewer.java
@@ -1,6 +1,6 @@
 /* Copyright (C) 2002-2005 RealVNC Ltd.  All Rights Reserved.
  * Copyright 2011 Pierre Ossman <ossman@cendio.se> for Cendio AB
- * Copyright (C) 2011-2018, 2020-2021 D. R. Commander.  All Rights Reserved.
+ * Copyright (C) 2011-2018, 2020-2022 D. R. Commander.  All Rights Reserved.
  * Copyright (C) 2011-2013, 2016 Brian P. Hinz
  *
  * This is free software; you can redistribute it and/or modify
@@ -920,11 +920,11 @@ public class VncViewer implements Runnable, OptionsDialogCallback {
       }
 
       // SECURITY AND AUTHENTICATION OPTIONS
-      opts.setSecurityTypes(Params.secTypes.getValue());
       opts.sendLocalUsername = Params.sendLocalUsername.getValue();
-      opts.tunnel = Params.tunnel.getValue();
       if (Params.user.getValue() != null)
         opts.user = new String(Params.user.getValue());
+      opts.setSecurityTypes(Params.secTypes.getValue());
+      opts.tunnel = Params.tunnel.getValue();
 
       String v = Params.via.getValue();
       if (v != null && !v.isEmpty()) {