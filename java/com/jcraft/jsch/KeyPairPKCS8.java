c7ba03823aab2213044abac9a5c3a11fda28267f
DRC <information@virtualgl.org>
2022-05-20 15:33:25 -0500

    JSch: Remove extraneous commas from (C) messages
    (I misunderstood the format of the existing copyright messages.)

--- java/com/jcraft/jsch/KeyPairPKCS8.java
+++ java/com/jcraft/jsch/KeyPairPKCS8.java
@@ -1,7 +1,7 @@
 /* -*-mode:java; c-basic-offset:2; indent-tabs-mode:nil -*- */
 /*
 Copyright (c) 2013-2016 ymnk, JCraft,Inc. All rights reserved.
-Copyright (c) 2018, D. R. Commander. All rights reserved.
+Copyright (c) 2018 D. R. Commander. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
