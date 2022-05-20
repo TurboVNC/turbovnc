68301ee7d84375fbbe04dce437898ea38554845e
DRC <information@virtualgl.org>
2022-05-20 16:32:34 -0500

    JSch: Update copyright headers for 45b0e762
    45b0e76224983b4a86cf5d8c83d26b0735402bb0 was based on:
    https://github.com/mwiede/jsch/commit/53671ba1d8175f111fd4fce2ac00f0c9c088692a
    
    Since TurboVNC predates Git, it is our policy to explicitly list all
    contributors in the copyright headers of individual source files rather
    than relying on git-blame to tell us that.
    
    The copyright header for KeyPairDeferred.java is inferred from the
    context.

--- java/com/jcraft/jsch/KeyPairDSA.java
+++ java/com/jcraft/jsch/KeyPairDSA.java
@@ -2,6 +2,7 @@
 /*
 Copyright (c) 2002-2018 ymnk, JCraft,Inc. All rights reserved.
 Copyright (c) 2018 D. R. Commander. All rights reserved.
+Copyright (c) 2020 Matthias Wiedemann. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
