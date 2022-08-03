2cffa9da773fafd0b7e498acdeb06ceb7a143b80
DRC <information@virtualgl.org>
2022-08-03 12:52:34 -0500

    DesktopWindow.java: Reformat overly long lines
    (to make checkstyle happy)

--- java/com/turbovnc/vncviewer/DesktopWindow.java
+++ java/com/turbovnc/vncviewer/DesktopWindow.java
@@ -258,7 +258,8 @@ class DesktopWindow extends JPanel implements Runnable, MouseListener,
         if (!swingDB &&
             (activeWindow == null || activeWindow instanceof Viewport ||
              (activeWindow instanceof JDialog &&
-              ((JDialog)activeWindow).getTitle().equals("TurboVNC profiling information"))))
+              ((JDialog)activeWindow).getTitle().equals(
+                "TurboVNC profiling information"))))
           RepaintManager.currentManager(this).setDoubleBufferingEnabled(false);
         if (cc.viewer.benchFile != null)
           paintImmediately(x, y, width, height);
@@ -278,7 +279,8 @@ class DesktopWindow extends JPanel implements Runnable, MouseListener,
         if (!swingDB &&
             (activeWindow == null || activeWindow instanceof Viewport ||
              (activeWindow instanceof JDialog &&
-              ((JDialog)activeWindow).getTitle().equals("TurboVNC profiling information"))))
+              ((JDialog)activeWindow).getTitle().equals(
+                "TurboVNC profiling information"))))
           RepaintManager.currentManager(this).setDoubleBufferingEnabled(false);
         if (cc.viewer.benchFile != null)
           paintImmediately(x, y, r.width(), r.height());
