af8ba02a8ef3c6f5081971856587020702ada14f
DRC <information@virtualgl.org>
2022-09-08 10:46:01 -0500

    Build: Remove Solaris 10 support ...
    ... and Solaris-specific build features.
    
    - Xvnc: Don't define the XUSE_NETDB_R_API macro.  The code in Xos_r.h
      that is enabled by that macro is never used in the TurboVNC Server.
    
    - Xvnc: Don't enable local transports.  Solaris 11 has proper Unix
      domain socket support and does not need local transports.
    
    - Xvnc: Don't use finite() (from ieeefp.h) rather than isfinite().
      Solaris 11 has a more modern version of GCC that supports isfinite().
    
    - Xvnc: Don't disable XTrans FD passing support.  That feature works
      properly with Solaris 11.
    
    - Xvnc: Don't link with libsocket and libnsl.  That is no longer
      necessary on Solaris 11.
    
    - Xvnc: Don't enable Secure RPC.  Secure RPC is a Solaris-specific
      feature that is rarely used and is not supported by the TurboVNC
      Server.
    
    - xstartup.turbovnc: Remove support for the CDE display manager
      (dtlogin), which is no longer present in Solaris 11.

--- unix/Xvnc/programs/Xserver/dix/devices.c
+++ unix/Xvnc/programs/Xserver/dix/devices.c
@@ -76,11 +76,6 @@ SOFTWARE.
 #include <X11/extensions/XI2.h>
 #include <X11/extensions/XIproto.h>
 #include <math.h>
-#ifdef TURBOVNC
-#ifdef HAVE_IEEEFP_H
-#include <ieeefp.h>
-#endif
-#endif
 #include <pixman.h>
 #include "exglobals.h"
 #include "exevents.h"
@@ -179,11 +174,7 @@ DeviceSetProperty(DeviceIntPtr dev, Atom property, XIPropertyValuePtr prop,
             return BadValue;
 
         for (i = 0; i < 9; i++)
-#ifdef TURBOVNC
-            if (!finite(f[i]))
-#else
             if (!isfinite(f[i]))
-#endif
                 return BadValue;
 
         if (!dev->valuator)
