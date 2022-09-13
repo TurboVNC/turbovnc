TurboVNC/turbovnc

commit d94fe5343c964af95c68be069b1033403b78ed28

Viewer: Extend NoReconnect to initial conn./auth.
Offer to reconnect if the initial connection or authentication fails.
Note that the viewer will only offer to reconnect during the initial
connection/authentication phase if a WarningException is thrown.
Effectively, that means that it will offer to reconnect due to a normal
connection failure (for instance, specifying the wrong hostname or
display number)	or a normal authentication failure (for instance,
mistyping the password), but it will not offer to reconnect if the
maximum number of consecutive VNC password or OTP authentication
failures has been exceeded.  Furthermore, if an authentication failure
occurs, the viewer will return to the authentication dialog, but if any
other failure occurs, it will return to the "New TurboVNC Connection"
dialog (thus allowing the user to adjust the hostname, display number,
gateway, etc.)

This commit also modifies CConn to ensure that session manager/tunneling
errors due to an unknown host are thrown as WarningExceptions, thus
causing the viewer to offer to reconnect.

Closes #92

java/com/turbovnc/vncviewer/VncViewer.java
@@ -631,7 +631,7 @@ public class VncViewer implements Runnable, OptionsDialogCallback {
     Object[] dlgOptions = { UIManager.getString("OptionPane.yesButtonText"),
                             UIManager.getString("OptionPane.noButtonText") };
     if (reconnect)
-      pane = new JOptionPane(msg + "\nReconnect?", msgType,
+      pane = new JOptionPane(msg + "\nAttempt to reconnect?", msgType,
                              JOptionPane.YES_NO_OPTION, null, dlgOptions,
                              dlgOptions[1]);
     else
@@ -639,8 +639,11 @@ public class VncViewer implements Runnable, OptionsDialogCallback {
     JDialog dlg = pane.createDialog(null, title);
     dlg.setAlwaysOnTop(true);
     dlg.setVisible(true);
-    if (reconnect && pane.getValue() == dlgOptions[0])
+    if (reconnect && pane.getValue() == dlgOptions[0]) {
+      if (!(e instanceof AuthFailureException))
+        params.server.set(null);
       start();
+    }
     else {
       synchronized(this) {
         this.notify();
@@ -804,9 +807,10 @@ public class VncViewer implements Runnable, OptionsDialogCallback {
         }
       } catch (Exception e) {
         if (cc == null || !cc.shuttingDown) {
-          reportException(e, cc != null &&
-                          cc.state() == CConnection.RFBSTATE_NORMAL &&
-                          !params.noReconnect.get());
+          reportException(e, !params.noReconnect.get() &&
+                             ((cc != null &&
+                               cc.state() == CConnection.RFBSTATE_NORMAL) ||
+                              e instanceof WarningException));
           exitStatus = 1;
           if (cc != null) {
             cc.deleteWindow(true);

java/com/turbovnc/vncviewer/CConn.java
@@ -141,8 +141,12 @@ public class CConn extends CConnection implements UserPasswdGetter,
             params.sessMgrActive = true;
             params.port.set(Hostname.getPort(session));
           } catch (Exception e) {
-            throw new ErrorException("Session Manager Error:\n" +
-                                     e.getMessage());
+            if (e instanceof com.jcraft.jsch.JSchException)
+              throw new WarningException("Session Manager Error:\n" +
+                                         e.getMessage());
+            else
+              throw new ErrorException("Session Manager Error:\n" +
+                                       e.getMessage());
           }
         }
         try {
@@ -150,8 +154,12 @@ public class CConn extends CConnection implements UserPasswdGetter,
           port = Hostname.getPort(params.server.get());
           server = Hostname.getHost(params.server.get());
         } catch (Exception e) {
-          throw new ErrorException("Could not create SSH tunnel:\n" +
-                                   e.getMessage());
+          if (e instanceof com.jcraft.jsch.JSchException)
+            throw new WarningException("Could not create SSH tunnel:\n" +
+                                       e.getMessage());
+          else
+            throw new ErrorException("Could not create SSH tunnel:\n" +
+                                     e.getMessage());
         }
       }
 
@@ -166,8 +174,12 @@ public class CConn extends CConnection implements UserPasswdGetter,
           port = Hostname.getPort(session);
           params.sshSession.disconnect();
         } catch (Exception e) {
-          throw new ErrorException("Session Manager Error:\n" +
-                                   e.getMessage());
+          if (e instanceof com.jcraft.jsch.JSchException)
+            throw new WarningException("Session Manager Error:\n" +
+                                       e.getMessage());
+          else
+            throw new ErrorException("Session Manager Error:\n" +
+                                     e.getMessage());
         }
       }
 

commit 2fc109d852a3d5d4e46dde8f7cd74a1663b66354

VncViewer.java: Fix code formatting issue
(detected by checkstyle, introduced by
d94fe5343c964af95c68be069b1033403b78ed28)

java/com/turbovnc/vncviewer/VncViewer.java
@@ -643,8 +643,7 @@ public class VncViewer implements Runnable, OptionsDialogCallback {
       if (!(e instanceof AuthFailureException))
         params.server.set(null);
       start();
-    }
-    else {
+    } else {
       synchronized(this) {
         this.notify();
       }