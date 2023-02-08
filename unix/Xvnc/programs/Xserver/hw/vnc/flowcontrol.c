TurboVNC/turbovnc

commit 94d4718904aeca743626ba5fecc1cc67d8297274

Server: Work around cong control algorithm misfit
As implemented in the TigerVNC Server, the overhauled congestion control
algorithms will, if the ETA is -1 (which would be the case if baseRTT is
(unsigned)-1, which would be the case after baseRTT is reset due to an
idle connection), report congestion without setting the congestion
timer.  TigerVNC can get away with that because it has a "frame timer"
that will deliver the congestion-deferred updates at the next frame
boundary, but in the case of TurboVNC, deferring updates due to
congestion without setting the congestion timer results in the updates
not being delivered in a timely manner.

Our implementation now treats ETA <= 0 as uncongested, since it doesn't
make much sense to set a 1 ms congestion timer if the uncongested ETA is
0.

unix/Xvnc/programs/Xserver/hw/vnc/flowcontrol.c
@@ -298,11 +298,13 @@ Bool rfbIsCongested(rfbClientPtr cl)
     return FALSE;
 
   eta = GetUncongestedETA(cl);
-  if (eta >= 0)
-    cl->congestionTimer = TimerSet(cl->congestionTimer, 0, eta <= 0 ? 1 : eta,
+  if (eta >= 1) {
+    cl->congestionTimer = TimerSet(cl->congestionTimer, 0, eta,
                                    congestionCallback, cl);
+    return TRUE;
+  }
 
-  return TRUE;
+  return FALSE;
 }
 
 

commit 7e7ff2fcb33f84fdbb99618e00f3c0d3ffa74ebf

Server: Don't lock up if network connection drops
Regression introduced by 94d4718904aeca743626ba5fecc1cc67d8297274

Referring to the discussions at
https://groups.google.com/g/turbovnc-users/c/NzoZq_mIuP0/m/znjQm-3uAQAJ
and
https://github.com/TigerVNC/tigervnc/commit/a99d14d1939cb2338b6268d9aebe3850df66daed#r57748408
the overhauled congestion control algorithms
(a0f5670ecc42538f95f56ee81a885c6ba32916f1,
ffdb526ab850352f1f759c7e84cb0ceba6c4db91,
9f3153645b70c5036591d8bdfa4f5b50784ad01a) caused a regression whereby
large framebuffer updates were sometimes not delivered in a timely
manner over high-latency connections.  My attempt to fix the regression
(94d4718904aeca743626ba5fecc1cc67d8297274) caused another issue (#359)
whereby Xvnc froze if the network connection dropped and a connected VNC
viewer exited or disconnected before the network connection was restored
(thus causing the TCP connection to become orphaned at the server end.)

More specifically, the TigerVNC v1.9+ implementation of the congestion
control algorithms treats an uncongested ETA of 0 and -1 as congested,
but it only sets a deferred update timer if the ETA is >= 0.  If
congestion control is reset, due to an idle connection, before the
deferred update can be sent, then the update is effectively dropped
(spoiled.)  94d4718904aeca743626ba5fecc1cc67d8297274 modified the
algorithms so that an uncongested ETA of 0 or -1 was treated as
uncongested, but when the network connection dropped, that caused the
dropped connection to be treated as uncongested rather than congested.
Thus, the TurboVNC Server attempted to send framebuffer updates through
the dropped connection, which quickly filled the TCP buffers and caused
write()/SSL_write() to block.  When the VNC viewer disconnected or
exited, the TCP connection became orphaned at the server end, so
write()/SSL_write() continued to block when the network connection was
restored.  The orphaned TCP connection was eventually disconnected,

unix/Xvnc/programs/Xserver/hw/vnc/flowcontrol.c
@@ -2,8 +2,8 @@
  * flowcontrol.c - implement RFB flow control extensions
  */
 
-/* Copyright (C) 2012, 2014, 2017-2018, 2021 D. R. Commander.
- *                                           All Rights Reserved.
+/* Copyright (C) 2012, 2014, 2017-2018, 2021, 2023 D. R. Commander.
+ *                                                 All Rights Reserved.
  * Copyright (C) 2018 Peter Åstrand for Cendio AB.  All Rights Reserved.
  * Copyright (C) 2011, 2015 Pierre Ossman for Cendio AB.  All Rights Reserved.
  *
@@ -297,13 +297,9 @@ Bool rfbIsCongested(rfbClientPtr cl)
     return FALSE;
 
   eta = GetUncongestedETA(cl);
-  if (eta >= 1) {
-    cl->congestionTimer = TimerSet(cl->congestionTimer, 0, eta,
-                                   congestionCallback, cl);
-    return TRUE;
-  }
-
-  return FALSE;
+  cl->congestionTimer = TimerSet(cl->congestionTimer, 0, eta <= 0 ? 1 : eta,
+                                 congestionCallback, cl);
+  return TRUE;
 }
 
 