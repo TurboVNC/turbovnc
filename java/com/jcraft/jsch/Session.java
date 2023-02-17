TurboVNC/turbovnc

commit d6ae34d6f4bd29308acc9835c8eb80f5e4b2143c

JSch: Implement rsa-sha2-256/-512 sig. schemes
OpenSSH v8.8 no longer supports the "ssh-rsa" signature scheme (which
uses the now-defeated SHA-1 hash algorithm) by default.  The same RSA
keys can, however, be used with the "rsa-sha2-256" and "rsa-sha2-512"
signature schemes, which respectively use the SHA-256 and SHA-512 hash
algorithms.

Based on:
https://github.com/mwiede/jsch/commit/045799e55e11ce90a1cf5be8e3abdf4e6a7549f4
https://github.com/mwiede/jsch/commit/4895cc481e1effbd07e38315ec553c0a9796f2eb
https://github.com/mwiede/jsch/commit/90a711bfe533e8b483a6617b37f5a9fbd1204820
https://github.com/mwiede/jsch/commit/69398c9fa20c00593e410dd94534bae298b6dbe0

java/com/jcraft/jsch/Session.java
@@ -2,6 +2,7 @@
 /*
 Copyright (c) 2002-2018 ymnk, JCraft,Inc. All rights reserved.
 Copyright (c) 2018 D. R. Commander. All rights reserved.
+Copyright (c) 2020-2021 Jeremy Norris. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
@@ -2305,8 +2306,9 @@ break;
       if(config==null) 
         config=new java.util.Hashtable();
       for(java.util.Enumeration e=newconf.keys() ; e.hasMoreElements() ;) {
-        String key=(String)(e.nextElement());
-        config.put(key, (String)(newconf.get(key)));
+        String newkey=(String)(e.nextElement());
+        String key=(newkey.equals("PubkeyAcceptedKeyTypes") ? "PubkeyAcceptedAlgorithms" : newkey);
+        config.put(key, (String)(newconf.get(newkey)));
       }
     }
   }
@@ -2316,11 +2318,19 @@ break;
       if(config==null){
         config=new java.util.Hashtable();
       }
-      config.put(key, value);
+      if(key.equals("PubkeyAcceptedKeyTypes")){
+        config.put("PubkeyAcceptedAlgorithms", value);
+      }
+      else{
+        config.put(key, value);
+      }
     }
   }
 
   public String getConfig(String key){
+    if(key.equals("PubkeyAcceptedKeyTypes")){
+      key="PubkeyAcceptedAlgorithms";
+    }
     Object foo=null;
     if(config!=null){
       foo=config.get(key);
@@ -2707,6 +2717,7 @@ break;
     checkConfig(config, "StrictHostKeyChecking");
     checkConfig(config, "HashKnownHosts");
     checkConfig(config, "PreferredAuthentications");
+    checkConfig(config, "PubkeyAcceptedAlgorithms");
     checkConfig(config, "MaxAuthTries");
     checkConfig(config, "ClearAllForwardings");
 
@@ -2853,6 +2864,8 @@ break;
 
   private void checkConfig(ConfigRepository.Config config, String key){
     String value = config.getValue(key);
+    if(value == null && key.equals("PubkeyAcceptedAlgorithms"))
+      value = config.getValue("PubkeyAcceptedKeyTypes");
     if(value != null)
       this.setConfig(key, value);
   }

java/com/jcraft/jsch/UserAuthPublicKey.java
@@ -1,6 +1,8 @@
 /* -*-mode:java; c-basic-offset:2; indent-tabs-mode:nil -*- */
 /*
 Copyright (c) 2002-2018 ymnk, JCraft,Inc. All rights reserved.
+Copyright (c) 2020 Jeremy Norris. All rights reserved.
+Copyright (c) 2020-2021 Matthias Wiedemann. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
@@ -48,8 +50,30 @@ class UserAuthPublicKey extends UserAuth{
         return false;
       }
 
+      String pkmethods=session.getConfig("PubkeyAcceptedAlgorithms");
+      String[] pkmethoda=Util.split(pkmethods, ",");
+      if(pkmethoda.length==0){
+        return false;
+      }
+
+      String rsamethods=null;
+      String nonrsamethods=null;
+      for(int i=0; i<pkmethoda.length; i++){
+        if(pkmethoda[i].equals("ssh-rsa") || pkmethoda[i].equals("rsa-sha2-256") || pkmethoda[i].equals("rsa-sha2-512")){
+          if(rsamethods==null) rsamethods=pkmethoda[i];
+          else rsamethods+=","+pkmethoda[i];
+        }
+        else{
+          if(nonrsamethods==null) nonrsamethods=pkmethoda[i];
+          else nonrsamethods+=","+pkmethoda[i];
+        }
+      }
+      String[] rsamethoda=Util.split(rsamethods, ",");
+      String[] nonrsamethoda=Util.split(nonrsamethods, ",");
+
       _username=Util.str2byte(username);
 
+      iloop:
       for(int i=0; i<identities.size(); i++){
 
         if(session.auth_failures >= session.max_auth_tries){
@@ -57,57 +81,97 @@ class UserAuthPublicKey extends UserAuth{
         }
 
         Identity identity=(Identity)(identities.elementAt(i));
+
+        String ipkmethod=identity.getAlgName();
+        String[] ipkmethoda=null;
+        if(ipkmethod.equals("ssh-rsa")){
+          ipkmethoda=rsamethoda;
+        }
+        else if(nonrsamethoda!=null && nonrsamethoda.length>0){
+          for(int j=0; j<nonrsamethoda.length; j++){
+            if(ipkmethod.equals(nonrsamethoda[j])){
+              ipkmethoda=new String[]{ipkmethod};
+              break;
+            }
+          }
+        }
+        if(ipkmethoda==null) {
+          if(JSch.getLogger().isEnabled(Logger.DEBUG)){
+            JSch.getLogger().log(Logger.DEBUG,
+                    ipkmethod+" cannot be used as public key type for identity "+identity.getName());
+          }
+          continue;
+        }
+
         byte[] pubkeyblob=identity.getPublicKeyBlob();
+        String[] pkmethodsuccess=null;
 
         if(pubkeyblob!=null){
-          // send
-          // byte      SSH_MSG_USERAUTH_REQUEST(50)
-          // string    user name
-          // string    service name ("ssh-connection")
-          // string    "publickey"
-          // boolen    FALSE
-          // string    public key algorithm name
-          // string    public key blob
-          packet.reset();
-          buf.putByte((byte)SSH_MSG_USERAUTH_REQUEST);
-          buf.putString(_username);
-          buf.putString(Util.str2byte("ssh-connection"));
-          buf.putString(Util.str2byte("publickey"));
-          buf.putByte((byte)0);
-          buf.putString(Util.str2byte(identity.getAlgName()));
-          buf.putString(pubkeyblob);
-          session.write(packet);
+          command=SSH_MSG_USERAUTH_FAILURE;
+          loop3:
+          for(int j=0; j<ipkmethoda.length; j++){
+            // send
+            // byte      SSH_MSG_USERAUTH_REQUEST(50)
+            // string    user name
+            // string    service name ("ssh-connection")
+            // string    "publickey"
+            // boolen    FALSE
+            // string    public key algorithm name
+            // string    public key blob
+            packet.reset();
+            buf.putByte((byte)SSH_MSG_USERAUTH_REQUEST);
+            buf.putString(_username);
+            buf.putString(Util.str2byte("ssh-connection"));
+            buf.putString(Util.str2byte("publickey"));
+            buf.putByte((byte)0);
+            buf.putString(Util.str2byte(ipkmethoda[j]));
+            buf.putString(pubkeyblob);
+            session.write(packet);
 
-          loop1:
-          while(true){
-            buf=session.read(buf);
-            command=buf.getCommand()&0xff;
+            loop1:
+            while(true){
+              buf=session.read(buf);
+              command=buf.getCommand()&0xff;
 
-            if(command==SSH_MSG_USERAUTH_PK_OK){
-              break;
-            }
-            else if(command==SSH_MSG_USERAUTH_FAILURE){
-              break;
-            }
-            else if(command==SSH_MSG_USERAUTH_BANNER){
-              buf.getInt(); buf.getByte(); buf.getByte();
-              byte[] _message=buf.getString();
-              byte[] lang=buf.getString();
-              String message=Util.byte2str(_message);
-              if(userinfo!=null){
-                userinfo.showMessage(message);
+              if(command==SSH_MSG_USERAUTH_PK_OK){
+                if(JSch.getLogger().isEnabled(Logger.DEBUG)){
+                  JSch.getLogger().log(Logger.DEBUG,
+                                       ipkmethoda[j] + " preauth success");
+                }
+                pkmethodsuccess=new String[]{ipkmethoda[j]};
+                break loop3;
               }
-              continue loop1;
-            }
-            else{
-	    //System.err.println("USERAUTH fail ("+command+")");
-	    //throw new JSchException("USERAUTH fail ("+command+")");
-              break;
+              else if(command==SSH_MSG_USERAUTH_FAILURE){
+                if(JSch.getLogger().isEnabled(Logger.DEBUG)){
+                  JSch.getLogger().log(Logger.DEBUG,
+                                       ipkmethoda[j] + " preauth failure");
+                }
+                continue loop3;
+              }
+              else if(command==SSH_MSG_USERAUTH_BANNER){
+                buf.getInt(); buf.getByte(); buf.getByte();
+                byte[] _message=buf.getString();
+                byte[] lang=buf.getString();
+                String message=Util.byte2str(_message);
+                if(userinfo!=null){
+                  userinfo.showMessage(message);
+                }
+                continue loop1;
+              }
+              else{
+              //System.err.println("USERAUTH fail ("+command+")");
+              //throw new JSchException("USERAUTH fail ("+command+")");
+                if(JSch.getLogger().isEnabled(Logger.DEBUG)){
+                  JSch.getLogger().log(Logger.DEBUG,
+                                       ipkmethoda[j] + " preauth failure command (" + command + ")");
+                }
+                continue loop3;
+              }
             }
           }
 
           if(command!=SSH_MSG_USERAUTH_PK_OK){
-            continue;
+            continue iloop;
           }
         }
 
@@ -154,78 +218,98 @@ class UserAuthPublicKey extends UserAuth{
 //System.err.println("UserAuthPublicKey: pubkeyblob="+pubkeyblob);
 
         if(pubkeyblob==null) continue;
+        if(pkmethodsuccess==null) pkmethodsuccess=ipkmethoda;
 
-        // send
-        // byte      SSH_MSG_USERAUTH_REQUEST(50)
-        // string    user name
-        // string    service name ("ssh-connection")
-        // string    "publickey"
-        // boolen    TRUE
-        // string    public key algorithm name
-        // string    public key blob
-        // string    signature
-        packet.reset();
-        buf.putByte((byte)SSH_MSG_USERAUTH_REQUEST);
-        buf.putString(_username);
-        buf.putString(Util.str2byte("ssh-connection"));
-        buf.putString(Util.str2byte("publickey"));
-        buf.putByte((byte)1);
-        buf.putString(Util.str2byte(identity.getAlgName()));
-        buf.putString(pubkeyblob);
+        loop4:
+        for(int j=0; j<pkmethodsuccess.length; j++){
+          // send
+          // byte      SSH_MSG_USERAUTH_REQUEST(50)
+          // string    user name
+          // string    service name ("ssh-connection")
+          // string    "publickey"
+          // boolen    TRUE
+          // string    public key algorithm name
+          // string    public key blob
+          // string    signature
+          packet.reset();
+          buf.putByte((byte)SSH_MSG_USERAUTH_REQUEST);
+          buf.putString(_username);
+          buf.putString(Util.str2byte("ssh-connection"));
+          buf.putString(Util.str2byte("publickey"));
+          buf.putByte((byte)1);
+          buf.putString(Util.str2byte(pkmethodsuccess[j]));
+          buf.putString(pubkeyblob);
 
-//      byte[] tmp=new byte[buf.index-5];
-//      System.arraycopy(buf.buffer, 5, tmp, 0, tmp.length);
-//      buf.putString(signature);
+//        byte[] tmp=new byte[buf.index-5];
+//        System.arraycopy(buf.buffer, 5, tmp, 0, tmp.length);
+//        buf.putString(signature);
 
-        byte[] sid=session.getSessionId();
-        int sidlen=sid.length;
-        byte[] tmp=new byte[4+sidlen+buf.index-5];
-        tmp[0]=(byte)(sidlen>>>24);
-        tmp[1]=(byte)(sidlen>>>16);
-        tmp[2]=(byte)(sidlen>>>8);
-        tmp[3]=(byte)(sidlen);
-        System.arraycopy(sid, 0, tmp, 4, sidlen);
-        System.arraycopy(buf.buffer, 5, tmp, 4+sidlen, buf.index-5);
-        byte[] signature=identity.getSignature(tmp);
-        if(signature==null){  // for example, too long key length.
-          break;
-        }
-        buf.putString(signature);
-        session.write(packet);
-
-        loop2:
-        while(true){
-          buf=session.read(buf);
-          command=buf.getCommand()&0xff;
-
-          if(command==SSH_MSG_USERAUTH_SUCCESS){
-            return true;
-          }
-          else if(command==SSH_MSG_USERAUTH_BANNER){
-            buf.getInt(); buf.getByte(); buf.getByte();
-            byte[] _message=buf.getString();
-            byte[] lang=buf.getString();
-            String message=Util.byte2str(_message);
-            if(userinfo!=null){
-              userinfo.showMessage(message);
+          byte[] sid=session.getSessionId();
+          int sidlen=sid.length;
+          byte[] tmp=new byte[4+sidlen+buf.index-5];
+          tmp[0]=(byte)(sidlen>>>24);
+          tmp[1]=(byte)(sidlen>>>16);
+          tmp[2]=(byte)(sidlen>>>8);
+          tmp[3]=(byte)(sidlen);
+          System.arraycopy(sid, 0, tmp, 4, sidlen);
+          System.arraycopy(buf.buffer, 5, tmp, 4+sidlen, buf.index-5);
+          byte[] signature=identity.getSignature(tmp, pkmethodsuccess[j]);
+          if(signature==null){  // for example, too long key length.
+            if(JSch.getLogger().isEnabled(Logger.DEBUG)){
+              JSch.getLogger().log(Logger.DEBUG,
+                                   pkmethodsuccess[j] + " signature failure");
             }
-            continue loop2;
+            continue loop4;
           }
-          else if(command==SSH_MSG_USERAUTH_FAILURE){
-            buf.getInt(); buf.getByte(); buf.getByte(); 
-            byte[] foo=buf.getString();
-            int partial_success=buf.getByte();
-	  //System.err.println(new String(foo)+
-	  //                   " partial_success:"+(partial_success!=0));
-            if(partial_success!=0){
-              throw new JSchPartialAuthException(Util.byte2str(foo));
+          buf.putString(signature);
+          session.write(packet);
+
+          loop2:
+          while(true){
+            buf=session.read(buf);
+            command=buf.getCommand()&0xff;
+
+            if(command==SSH_MSG_USERAUTH_SUCCESS){
+              if(JSch.getLogger().isEnabled(Logger.DEBUG)){
+                JSch.getLogger().log(Logger.DEBUG,
+                                     pkmethodsuccess[j] + " auth success");
+              }
+              return true;
             }
-            session.auth_failures++;
-            break;
+            else if(command==SSH_MSG_USERAUTH_BANNER){
+              buf.getInt(); buf.getByte(); buf.getByte();
+              byte[] _message=buf.getString();
+              byte[] lang=buf.getString();
+              String message=Util.byte2str(_message);
+              if(userinfo!=null){
+                userinfo.showMessage(message);
+              }
+              continue loop2;
+            }
+            else if(command==SSH_MSG_USERAUTH_FAILURE){
+              buf.getInt(); buf.getByte(); buf.getByte();
+              byte[] foo=buf.getString();
+              int partial_success=buf.getByte();
+            //System.err.println(new String(foo)+
+            //                   " partial_success:"+(partial_success!=0));
+              if(partial_success!=0){
+                throw new JSchPartialAuthException(Util.byte2str(foo));
+              }
+              session.auth_failures++;
+              if(JSch.getLogger().isEnabled(Logger.DEBUG)){
+                JSch.getLogger().log(Logger.DEBUG,
+                                     pkmethodsuccess[j] + " auth failure");
+              }
+              break loop2;
+            }
+            //System.err.println("USERAUTH fail ("+command+")");
+            //throw new JSchException("USERAUTH fail ("+command+")");
+            if(JSch.getLogger().isEnabled(Logger.DEBUG)){
+              JSch.getLogger().log(Logger.DEBUG,
+                                   pkmethodsuccess[j] + " auth failure command (" + command +")");
+            }
+            break loop2;
           }
-          //System.err.println("USERAUTH fail ("+command+")");
-          //throw new JSchException("USERAUTH fail ("+command+")");
-          break;
         }
       }
     }

java/CMakeLists.txt
@@ -80,6 +80,9 @@ set(JSCH_CLASSNAMES
 	jce/SignatureECDSA521
 	jce/SignatureECDSAN
 	jce/SignatureRSA
+	jce/SignatureRSAN
+	jce/SignatureRSASHA256
+	jce/SignatureRSASHA512
 	jce/TripleDESCBC
 	jce/TripleDESCTR
 	jcraft/Compression

java/com/jcraft/jsch/Identity.java
@@ -2,6 +2,7 @@
 /*
 Copyright (c) 2002-2018 ymnk, JCraft,Inc. All rights reserved.
 Copyright (c) 2018 D. R. Commander. All rights reserved.
+Copyright (c) 2020 Jeremy Norris. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
@@ -58,6 +59,14 @@ public interface Identity{
    * @return the signature
    */
   public byte[] getSignature(byte[] data);
+
+  /**
+   * Signs on data with this identity, and returns the result.
+   * @param data data to be signed
+   * @param alg signature algorithm to use
+   * @return the signature
+   */
+  public byte[] getSignature(byte[] data, String alg);
 
   /**
    * @deprecated The decryption should be done automatically in #setPassphase(byte[] passphrase)

java/com/jcraft/jsch/IdentityFile.java
@@ -2,6 +2,7 @@
 /*
 Copyright (c) 2002-2018 ymnk, JCraft,Inc. All rights reserved.
 Copyright (c) 2018 D. R. Commander. All rights reserved.
+Copyright (c) 2020 Jeremy Norris. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
@@ -87,6 +88,16 @@ class IdentityFile implements Identity{
    */
   public byte[] getSignature(byte[] data){
     return kpair.getSignature(data);
+  }
+
+  /**
+   * Signs on data with this identity, and returns the result.
+   * @param data data to be signed
+   * @param alg signature algorithm to use
+   * @return the signature
+   */
+  public byte[] getSignature(byte[] data, String alg){
+    return kpair.getSignature(data, alg);
   }
 
   /**

java/com/jcraft/jsch/JSch.java
@@ -2,6 +2,7 @@
 /*
 Copyright (c) 2002-2018 ymnk, JCraft,Inc. All rights reserved.
 Copyright (c) 2018 D. R. Commander. All rights reserved.
+Copyright (c) 2020-2021 Jeremy Norris. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
@@ -42,7 +43,7 @@ public class JSch{
   static java.util.Hashtable config=new java.util.Hashtable();
   static{
     config.put("kex", "ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group14-sha1,diffie-hellman-group-exchange-sha256,diffie-hellman-group-exchange-sha1,diffie-hellman-group1-sha1");
-    config.put("server_host_key", "ssh-rsa,ssh-dss,ecdsa-sha2-nistp256,ecdsa-sha2-nistp384,ecdsa-sha2-nistp521");
+    config.put("server_host_key", "rsa-sha2-256,rsa-sha2-512,ssh-rsa,ssh-dss,ecdsa-sha2-nistp256,ecdsa-sha2-nistp384,ecdsa-sha2-nistp521");
     config.put("cipher.s2c", 
                "aes128-ctr,aes128-cbc,3des-ctr,3des-cbc,blowfish-cbc,aes192-ctr,aes192-cbc,aes256-ctr,aes256-cbc");
     config.put("cipher.c2s",
@@ -94,7 +95,9 @@ public class JSch{
     config.put("sha-512",         "com.jcraft.jsch.jce.SHA512");
     config.put("md5",           "com.jcraft.jsch.jce.MD5");
     config.put("signature.dss", "com.jcraft.jsch.jce.SignatureDSA");
-    config.put("signature.rsa", "com.jcraft.jsch.jce.SignatureRSA");
+    config.put("ssh-rsa",       "com.jcraft.jsch.jce.SignatureRSA");
+    config.put("rsa-sha2-256",  "com.jcraft.jsch.jce.SignatureRSASHA256");
+    config.put("rsa-sha2-512",  "com.jcraft.jsch.jce.SignatureRSASHA512");
     config.put("keypairgen.dsa",   "com.jcraft.jsch.jce.KeyPairGenDSA");
     config.put("keypairgen.rsa",   "com.jcraft.jsch.jce.KeyPairGenRSA");
     config.put("keypairgen.ecdsa", "com.jcraft.jsch.jce.KeyPairGenECDSA");
@@ -130,10 +133,11 @@ public class JSch{
     config.put("HashKnownHosts",  "no");
 
     config.put("PreferredAuthentications", "gssapi-with-mic,publickey,keyboard-interactive,password");
+    config.put("PubkeyAcceptedAlgorithms", "rsa-sha2-256,rsa-sha2-512,ssh-rsa,ssh-dss,ecdsa-sha2-nistp256,ecdsa-sha2-nistp384,ecdsa-sha2-nistp521");
 
     config.put("CheckCiphers", "aes256-ctr,aes192-ctr,aes128-ctr,aes256-cbc,aes192-cbc,aes128-cbc,3des-ctr,arcfour,arcfour128,arcfour256");
     config.put("CheckKexes", "diffie-hellman-group14-sha1,ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521");
-    config.put("CheckSignatures", "ecdsa-sha2-nistp256,ecdsa-sha2-nistp384,ecdsa-sha2-nistp521");
+    config.put("CheckSignatures", "rsa-sha2-256,rsa-sha2-512,ecdsa-sha2-nistp256,ecdsa-sha2-nistp384,ecdsa-sha2-nistp521");
 
     config.put("MaxAuthTries", "6");
     config.put("ClearAllForwardings", "no");
@@ -588,6 +592,9 @@ public class JSch{
    */
   public static String getConfig(String key){ 
     synchronized(config){
+      if(key.equals("PubkeyAcceptedKeyTypes")){
+        key="PubkeyAcceptedAlgorithms";
+      }
       return (String)(config.get(key));
     } 
   }
@@ -600,8 +607,9 @@ public class JSch{
   public static void setConfig(java.util.Hashtable newconf){
     synchronized(config){
       for(java.util.Enumeration e=newconf.keys() ; e.hasMoreElements() ;) {
-	String key=(String)(e.nextElement());
-	config.put(key, (String)(newconf.get(key)));
+	String newkey=(String)(e.nextElement());
+	String key=(newkey.equals("PubkeyAcceptedKeyTypes") ? "PubkeyAcceptedAlgorithms" : newkey);
+	config.put(key, (String)(newconf.get(newkey)));
       }
     }
   }
@@ -613,7 +621,12 @@ public class JSch{
    * @param value value for the configuration
    */
   public static void setConfig(String key, String value){
-    config.put(key, value);
+    if(key.equals("PubkeyAcceptedKeyTypes")){
+      config.put("PubkeyAcceptedAlgorithms", value);
+    }
+    else{
+      config.put(key, value);
+    }
   }
 
   /**

java/com/jcraft/jsch/KeyExchange.java
@@ -2,6 +2,7 @@
 /*
 Copyright (c) 2002-2018 ymnk, JCraft,Inc. All rights reserved.
 Copyright (c) 2018 D. R. Commander. All rights reserved.
+Copyright (c) 2020 Jeremy Norris. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
@@ -214,8 +215,10 @@ public abstract class KeyExchange{
       n=tmp;
 	
       SignatureRSA sig=null;
+      Buffer buf=new Buffer(sig_of_H);
+      String foo=Util.byte2str(buf.getString());
       try{
-        Class c=Class.forName(session.getConfig("signature.rsa"));
+        Class c=Class.forName(session.getConfig(foo));
         sig=(SignatureRSA)(c.getDeclaredConstructor().newInstance());
         sig.init();
       }

java/com/jcraft/jsch/KeyPair.java
@@ -2,6 +2,7 @@
 /*
 Copyright (c) 2002-2018 ymnk, JCraft,Inc. All rights reserved.
 Copyright (c) 2018 D. R. Commander. All rights reserved.
+Copyright (c) 2020 Jeremy Norris. All rights reserved.
 Copyright (c) 2020 Matthias Wiedemann. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -77,7 +78,9 @@ public abstract class KeyPair{
   abstract int getKeySize();
 
   public abstract byte[] getSignature(byte[] data);
+  public abstract byte[] getSignature(byte[] data, String alg);
   public abstract Signature getVerifier();
+  public abstract Signature getVerifier(String alg);
 
   public abstract byte[] forSSHAgent() throws JSchException;
 

commit fd34df2ca25f99288f0569511b68f107c289e7b1

JSch: Only try RSA sig schemes the server supports
Regression introduced by d6ae34d6f4bd29308acc9835c8eb80f5e4b2143c

The code we borrowed from the JSch fork (https://github.com/mwiede/jsch)
in order to implement the rsa-sha2-256 and rsa-sha2-512 signature
schemes tries all of the RSA signature schemes in sequence for every RSA
private key offered by the agent, even if the server doesn't support all
of those schemes.  That caused our implementation of JSch to prematurely
exceed the SSH server's MaxAuthTries.  It is unclear whether there is
other code in the JSch fork that prevents this issue.

Fixes #361

java/com/jcraft/jsch/Session.java
@@ -1,7 +1,7 @@
 /* -*-mode:java; c-basic-offset:2; indent-tabs-mode:nil -*- */
 /*
 Copyright (c) 2002-2018 ymnk, JCraft,Inc. All rights reserved.
-Copyright (c) 2018 D. R. Commander. All rights reserved.
+Copyright (c) 2018, 2023 D. R. Commander. All rights reserved.
 Copyright (c) 2020-2021 Jeremy Norris. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -89,6 +89,7 @@ public class Session implements Runnable{
   private int seqo=0;
 
   String[] guess=null;
+  Vector supportedRSAMethods=new Vector();
   private Cipher s2ccipher;
   private Cipher c2scipher;
   private MAC s2cmac;
@@ -590,6 +591,11 @@ public class Session implements Runnable{
     guess=KeyExchange.guess(I_S, I_C);
     if(guess==null){
       throw new JSchException("Algorithm negotiation fail");
+    }
+    for(String method : guess){
+      if(method.equals("ssh-rsa") || method.equals("rsa-sha2-256") || method.equals("rsa-sha2-512")){
+        supportedRSAMethods.addElement(method);
+      }
     }
 
     if(!isAuthed &&

java/com/jcraft/jsch/UserAuthPublicKey.java
@@ -3,6 +3,7 @@
 Copyright (c) 2002-2018 ymnk, JCraft,Inc. All rights reserved.
 Copyright (c) 2020 Jeremy Norris. All rights reserved.
 Copyright (c) 2020-2021 Matthias Wiedemann. All rights reserved.
+Copyright (c) 2023 D. R. Commander. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
@@ -59,8 +60,10 @@ class UserAuthPublicKey extends UserAuth{
       String nonrsamethods=null;
       for(int i=0; i<pkmethoda.length; i++){
         if(pkmethoda[i].equals("ssh-rsa") || pkmethoda[i].equals("rsa-sha2-256") || pkmethoda[i].equals("rsa-sha2-512")){
-          if(rsamethods==null) rsamethods=pkmethoda[i];
-          else rsamethods+=","+pkmethoda[i];
+          if(session.supportedRSAMethods.contains(pkmethoda[i])){
+            if(rsamethods==null) rsamethods=pkmethoda[i];
+            else rsamethods+=","+pkmethoda[i];
+          }
         }
         else{
           if(nonrsamethods==null) nonrsamethods=pkmethoda[i];