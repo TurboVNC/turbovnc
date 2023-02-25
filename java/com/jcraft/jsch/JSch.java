TurboVNC/turbovnc

commit 5a583cd221d988d961c34cc4128c195b15d1e233

Java: Allow setting SSH auth methods w/system prop
+ document the new system property and eliminate the less useful and
undocumented turbovnc.gssapi system property.

java/com/turbovnc/vncviewer/Tunnel.java
@@ -175,11 +175,10 @@ public final class Tunnel {
     if (opts.sshSession.getConfig("StrictHostKeyChecking") == null)
       opts.sshSession.setConfig("StrictHostKeyChecking", "ask");
     opts.sshSession.setConfig("MaxAuthTries", "3");
-    if (!VncViewer.getBooleanProperty("turbovnc.gssapi", false)) {
-      String auth = opts.sshSession.getConfig("PreferredAuthentications");
-      opts.sshSession.setConfig("PreferredAuthentications",
-                                auth.replaceAll("gssapi-with-mic,", ""));
-    }
+    String auth = System.getProperty("turbovnc.sshauth");
+    if (auth == null)
+      auth = "publickey,keyboard-interactive,password";
+    opts.sshSession.setConfig("PreferredAuthentications", auth);
     PasswdDialog dlg = new PasswdDialog(new String("SSH Authentication"),
                                         true, user, false);
     opts.sshSession.setUserInfo(dlg);

doc/index.html
@@ -3,7 +3,7 @@
 <head>
 <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
 <meta name="language" content="en">
-<meta name="date" content="2018-12-03T16:27:36">
+<meta name="date" content="2018-12-03T16:28:24">
 <meta name="generator" content="deplate.rb 0.8.5">
 <title>User&rsquo;s Guide for TurboVNC 2.2.1</title>
 <link rel="start" href="index.html" title="Frontpage">
@@ -3158,6 +3158,35 @@ Java command-line arguments in a Java Web Start environment.</p>
         A more Java-friendly way of disabling the multi-screen-aware automatic 
         desktop resize feature.  See the <code>TVNC_SINGLESCREEN</code> 
         environment variable above for more details.
+    </dd>
+</dl>
+
+<div class="table">
+<table class="standard">
+  <tr class="standard">
+    <td class="high standard">Java System Property</td>
+    <td class="standard"><code>turbovnc.sshauth</code></td>
+  </tr>
+  <tr class="standard">
+    <td class="high standard">Summary</td>
+    <td class="standard">Preferred authentication methods for the built-in SSH client</td>
+  </tr>
+  <tr class="standard">
+    <td class="high standard">Default Value</td>
+    <td class="standard"><code>publickey,keyboard-interactive,password</code></td>
+  </tr>
+</table>
+</div>
+
+
+<dl class="Description">
+    <dt class="Description-1 Description">Description</dt>
+    <dd class="Description-1 Description">
+        This system property can be used to enable or disable particular SSH 
+        authentication methods, as well as to specify their preferred order.  
+        The same thing can be accomplished by using the 
+        <code>PreferredAuthentications</code> directive in the OpenSSH config 
+        file.
     </dd>
 </dl>
 

commit 6838846316147089e9e03940edebe82fdd837e43

Viewer: Don't stomp on OpenSSH cfg PreferredAuth
(again)

Regression introduced by 5a583cd221d988d961c34cc4128c195b15d1e233

Referring to ae7a994c0716ad409bb7778f6746fe5b2724b43c, we don't want the
default list of SSH authentication methods to contain gssapi-with-mic,
but referring to 20537fc51137b861a7a452c752990b1051771acf, we also want
to allow the list of SSH authentication methods to be specified in an
OpenSSH config file.  This commit simply removes gssapi-with-mic from
the default list of SSH authentication methods in JSch rather than
trying to override the default list in the TurboVNC Viewer.

java/com/jcraft/jsch/JSch.java
@@ -132,7 +132,7 @@ public class JSch{
     config.put("StrictHostKeyChecking",  "ask");
     config.put("HashKnownHosts",  "no");
 
-    config.put("PreferredAuthentications", "gssapi-with-mic,publickey,keyboard-interactive,password");
+    config.put("PreferredAuthentications", "publickey,keyboard-interactive,password");
     config.put("PubkeyAcceptedAlgorithms", "rsa-sha2-256,rsa-sha2-512,ssh-rsa,ssh-dss,ecdsa-sha2-nistp256,ecdsa-sha2-nistp384,ecdsa-sha2-nistp521");
 
     config.put("CheckCiphers", "aes256-ctr,aes192-ctr,aes128-ctr,aes256-cbc,aes192-cbc,aes128-cbc,3des-ctr,arcfour,arcfour128,arcfour256");

java/com/turbovnc/vncviewer/Tunnel.java
@@ -241,9 +241,8 @@ public class Tunnel {
       opts.sshSession.setConfig("StrictHostKeyChecking", "ask");
     opts.sshSession.setConfig("MaxAuthTries", "3");
     String auth = System.getProperty("turbovnc.sshauth");
-    if (auth == null)
-      auth = "publickey,keyboard-interactive,password";
-    opts.sshSession.setConfig("PreferredAuthentications", auth);
+    if (auth != null)
+      opts.sshSession.setConfig("PreferredAuthentications", auth);
     PasswdDialog dlg = new PasswdDialog(new String("SSH Authentication"),
                                         true, user, false, true, -1);
     if (!Utils.getBooleanProperty("turbovnc.sshkeytest", false))