3f8f0eb346b8fe7e5829e28af760801e3b6d94bd
DRC <information@virtualgl.org>
2024-01-17 10:51:28 -0500

    Win: Stop F10 from changing KB focus to sys menu
    I'm not sure when Windows 10 started hijacking the F10 key to serve the
    same purpose as the left Alt key (activating the system menu), but it
    must have been recently.  (In the process of integrating/implementing
    the server-side key mapping feature, I thoroughly tested all possible
    keystrokes on Windows, macOS, and Linux clients.)
    
    This commit simply extends the fix in
    4d4072283df07a4838ab325b9714e02f73366f71 to cover the F10 key as well.

--- java/com/turbovnc/vncviewer/DesktopWindow.java
+++ java/com/turbovnc/vncviewer/DesktopWindow.java
@@ -1,4 +1,4 @@
-/* Copyright (C) 2010, 2012-2013, 2015-2018, 2020-2023 D. R. Commander.
+/* Copyright (C) 2010, 2012-2013, 2015-2018, 2020-2024 D. R. Commander.
  *                                                     All Rights Reserved.
  * Copyright (C) 2011-2013 Brian P. Hinz
  * Copyright (C) 2009 Paul Donohue.  All Rights Reserved.
@@ -99,8 +99,9 @@ class DesktopWindow extends JPanel implements Runnable, MouseListener,
       private final KeyEventDispatcher keyEventDispatcher =
         new KeyEventDispatcher() {
         public boolean dispatchKeyEvent(KeyEvent e) {
-          if (e.getKeyCode() == 18 &&
-              e.getKeyLocation() == KeyEvent.KEY_LOCATION_LEFT) {
+          if ((e.getKeyCode() == 18 &&
+               e.getKeyLocation() == KeyEvent.KEY_LOCATION_LEFT) ||
+              e.getKeyCode() == 121) {
             if (e.getID() == KeyEvent.KEY_PRESSED)
               cc.desktop.keyPressed(e);
             else if (e.getID() == KeyEvent.KEY_RELEASED)
