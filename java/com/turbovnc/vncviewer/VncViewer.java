08fd7925b147f9f92f0a901e56f3eeaea5c13dc9
DRC <information@virtualgl.org>
2024-11-16 12:11:51 -0500

    Viewer: Usage screen clarification
    The usage screen documents command-line usage, so it is assumed that the
    VNC host or VNC server will be specified.  Thus, the VNC host or server
    should not be surrounded by square brackets (which indicate optional
    parts of the command line.)

--- java/com/turbovnc/vncviewer/VncViewer.java
+++ java/com/turbovnc/vncviewer/VncViewer.java
@@ -514,7 +514,7 @@ public final class VncViewer implements Runnable, OptionsDialogCallback {
       "-----\n" +
       "\n" +
       (Utils.getBooleanProperty("turbovnc.sessmgr", true) ?
-       "vncviewer [options/parameters] [host] [options/parameters]\n" +
+       "vncviewer [options/parameters] [user@]host [options/parameters]\n" +
        "\n" +
        "Connect to the specified TurboVNC host using the TurboVNC Session Manager,\n" +
        "which uses the TurboVNC Viewer's built-in SSH client to remotely start a new\n" +
@@ -524,8 +524,8 @@ public final class VncViewer implements Runnable, OptionsDialogCallback {
        "it expects the TurboVNC Server to be installed under /opt/TurboVNC on the host.\n" +
        "Refer to the TurboVNC User's Guide for more details.\n" +
        "\n" : "") +
-      "vncviewer [options/parameters] [host:displayNum] [options/parameters]\n" +
-      "vncviewer [options/parameters] [host::port] [options/parameters]\n" +
+      "vncviewer [options/parameters] host:displayNum [options/parameters]\n" +
+      "vncviewer [options/parameters] host::port [options/parameters]\n" +
       "\n" +
       "Connect directly to the VNC server that is listening on the specified VNC\n" +
       "display number or TCP port on the specified host.  This mode of operation does\n" +
