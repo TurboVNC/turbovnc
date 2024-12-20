TurboVNC/turbovnc

commit dda0283384b82c62b930c6516f6c19a59e8a1a46

Viewer: Allow SSHPort to override OpenSSH cfg file
In OpenSSH, command-line parameters always override OpenSSH config file
keywords.  d654a91e3e5da79243e3a89b57aad44d957cf333 (TurboVNC 2.2.1)
gave the OpenSSH config file precedence over TurboVNC Viewer parameters,
because otherwise, SSH-related options automatically saved from the
TurboVNC Viewer Options dialog would always override the OpenSSH config
file.  However, the introduction of per-host options and other option
handling improvements in TurboVNC 3.1.x eliminated that concern.
Because of 992657d4978dba83b2fbb8b2e7e3b3960f1a0dc6 (TurboVNC 3.1), the
SSHUser parameter now takes precedence over the OpenSSH config file, but
the saved value of that parameter affects only one host.  Also, users
can override that saved value using the command line or a connection
info file.  Furthermore, SSHPort is not a GUI option, so the
aforementioned concern never applied to it.

Also adjust the SSHUser parameter documentation and code comments to
reflect the current reality.

java/com/jcraft/jsch/Session.java
@@ -2700,9 +2700,11 @@ break;
     if(value != null)
       host = value;
 
-    int port = config.getPort();
-    if(port != -1)
-      this.port = port;
+    if(port == -1) {
+      int port = config.getPort();
+      if(port != -1)
+        this.port = port;
+    }
 
     checkConfig(config, "kex");
     checkConfig(config, "server_host_key");

java/com/turbovnc/rfb/IntParameter.java
@@ -1,4 +1,4 @@
-/* Copyright (C) 2012, 2017-2018, 2022-2023 D. R. Commander.
+/* Copyright (C) 2012, 2017-2018, 2022-2024 D. R. Commander.
  *                                          All Rights Reserved.
  * Copyright (C) 2002-2005 RealVNC Ltd.  All Rights Reserved.
  *
@@ -64,16 +64,21 @@ public class IntParameter extends VoidParameter {
     if ((useMin && value_ < minValue) || (useMax && value_ > maxValue))
       return false;
     value = value_;
+    isDefault = false;
     setCommandLine(false);
     return true;
   }
 
-  public final synchronized void reset() { set(defValue); }
+  public final synchronized void reset() {
+    set(defValue);
+    isDefault = true;
+  }
 
   public final synchronized boolean setDefault(int defValue_) {
     if ((useMin && defValue_ < minValue) || (useMax && defValue_ > maxValue))
       return false;
     value = defValue = defValue_;
+    isDefault = true;
     return true;
   }
 
@@ -103,6 +108,10 @@ public class IntParameter extends VoidParameter {
     }
     return null;
   }
+
+  public final synchronized boolean isDefault() { return isDefault; }
+
+  private boolean isDefault = true;
 
   int value, defValue;
   final int minValue, maxValue;

java/com/turbovnc/rfb/Params.java
@@ -1024,8 +1024,9 @@ public final class Params {
   public StringParameter sshConfig =
   new StringParameter("SSHConfig", this, false,
   "Path to an OpenSSH configuration file to use with the built-in SSH " +
-  "client.  The OpenSSH configuration file takes precedence over any " +
-  "TurboVNC Viewer parameters.", Utils.getHomeDir() + ".ssh/config");
+  "client.  If specified on the command line or in a connection info file, " +
+  "TurboVNC Viewer parameters take precedence over the OpenSSH " +
+  "configuration file.", Utils.getHomeDir() + ".ssh/config");
 
   public StringParameter sshKey =
   new StringParameter("SSHKey", this, false,

java/com/turbovnc/vncviewer/Tunnel.java
@@ -174,7 +174,7 @@ public class Tunnel {
     }
 
     // username and passphrase will be given via UserInfo interface.
-    int port = params.sshPort.get();
+    int port = params.sshPort.isDefault() ? -1 : params.sshPort.get();
     String user = params.sshUser.get();
 
     File sshConfigFile = new File(params.sshConfig.get());
@@ -183,10 +183,6 @@ public class Tunnel {
         OpenSSHConfig.parseFile(sshConfigFile.getAbsolutePath());
       jsch.setConfigRepository(repo);
       vlog.debug("Read OpenSSH config file " + params.sshConfig.get());
-      // This just ensures that the password dialog displays the correct
-      // username.  JSch will ignore the username and port passed to
-      // getSession() if the configuration has already been set using an
-      // OpenSSH configuration file.
       String repoUser = repo.getConfig(host).getUser();
       if (repoUser != null && user == null)
         user = repoUser;

commit 053e754cb720bd9d4e07e0e8ca43222f0c312afc

Viewer: Fix IllegalArgException w/unknown SSH host
... if the SSHPort parameter is unspecified.

(regression introduced by dda0283384b82c62b930c6516f6c19a59e8a1a46)

java/com/jcraft/jsch/Session.java
@@ -150,7 +150,8 @@ public class Session implements Runnable{
 
   String host="127.0.0.1";
   String org_host="127.0.0.1";
-  int port=22;
+  int defaultPort=22;
+  int port=defaultPort;
 
   String username=null;
   byte[] password=null;
@@ -2701,9 +2702,11 @@ break;
       host = value;
 
     if(port == -1) {
-      int port = config.getPort();
-      if(port != -1)
-        this.port = port;
+      int configPort = config.getPort();
+      if(configPort != -1)
+        port = configPort;
+      else
+        port = defaultPort;
     }
 
     checkConfig(config, "kex");