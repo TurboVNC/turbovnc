TurboVNC/turbovnc

commit 5f3218e0fae6da34a6e026c4f71bfd4fbe82cd3c

Viewer: Send horizontal scroll wheel events
On macOS and Windows, these are communicated to applications as vertical
scroll wheel events with the Shift key held down.  (The operating system
acts as if the Shift key is held down, but no Shift key press/release
events are actually fired.)  We use RFB Buttons 6 and 7 for,
respectively, left and right scroll wheel events, since X11 Buttons 6
and 7 are used for the same purpose.  (Thus, implementing this feature
in the TurboVNC Server simply required incrementing the loop limit in
PtrAddEvent().)  Java doesn't need to use Java Buttons 4 and 5 for
vertical scroll wheel events, since those events have a separate
subclass.  Thus, X11 Buttons 6 and 7 map to Java Buttons 4 and 5, and we
simply map Java Buttons 4 and 5 to RFB Buttons 6 and 7 in order to
implement horizontal scrolling for X11 clients.

Server and macOS/Windows viewer implementation based on:
https://github.com/rsparlin/turbovnc/commit/77e266d6deaa1df6b537cfd3599c445e90a378db

Closes #416

java/com/turbovnc/vncviewer/CConn.java
@@ -2438,11 +2438,20 @@ public final class CConn extends CConnection implements UserPasswdGetter,
             buttonMask |= RFB.BUTTON2_MASK;  break;
           case 3:
             buttonMask |= RFB.BUTTON3_MASK;  break;
+          case 4:
+            // X11 uses Buttons 6 and 7 for (respectively) left and right
+            // scroll wheel events, but Java/X11 uses Buttons 4 and 5.
+            if (!Utils.isX11()) return;
+            buttonMask |= RFB.BUTTON6_MASK;  break;
+          case 5:
+            if (!Utils.isX11()) return;
+            buttonMask |= RFB.BUTTON7_MASK;  break;
           default:
             return;
         }
-        vlog.debug("mouse PRESS, button " + ev.getButton() +
-                   ", coords " + ev.getX() + "," + ev.getY());
+        if (ev.getButton() <= 3)
+          vlog.debug("mouse PRESS, button " + ev.getButton() +
+                     ", coords " + ev.getX() + "," + ev.getY());
         break;
       case MouseEvent.MOUSE_RELEASED:
         switch (ev.getButton()) {
@@ -2452,11 +2461,18 @@ public final class CConn extends CConnection implements UserPasswdGetter,
             buttonMask &= ~RFB.BUTTON2_MASK;  break;
           case 3:
             buttonMask &= ~RFB.BUTTON3_MASK;  break;
+          case 4:
+            if (!Utils.isX11()) return;
+            buttonMask &= ~RFB.BUTTON6_MASK;  break;
+          case 5:
+            if (!Utils.isX11()) return;
+            buttonMask &= ~RFB.BUTTON7_MASK;  break;
           default:
             return;
         }
-        vlog.debug("mouse release, button " + ev.getButton() +
-                   ", coords " + ev.getX() + "," + ev.getY());
+        if (ev.getButton() <= 3)
+          vlog.debug("mouse release, button " + ev.getButton() +
+                     ", coords " + ev.getX() + "," + ev.getY());
         break;
     }
 
@@ -2494,10 +2510,20 @@ public final class CConn extends CConnection implements UserPasswdGetter,
     if (opts.reverseScroll) {
       clicks = -clicks;
     }
+    // On macOS and Windows, horizontal scroll wheel events are simply vertical
+    // scroll wheel events with the Shift key held down.  (The operating system
+    // acts as if the Shift key is held down, but no Shift key press/release
+    // events are actually fired.)
+    boolean isHorizontal =
+      (Utils.isMac() || Utils.isWindows()) && ev.isShiftDown();
+    // X11 uses Buttons 4, 5, 6, and 7 for (respectively) up, down, left, and
+    // right scroll wheel events.
     if (clicks < 0) {
-      wheelMask = buttonMask | RFB.BUTTON4_MASK;
+      wheelMask = buttonMask |
+                  (isHorizontal ? RFB.BUTTON6_MASK : RFB.BUTTON4_MASK);
     } else {
-      wheelMask = buttonMask | RFB.BUTTON5_MASK;
+      wheelMask = buttonMask |
+                  (isHorizontal ? RFB.BUTTON7_MASK : RFB.BUTTON5_MASK);
     }
 
     if (cp.width != desktop.scaledWidth ||

java/com/turbovnc/rfb/RFB.java
@@ -309,6 +309,8 @@ public final class RFB {
   public static final int BUTTON3_MASK = 4;
   public static final int BUTTON4_MASK = 8;
   public static final int BUTTON5_MASK = 16;
+  public static final int BUTTON6_MASK = 32;
+  public static final int BUTTON7_MASK = 64;
 
   //***************************************************************************
   // GII

unix/Xvnc/programs/Xserver/hw/vnc/kbdptr.c
@@ -480,7 +480,7 @@ void PtrAddEvent(int buttonMask, int x, int y, rfbClientPtr cl)
     cursorPosY = y;
   }
 
-  for (i = 0; i < 5; i++) {
+  for (i = 0; i < 7; i++) {
     if ((buttonMask ^ oldButtonMask) & (1 << i)) {
       if (buttonMask & (1 << i)) {
         valuator_mask_set_range(&mask, 0, 0, NULL);

commit 0e4b7ac9451e9a29843c5afdeabe5953ebe461f7

Mac/Win: Fix interpretation of Shift+scroll wheel
On macOS and Windows, horizontal scroll wheel events are communicated to
applications as vertical scroll wheel events with the Shift key held
down, but no corresponding Shift key press or release occurs.  Because
some Un*x applications and window managers use Shift + vertical scroll
wheel events, we need to distinguish horizontal scroll wheel events from
vertical scroll wheel events with an actual Shift key press.
CConn.isKeyPressed() to the rescue.

(regression introduced by 5f3218e0fae6da34a6e026c4f71bfd4fbe82cd3c)

Fixes #437

java/com/turbovnc/vncviewer/CConn.java
@@ -2517,7 +2517,9 @@ public final class CConn extends CConnection implements UserPasswdGetter,
     // acts as if the Shift key is held down, but no Shift key press/release
     // events are actually fired.)
     boolean isHorizontal =
-      (Utils.isMac() || Utils.isWindows()) && ev.isShiftDown();
+      (Utils.isMac() || Utils.isWindows()) && ev.isShiftDown() &&
+      !pressedKeys.containsValue(Keysyms.SHIFT_L) &&
+      !pressedKeys.containsValue(Keysyms.SHIFT_R);
     // X11 uses Buttons 4, 5, 6, and 7 for (respectively) up, down, left, and
     // right scroll wheel events.
     if (clicks < 0) {