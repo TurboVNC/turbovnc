dcc3f3b8a4bfdcd4f336ebeffd9102fa46c05a81
DRC <information@virtualgl.org>
2025-01-20 14:08:57 -0500

    Viewer: Always show Helper status in conn info dlg
    Because of 4d9a87732fdf26a772deeab4095856bb0e1dbe92, the TurboVNC Helper
    status is now relevant on all platforms.  Also, since TurboVNC 2.2.x,
    the union of platforms that support keyboard grabbing and extended input
    devices is the set of all platforms, so this commit has no effect other
    than improving code readability.

--- java/com/turbovnc/vncviewer/CConn.java
+++ java/com/turbovnc/vncviewer/CConn.java
@@ -1702,8 +1702,7 @@ public final class CConn extends CConnection implements UserPasswdGetter,
       "Encryption protocol:  " + getEncryptionProtocol() + "\n" +
       "JPEG decompression:  " +
         (reader.isTurboJPEG() ? "Turbo" : "Unaccelerated") +
-      (Utils.osGrab() || Utils.osEID() ? "\nTurboVNC Helper:  " +
-        (Helper.isAvailable() ? "Loaded" : "Not found") : ""),
+      "\nTurboVNC Helper:  " + (Helper.isAvailable() ? "Loaded" : "Not found"),
       "VNC connection info", JOptionPane.PLAIN_MESSAGE);
   }
 
