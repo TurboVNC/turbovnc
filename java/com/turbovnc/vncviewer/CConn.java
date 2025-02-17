TurboVNC/turbovnc

commit c3dd8fe637f0e7d50125a237f33d99e80003e89a

Viewer: Fix race condition in connection shutdown
If the viewer window is closed using its close gadget or a hotkey, then
CConn.close() will execute in the Swing Event Dispatch Thread (EDT), so
we need to ensure that CConnection.processMsg() cannot execute
concurrently in the RFB thread.  Otherwise, TightDecoder could try to
use the native TurboJPEG instance after the instance has been destroyed,
thus causing the Java process to crash with a segfault on Un*x and an
EXCEPTION_GUARD_PAGE error on Windows.

java/com/turbovnc/vncviewer/CConn.java
@@ -1,6 +1,6 @@
 /* Copyright (C) 2002-2005 RealVNC Ltd.  All Rights Reserved.
  * Copyright 2009-2011 Pierre Ossman <ossman@cendio.se> for Cendio AB
- * Copyright (C) 2011-2022 D. R. Commander.  All Rights Reserved.
+ * Copyright (C) 2011-2023 D. R. Commander.  All Rights Reserved.
  * Copyright (C) 2011-2015 Brian P. Hinz
  *
  * This is free software; you can redistribute it and/or modify
@@ -1446,7 +1446,7 @@ public class CConn extends CConnection implements UserPasswdGetter,
   // close() shuts down the socket, thus waking up the RFB thread.
   public void close() { close(true); }
 
-  public void close(boolean disposeViewport) {
+  public synchronized void close(boolean disposeViewport) {
     if (disposeViewport && !confirmClose()) return;
     deleteWindow(disposeViewport);
     shuttingDown = true;

java/com/turbovnc/rfb/CConnection.java
@@ -1,7 +1,7 @@
 /* Copyright (C) 2002-2005 RealVNC Ltd.  All Rights Reserved.
  * Copyright (C) 2011-2012 Brian P. Hinz
- * Copyright (C) 2012, 2014, 2016, 2018, 2021 D. R. Commander.
- *                                            All Rights Reserved.
+ * Copyright (C) 2012, 2014, 2016, 2018, 2021, 2023 D. R. Commander.
+ *                                                  All Rights Reserved.
  *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -55,7 +55,7 @@ public abstract class CConnection extends CMsgHandler {
 
   // processMsg() should be called whenever there is data to read on the
   // InStream.  You must have called initialiseProtocol() first.
-  public void processMsg(boolean benchmark) {
+  public synchronized void processMsg(boolean benchmark) {
     switch (state) {
       case RFBSTATE_PROTOCOL_VERSION:  processVersionMsg();         break;
       case RFBSTATE_SECURITY_TYPES:    processSecurityTypesMsg();   break;

commit d2f548364f082636761ba4f844c5c1f8521e23aa

Viewer: Fix rare deadlock w/ auto desktop resizing
(regression introduced by c3dd8fe637f0e7d50125a237f33d99e80003e89a)

If automatic desktop resizing is enabled and the TurboVNC Viewer is in
windowed mode, recreating the viewer window in the body of
CConn.resizeFramebuffer() sometimes causes the window's focusLost()
method to be invoked.  Under rare circumstances, the method can be
invoked in the EDT while the RFB thread is still in the body of
CConn.resizeFramebuffer().  Because of
c3dd8fe637f0e7d50125a237f33d99e80003e89a, CConnection.processMsg()
(in the RFB thread) obtains a monitor lock on the CConnection/CConn
instance, and the lock is still owned by the RFB thread when
CConn.resizeFramebuffer() is called from CConnection.processMsg().  When
the viewer window's focusLost() method attempted to call
CConn.releasePressedKeys() in the EDT, CConn.releasePressedKeys() failed
to obtain a monitor lock on the CConnection/CConn instance, and a
deadlock occurred.

It isn't actually necessary for CConn.releasePressedKeys() to obtain a
monitor lock.  That method is only ever executed in the EDT, and the
resources it accesses are only ever accessed in the EDT.  The monitor
lock was in the original code base that the TurboVNC Viewer inherited
from TigerVNC, but TigerVNC no longer has it.  (The 'synchronized'
keyword was silently removed in
TigerVNC/tigervnc@e5b4003bfb24f740e0d6ea2c82422d1dacf7b927.)

Fixes #447

java/com/turbovnc/vncviewer/CConn.java
@@ -2467,7 +2467,7 @@ public class CConn extends CConnection implements UserPasswdGetter,
 
 
   // EDT
-  synchronized void releasePressedKeys() {
+  void releasePressedKeys() {
     for (Map.Entry<Integer, Integer> entry : pressedKeys.entrySet()) {
       vlog.debug(String.format("Lost focus.  Releasing key symbol 0x%04x",
                  entry.getValue()));