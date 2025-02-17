TurboVNC/turbovnc

commit 01549361cb5f85b93403feb36df8ab5694a6f962

Viewer: Fix race condition in connection shutdown
If the viewer window is closed using its close gadget or a hotkey, then
CConn.close() will execute in the Swing Event Dispatch Thread (EDT), so
we need to ensure that CConnection.processMsg() cannot execute
concurrently in the RFB thread.  Otherwise, TightDecoder could try to
use the native TurboJPEG instance after the instance has been destroyed,
thus causing the Java process to crash with a segfault on Un*x and an
EXCEPTION_GUARD_PAGE error on Windows.

java/com/turbovnc/vncviewer/CConn.java
@@ -1,4 +1,4 @@
-/* Copyright (C) 2011-2022 D. R. Commander.  All Rights Reserved.
+/* Copyright (C) 2011-2023 D. R. Commander.  All Rights Reserved.
  * Copyright (C) 2011-2015 Brian P. Hinz
  * Copyright 2009-2011 Pierre Ossman <ossman@cendio.se> for Cendio AB
  * Copyright (C) 2002-2005 RealVNC Ltd.  All Rights Reserved.
@@ -1465,7 +1465,7 @@ public class CConn extends CConnection implements UserPasswdGetter,
   // close() shuts down the socket, thus waking up the RFB thread.
   public void close() { close(true); }
 
-  public void close(boolean disposeViewport) {
+  public synchronized void close(boolean disposeViewport) {
     if (disposeViewport && !confirmClose()) return;
     deleteWindow(disposeViewport);
     shuttingDown = true;

java/com/turbovnc/rfb/CConnection.java
@@ -1,4 +1,4 @@
-/* Copyright (C) 2012, 2014, 2016, 2018, 2020-2022 D. R. Commander.
+/* Copyright (C) 2012, 2014, 2016, 2018, 2020-2023 D. R. Commander.
  *                                                 All Rights Reserved.
  * Copyright (C) 2011-2012 Brian P. Hinz
  * Copyright (C) 2002-2005 RealVNC Ltd.  All Rights Reserved.
@@ -54,7 +54,7 @@ public abstract class CConnection extends CMsgHandler {
 
   // processMsg() should be called whenever there is data to read on the
   // InStream.  You must have called initialiseProtocol() first.
-  public void processMsg(boolean benchmark) {
+  public synchronized void processMsg(boolean benchmark) {
     switch (state) {
       case RFBSTATE_PROTOCOL_VERSION:  processVersionMsg();         break;
       case RFBSTATE_SECURITY_TYPES:    processSecurityTypesMsg();   break;

commit d6c001708d9e2eb58feed443840cbf86c04ca25d

Viewer: Fix rare deadlock w/ auto desktop resizing
(regression introduced by 01549361cb5f85b93403feb36df8ab5694a6f962)

If automatic desktop resizing is enabled and the TurboVNC Viewer is in
windowed mode, recreating the viewer window in the body of
CConn.resizeFramebuffer() sometimes causes the window's focusLost()
method to be invoked.  Under rare circumstances, the method can be
invoked in the EDT while the RFB thread is still in the body of
CConn.resizeFramebuffer().  Because of
01549361cb5f85b93403feb36df8ab5694a6f962, CConnection.processMsg()
(in the RFB thread) obtains a monitor lock on the CConnection/CConn
instance, and the lock is still owned by the RFB thread when
CConn.resizeFramebuffer() is called from CConnection.processMsg().  When
the viewer window's focusLost() method attempted to call
CConn.releasePressedKeys() in the EDT, CConn.releasePressedKeys() failed
to obtain a monitor lock on the CConnection/CConn instance, and a
deadlock occurred.

It isn't actually necessary for CConn.releasePressedKeys() to obtain a
monitor lock.  That method is only ever executed in the EDT, and the
resources it accesses are only ever accessed in the EDT.  The monitor
lock was in the original code base that the TurboVNC Viewer inherited
from TigerVNC, but TigerVNC no longer has it.  (The 'synchronized'
keyword was silently removed in
TigerVNC/tigervnc@e5b4003bfb24f740e0d6ea2c82422d1dacf7b927.)

Fixes #447

CMakeLists.txt
@@ -6,7 +6,7 @@ cmake_minimum_required(VERSION 3.16...3.28)
 
 project(TurboVNC NONE)
 string(TOLOWER ${CMAKE_PROJECT_NAME} CMAKE_PROJECT_NAME_LC)
-set(VERSION 3.1.90)
+set(VERSION 3.1.91)
 set(DOCVERSION 3.2)
 set(COPYRIGHT_YEAR "1999-2025")
 set(COPYRIGHT "The VirtualGL Project and many others (see README.md)")

doc/index.html
@@ -3,7 +3,7 @@
 <head>
 <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
 <meta name="language" content="en">
-<meta name="date" content="2025-02-06T09:28:15">
+<meta name="date" content="2025-02-17T12:08:14">
 <meta name="generator" content="deplate.rb 0.8.5">
 <title>User&rsquo;s Guide for TurboVNC 3.2</title>
 <link rel="start" href="index.html" title="Frontpage">
@@ -588,8 +588,8 @@ The TurboVNC security configuration file will not work when TurboVNC is installe
 <ol class="Ordered numeric">
     <li class="Ordered-1 Ordered">
         Download the TurboVNC Mac disk image 
-        (<strong class="filename">TurboVNC-3.1.90-x86_64.dmg</strong> for Intel 
-        CPUs or <strong class="filename">TurboVNC-3.1.90-arm64.dmg</strong> for 
+        (<strong class="filename">TurboVNC-3.1.91-x86_64.dmg</strong> for Intel 
+        CPUs or <strong class="filename">TurboVNC-3.1.91-arm64.dmg</strong> for 
         Apple silicon CPUs) from the 
         <span class="remote"><a href="https://github.com/TurboVNC/turbovnc/releases" target="_blank" class="remote">Releases 
         area</a></span><a name="idx0019"></a> of the 
@@ -610,7 +610,7 @@ The TurboVNC security configuration file will not work when TurboVNC is installe
 <ol class="Ordered numeric">
     <li class="Ordered-1 Ordered">
         Download the TurboVNC Windows installer package 
-        (<strong class="filename">TurboVNC-3.1.90.exe</strong>) from the 
+        (<strong class="filename">TurboVNC-3.1.91.exe</strong>) from the 
         <span class="remote"><a href="https://github.com/TurboVNC/turbovnc/releases" target="_blank" class="remote">Releases 
         area</a></span><a name="idx0021"></a> of the 
         <span class="remote"><a href="https://github.com/TurboVNC/turbovnc" target="_blank" class="remote">TurboVNC 
@@ -630,12 +630,12 @@ The TurboVNC security configuration file will not work when TurboVNC is installe
 <p>If you are using a Linux/Un*x platform for which there is not a 
 pre-built TurboVNC binary package available, then download the TurboVNC 
 source tarball 
-(<strong class="filename">turbovnc-3.1.90.tar.gz</strong>) from the 
+(<strong class="filename">turbovnc-3.1.91.tar.gz</strong>) from the 
 <span class="remote"><a href="https://github.com/TurboVNC/turbovnc/releases" target="_blank" class="remote">Releases 
 area</a></span><a name="idx0023"></a> of the 
 <span class="remote"><a href="https://github.com/TurboVNC/turbovnc" target="_blank" class="remote">TurboVNC 
 GitHub project page</a></span><a name="idx0024"></a>, uncompress it, 
-<code>cd turbovnc-3.1.90</code>, and read 
+<code>cd turbovnc-3.1.91</code>, and read 
 <strong class="filename">BUILDING.md</strong> for further instructions 
 on how to build TurboVNC from source.</p>
 

java/com/turbovnc/vncviewer/CConn.java
@@ -3000,7 +3000,7 @@ public final class CConn extends CConnection implements UserPasswdGetter,
 
 
   // EDT
-  synchronized void releasePressedKeys() {
+  void releasePressedKeys() {
     for (Map.Entry<Integer, Integer> entry : pressedVKeys.entrySet()) {
       vlog.debug(String.format("Lost focus.  Releasing key symbol 0x%04x",
                  entry.getValue()));