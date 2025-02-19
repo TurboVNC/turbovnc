TurboVNC/turbovnc

commit ed9e4b245a371404ff821cac9e1fc9fe8bcc778e

Viewer: Don't allow blank host w/ @ in conn. dlg.
The New TurboVNC Connection dialog now considers the @ sign when
deciding whether to enable the "Options..." and "Connect" buttons.
Previously, if "[{user}]@" was entered into the "VNC server" field, then
clicking "Options..." caused the TurboVNC Viewer Options dialog to pop
up with a blank hostname, and clicking "Connect" caused a
NullPointerException.  Hostname.getHost() now considers the @ sign as
well, which prevents another NullPointerException if "[{user}]@:" was
entered into the "VNC server" field.

java/com/turbovnc/vncviewer/ServerDialog.java
@@ -183,10 +183,11 @@ class ServerDialog extends Dialog implements ActionListener {
   }
 
   private void updateButtons() {
-    okButton.setEnabled(editor.getItem() != null &&
-                        ((String)editor.getItem()).length() > 0);
-    optionsButton.setEnabled(editor.getItem() != null &&
-                             ((String)editor.getItem()).length() > 0);
+    String serverStr = (String)editor.getItem();
+    if (serverStr != null)
+      serverStr = Hostname.getHost(serverStr);
+    okButton.setEnabled(serverStr != null && serverStr.length() > 0);
+    optionsButton.setEnabled(serverStr != null && serverStr.length() > 0);
   }
 
   public void actionPerformed(ActionEvent e) {
@@ -202,10 +203,7 @@ class ServerDialog extends Dialog implements ActionListener {
     } else if (s instanceof JButton && (JButton)s == optionsButton) {
       if (editor.getItem() != null &&
           ((String)editor.getItem()).length() > 0) {
-        String serverStr = (String)editor.getItem();
-        int atIndex = serverStr.lastIndexOf('@');
-        if (atIndex >= 0) serverStr = serverStr.substring(atIndex + 1);
-        serverStr = Hostname.getHost(serverStr);
+        String serverStr = Hostname.getHost((String)editor.getItem());
         UserPreferences.load(serverStr, params);
         options.setNode(serverStr);
         options.showDialog(getJDialog());
@@ -229,28 +227,29 @@ class ServerDialog extends Dialog implements ActionListener {
   private boolean commit() {
     try {
 
-      String serverName = (String)editor.getItem();
-      if (serverName == null || serverName.equals(""))
+      String serverStr = (String)editor.getItem();
+      if (serverStr == null || serverStr.equals(""))
         throw new WarningException("No server name specified");
 
       // set params
       if (params.via.get() != null &&
           Hostname.getColonPos(params.via.get()) >= 0) {
-        params.server.set(serverName);
+        params.server.set(serverStr);
       } else {
-        params.server.set(Hostname.getHost(serverName));
-        params.port.set(Hostname.getPort(serverName));
-        params.udsPath = Hostname.getUDSPath(serverName);
+        params.sshUser.set(Hostname.getSSHUser(serverStr));
+        params.server.set(Hostname.getHost(serverStr));
+        params.port.set(Hostname.getPort(serverStr));
+        params.udsPath = Hostname.getUDSPath(serverStr);
       }
 
       // Update the history list
       String valueStr = UserPreferences.get("ServerDialog", "history");
       String t = (valueStr == null) ? "" : valueStr;
       StringTokenizer st = new StringTokenizer(t, ",");
-      StringBuffer sb = new StringBuffer().append(serverName);
+      StringBuffer sb = new StringBuffer().append(serverStr);
       while (st.hasMoreTokens()) {
         String str = st.nextToken();
-        if (!str.equals(serverName) && !str.equals("")) {
+        if (!str.equals(serverStr) && !str.equals("")) {
           sb.append(',');
           sb.append(str);
         }

CMakeLists.txt
@@ -14,8 +14,8 @@ endif()
 
 project(TurboVNC NONE)
 string(TOLOWER ${CMAKE_PROJECT_NAME} CMAKE_PROJECT_NAME_LC)
-set(VERSION 3.1.3)
-set(DOCVERSION 3.1.3)
+set(VERSION 3.1.4)
+set(DOCVERSION 3.1.4)
 set(COPYRIGHT_YEAR "1999-2024")
 set(COPYRIGHT "The VirtualGL Project and many others (see README.md)")
 set(URLTEXT "Visit http://www.TurboVNC.org for more information on TurboVNC")

doc/index.html
@@ -3,9 +3,9 @@
 <head>
 <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
 <meta name="language" content="en">
-<meta name="date" content="2024-09-25T15:02:45">
+<meta name="date" content="2024-11-19T14:57:09">
 <meta name="generator" content="deplate.rb 0.8.5">
-<title>User&rsquo;s Guide for TurboVNC 3.1.3</title>
+<title>User&rsquo;s Guide for TurboVNC 3.1.4</title>
 <link rel="start" href="index.html" title="Frontpage">
 <link rel="chapter" href="index.html#hd001" title="1 Legal Information">
 <link rel="chapter" href="index.html#hd002" title="2 Conventions Used in This Document">
@@ -23,7 +23,7 @@
 <body >
 <a name="#pagetop"></a>
 <div class="title">
-<p class="title">User&rsquo;s Guide for TurboVNC 3.1.3</p>
+<p class="title">User&rsquo;s Guide for TurboVNC 3.1.4</p>
 </div>
 <div id="hd">
 <div id="hdBlock" class="hd">
@@ -588,8 +588,8 @@ The TurboVNC security configuration file will not work when TurboVNC is installe
 <ol class="Ordered numeric">
     <li class="Ordered-1 Ordered">
         Download the TurboVNC Mac disk image 
-        (<strong class="filename">TurboVNC-3.1.3-x86_64.dmg</strong> for Intel 
-        CPUs or <strong class="filename">TurboVNC-3.1.3-arm64.dmg</strong> for 
+        (<strong class="filename">TurboVNC-3.1.4-x86_64.dmg</strong> for Intel 
+        CPUs or <strong class="filename">TurboVNC-3.1.4-arm64.dmg</strong> for 
         Apple silicon CPUs) from the 
         <span class="remote"><a href="https://github.com/TurboVNC/turbovnc/releases" target="_blank" class="remote">Releases 
         area</a></span><a name="idx0020"></a> of the 
@@ -610,8 +610,8 @@ The TurboVNC security configuration file will not work when TurboVNC is installe
 <ol class="Ordered numeric">
     <li class="Ordered-1 Ordered">
         Download the TurboVNC Windows installer package 
-        (<strong class="filename">TurboVNC-3.1.3-x64.exe</strong> for 64-bit 
-        systems or <strong class="filename">TurboVNC-3.1.3-x86.exe</strong> for 
+        (<strong class="filename">TurboVNC-3.1.4-x64.exe</strong> for 64-bit 
+        systems or <strong class="filename">TurboVNC-3.1.4-x86.exe</strong> for 
         legacy 32-bit-only systems) from the 
         <span class="remote"><a href="https://github.com/TurboVNC/turbovnc/releases" target="_blank" class="remote">Releases 
         area</a></span><a name="idx0022"></a> of the 
@@ -631,13 +631,13 @@ The TurboVNC security configuration file will not work when TurboVNC is installe
 
 <p>If you are using a Linux/Un*x platform for which there is not a 
 pre-built TurboVNC binary package available, then download the TurboVNC 
-source tarball (<strong class="filename">turbovnc-3.1.3.tar.gz</strong>) 
+source tarball (<strong class="filename">turbovnc-3.1.4.tar.gz</strong>) 
 from the 
 <span class="remote"><a href="https://github.com/TurboVNC/turbovnc/releases" target="_blank" class="remote">Releases 
 area</a></span><a name="idx0024"></a> of the 
 <span class="remote"><a href="https://github.com/TurboVNC/turbovnc" target="_blank" class="remote">TurboVNC 
 GitHub project page</a></span><a name="idx0025"></a>, uncompress it, 
-<code>cd turbovnc-3.1.3</code>, and read 
+<code>cd turbovnc-3.1.4</code>, and read 
 <strong class="filename">BUILDING.md</strong> for further instructions 
 on how to build TurboVNC from source.</p>
 

java/com/turbovnc/rfb/Hostname.java
@@ -30,9 +30,10 @@ public final class Hostname {
   public static int getColonPos(String vncServerName) {
     int colonPos = vncServerName.lastIndexOf(':');
     int bracketPos = vncServerName.lastIndexOf(']');
+    int atPos = vncServerName.lastIndexOf('@');
 
     // No colon = hostname only
-    if (colonPos == -1)
+    if (colonPos == -1 || colonPos < atPos)
       return -1;
     // Last colon is inside square brackets = IPv6 address only
     if (bracketPos != -1 && colonPos < bracketPos)
@@ -41,9 +42,9 @@ public final class Hostname {
     // series.
     while (colonPos > 0 && vncServerName.charAt(colonPos - 1) == ':')
       colonPos--;
-    if (colonPos == 0) {
+    if (colonPos == atPos + 1) {
       // IPv6 loopback address only (special case)
-      if (vncServerName.equals("::1"))
+      if (vncServerName.regionMatches(atPos + 1, "::1", 0, 3))
         return -1;
       // Display number/port/UDS specified without host
       return colonPos;
@@ -71,14 +72,27 @@ public final class Hostname {
     return colonPos;
   }
 
+  public static String getSSHUser(String vncServerName) {
+    int atPos = vncServerName.lastIndexOf('@');
+    String sshUser = null;
+
+    if (atPos >= 0) {
+      sshUser = vncServerName.substring(0, atPos).replaceAll("\\s", "");
+      if (sshUser.length() > 0)
+        return sshUser;
+    }
+    return null;
+  }
+
   public static String getHost(String vncServerName) {
     int colonPos = getColonPos(vncServerName);
+    int atPos = vncServerName.lastIndexOf('@');
 
-    if (colonPos == 0)
+    if (colonPos == atPos + 1)
       return "localhost";
-    if (colonPos == -1)
+    if (colonPos == -1 || colonPos < atPos)
       colonPos = vncServerName.length();
-    return vncServerName.substring(0, colonPos).replaceAll("\\s", "");
+    return vncServerName.substring(atPos + 1, colonPos).replaceAll("\\s", "");
   }
 
   public static int getPort(String vncServerName) {

commit 0cca3de74d33ab0dbae42b26bd925a60a2491359

Viewer: Don't ignore SSH username in conn. dialog
(regression introduced by ed9e4b245a371404ff821cac9e1fc9fe8bcc778e)

Fixes #451

java/com/turbovnc/vncviewer/ServerDialog.java
@@ -1,5 +1,5 @@
-/* Copyright (C) 2012-2015, 2018, 2020, 2022, 2024 D. R. Commander.
- *                                                 All Rights Reserved.
+/* Copyright (C) 2012-2015, 2018, 2020, 2022, 2024-2025 D. R. Commander.
+ *                                                      All Rights Reserved.
  * Copyright (C) 2011-2013 Brian P. Hinz
  * Copyright (C) 2002-2005 RealVNC Ltd.  All Rights Reserved.
  *
@@ -236,7 +236,11 @@ class ServerDialog extends Dialog implements ActionListener {
           Hostname.getColonPos(params.via.get()) >= 0) {
         params.server.set(serverStr);
       } else {
-        params.sshUser.set(Hostname.getSSHUser(serverStr));
+        String sshUser = Hostname.getSSHUser(serverStr);
+        if (sshUser != null) {
+          params.sshUser.set(sshUser);
+          params.sshUser.setCommandLine(true);
+        }
         params.server.set(Hostname.getHost(serverStr));
         params.port.set(Hostname.getPort(serverStr));
         params.udsPath = Hostname.getUDSPath(serverStr);