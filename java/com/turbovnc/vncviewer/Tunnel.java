TurboVNC/turbovnc

commit ae1c3fafe02982a871c1d30eb21c8771546cfd56

Viewer: Use separate parameter for jump host
The jump host feature is an improvement upon the traditional Via
feature, since it eliminates the need to open RFB ports in the VNC
host's firewall, it ensures that the RFB connection is encrypted on the
server-area network, and it allows the TurboVNC Session Manager to be
used with TurboVNC hosts that are behind an SSH gateway.  Thus, I
initially thought it best to repurpose the Via parameter to specify a
jump host and to hide the traditional Via functionality behind a Java
system property (turbovnc.viajump.)  This was effectively a deprecation
model for the traditional Via feature and effectively made the jump host
feature opt-out instead of opt-in.

However, because Via can also be used to specify an UltraVNC Repeater
gateway, repurposing the parameter made it impossible to specify
different SSH ports for the jump host and VNC host without using an
OpenSSH config file.  I could have hidden the UltraVNC Repeater gateway
feature behind turbovnc.viajump as well, but that would have created a
backward compatibility issue.  (For instance, if saved options from a
previous version of TurboVNC specified Via={gateway_host}:1, then
TurboVNC 3.2 would have interpreted 1 as an SSH port instead of an
UltraVNC Repeater display number.)  I could have also added a new
parameter (ViaSSHPort, for example) for specifying the jump host's SSH
port, but ultimately it was less confusing to just introduce a dedicated
parameter for specifying a jump host and to leave Via alone.  The new
Jump parameter behaves identically to ssh -J, except that it does not
allow multiple comma-separated hops to be specified.

Closes #440

java/com/turbovnc/vncviewer/Tunnel.java
@@ -43,81 +43,94 @@ import com.jcraft.jsch.*;
 public class Tunnel {
 
   public static void createTunnel(Params params) throws Exception {
-    int localPort, remotePort;
-    String gatewaySSHUser, gatewayHost, remoteSSHUser, remoteHost;
+    int vncPort;
 
-    boolean tunnel = params.tunnel.get() ||
-                     (params.sessMgrActive && params.sessMgrAuto.get());
-
-    if (tunnel) {
-      gatewaySSHUser = Hostname.getSSHUser(params.server.get());
-      gatewayHost = Hostname.getHost(params.server.get());
-      remoteSSHUser = null;
-      remoteHost = "localhost";
-    } else {
-      gatewaySSHUser = Hostname.getSSHUser(params.via.get());
-      gatewayHost = Hostname.getHost(params.via.get());
-      remoteSSHUser = Hostname.getSSHUser(params.server.get());
-      remoteHost = Hostname.getHost(params.server.get());
-    }
     if (params.server.get() != null &&
         Hostname.getColonPos(params.server.get()) < 0 && params.port.get() > 0)
-      remotePort = params.port.get();
+      vncPort = params.port.get();
     else
-      remotePort = Hostname.getPort(params.server.get());
+      vncPort = Hostname.getPort(params.server.get());
 
     String pattern = params.extSSHTemplate.get();
     if (pattern == null) {
-      if (tunnel) {
-        pattern = System.getProperty("turbovnc.tunnel");
-        if (pattern == null)
-          pattern = System.getenv("VNC_TUNNEL_CMD");
-      } else {
+      if (params.via.get() != null) {
         pattern = System.getProperty("turbovnc.via");
         if (pattern == null)
           pattern = System.getenv("VNC_VIA_CMD");
+      } else {
+        pattern = System.getProperty("turbovnc.tunnel");
+        if (pattern == null)
+          pattern = System.getenv("VNC_TUNNEL_CMD");
       }
     }
 
     if (params.udsPath != null &&
         (pattern == null || pattern.indexOf("%L") < 0)) {
       // Connect to Unix domain socket using stdio-based forwarding
-      params.stdioSocket = createTunnelExtUDS(gatewaySSHUser, gatewayHost,
-                                              remoteSSHUser, remoteHost,
-                                              pattern, params, tunnel);
+      params.stdioSocket = createTunnelExtUDS(pattern, params);
     } else {
-      localPort = TcpSocket.findFreeTcpPort();
+      int localPort = TcpSocket.findFreeTcpPort();
       if (localPort == 0)
         throw new ErrorException("Could not obtain free TCP port");
 
       if (params.extSSH.get() || (pattern != null && pattern.length() > 0))
-        createTunnelExt(gatewaySSHUser, gatewayHost, remoteSSHUser, remoteHost,
-                        remotePort, localPort, pattern, params, tunnel);
+        createTunnelExt(vncPort, localPort, pattern, params);
       else {
-        String gatewayStr = remoteHost;
-        if (Utils.getBooleanProperty("turbovnc.viajump", true) && !tunnel) {
+        String vncHost, gatewayStr;
+        int sshPort = params.sshPort.isDefault() ? -1 : params.sshPort.get();
+
+        if (params.jump.get() != null) {
+          gatewayStr = Hostname.getHost(params.server.get());
           if (params.sshSession == null) {
-            vlog.debug("Opening SSH connection to host " + remoteHost);
-            params.sshSession = createTunnelJSch(remoteSSHUser, remoteHost,
-                                                 params.sshPort.get(),
-                                                 gatewaySSHUser, gatewayHost,
-                                                 params);
+            vncHost = gatewayStr;
+            vlog.debug("Opening SSH connection to host " + vncHost);
+            params.sshSession =
+              createTunnelJSch(Hostname.getSSHUser(params.server.get()),
+                               vncHost, sshPort,
+                               Hostname.getSSHUser(params.jump.get()),
+                               Hostname.getSSHHost(params.jump.get()),
+                               Hostname.getSSHPort(params.jump.get()), params);
           }
-          remoteHost = "localhost";
+          vncHost = "localhost";
         } else {
+          String gatewaySSHUser, gatewayHost;
+
+          boolean tunnel = params.tunnel.get() ||
+                           (params.sessMgrActive &&
+                            params.sessMgrAuto.get());
+
+          if (tunnel) {
+            gatewaySSHUser = Hostname.getSSHUser(params.server.get());
+            gatewayHost = Hostname.getHost(params.server.get());
+            vncHost = "localhost";
+          } else {
+            gatewaySSHUser = Hostname.getSSHUser(params.via.get());
+            gatewayHost = Hostname.getHost(params.via.get());
+            vncHost = Hostname.getHost(params.server.get());
+            // If the Session Manager is active and SessMgrAuto=0, then Via
+            // uses multi-level SSH tunneling for the Session Manager's SSH
+            // connection, but it uses direct port forwarding for the RFB/SSH
+            // connection.  Thus, close the Session Manager's multi-level SSH
+            // connection so a new single-level SSH connection can be created
+            // below.
+            if (params.sessMgrActive && params.sshSession != null) {
+              params.sshSession.disconnect();
+              params.sshSession = null;
+            }
+          }
+
           if (params.sshSession == null) {
             vlog.debug("Opening SSH tunnel through gateway " + gatewayHost);
             params.sshSession = createTunnelJSch(gatewaySSHUser, gatewayHost,
-                                                 params.sshPort.get(), null,
-                                                 null, params);
+                                                 sshPort, null, null, -1,
+                                                 params);
           }
-          remoteHost = remoteHost.replaceAll("[\\[\\]]", "");
+          vncHost = vncHost.replaceAll("[\\[\\]]", "");
           gatewayStr = gatewayHost;
         }
-        vlog.debug("Forwarding local port " + localPort + " to " + remoteHost +
-                   "::" + remotePort + " (relative to " + gatewayStr + ")");
-        params.sshSession.setPortForwardingL(localPort, remoteHost,
-                                             remotePort);
+        vlog.debug("Forwarding local port " + localPort + " to " + vncHost +
+                   "::" + vncPort + " (relative to " + gatewayStr + ")");
+        params.sshSession.setPortForwardingL(localPort, vncHost, vncPort);
       }
       params.server.set("localhost::" + localPort);
     }
@@ -174,7 +187,8 @@ public class Tunnel {
 
   protected static Session createTunnelJSch(String user, String host, int port,
                                             String jumpUser, String jumpHost,
-                                            Params params) throws Exception {
+                                            int jumpPort, Params params)
+                                            throws Exception {
     JSch jsch = new JSch();
     JSch.setLogger(LOGGER);
     String homeDir = new String("");
@@ -244,8 +258,6 @@ public class Tunnel {
     }
 
     // username and passphrase will be given via UserInfo interface.
-    int jumpPort = params.sshPort.get();
-
     File sshConfigFile = new File(params.sshConfig.get());
     if (sshConfigFile.exists() && sshConfigFile.canRead()) {
       ConfigRepository repo =
@@ -256,7 +268,7 @@ public class Tunnel {
       if (repoUser != null && user == null)
         user = repoUser;
       int repoPort = repo.getConfig(host).getPort();
-      if (repoPort != -1 && params.sshPort.isDefault())
+      if (repoPort != -1 && port == -1)
         port = repoPort;
       String[] identityFiles = repo.getConfig(host).getValues("IdentityFile");
       if (identityFiles != null) {
@@ -269,23 +281,9 @@ public class Tunnel {
       String repoJumpHost = repo.getConfig(host).getValue("ProxyJump");
       if (repoJumpHost != null && jumpHost == null) {
         jumpHost = repoJumpHost.replaceAll("\\s", "");
-        int atPos = jumpHost.lastIndexOf('@');
-        if (jumpUser == null && atPos > 0)
-          jumpUser = jumpHost.substring(0, atPos);
-        int colonPos = Hostname.getColonPos(jumpHost);
-        if (colonPos >= 0) {
-          if (colonPos != jumpHost.length() - 1) {
-            try {
-              int tempPort =
-                Integer.parseInt(jumpHost.substring(colonPos + 1));
-              if (tempPort >= 0 && tempPort <= 65535)
-                jumpPort = tempPort;
-            } catch (NumberFormatException e) {
-            }
-          }
-          if (atPos + 1 < colonPos)
-            jumpHost = jumpHost.substring(atPos + 1, colonPos);
-        }
+        jumpUser = Hostname.getSSHUser(jumpHost);
+        jumpPort = Hostname.getSSHPort(jumpHost);
+        jumpHost = Hostname.getSSHHost(jumpHost);
       }
     } else {
       if (params.sshConfig.isDefault()) {
@@ -331,8 +329,9 @@ public class Tunnel {
     if (jumpHost != null) {
       vlog.debug("Opening SSH connection to jump host " + jumpHost);
       jumpSSHSession = createTunnelJSch(jumpUser, jumpHost, jumpPort, null,
-                                        null, params);
+                                        null, -1, params);
     }
+    if (port == -1) port = params.sshPort.getDefault();
     sshSession = jsch.getSession(user, host, port);
     if (jumpSSHSession != null) {
       JumpProxy proxy = new JumpProxy(jumpSSHSession);
@@ -369,23 +368,22 @@ public class Tunnel {
 
   public static final String DEFAULT_TUNNEL_CMD =
     "%S -f -L %L:localhost:%R %H sleep 20";
+  public static final String DEFAULT_JUMP_CMD =
+    "%S -J %G -f -L %L:localhost:%R %H sleep 20";
   public static final String DEFAULT_VIA_CMD =
-    Utils.getBooleanProperty("turbovnc.viajump", true) ?
-    "%S -J %G -f -L %L:localhost:%R %H sleep 20" :
     "%S -f -L %L:%H:%R %G sleep 20";
 
-  private static void createTunnelExt(String gatewaySSHUser,
-                                      String gatewayHost, String remoteSSHUser,
-                                      String remoteHost, int remotePort,
-                                      int localPort, String pattern,
-                                      Params params, boolean tunnel)
+  private static void createTunnelExt(int vncPort, int localPort,
+                                      String pattern, Params params)
                                       throws Exception {
     if (pattern == null || pattern.length() < 1)
-      pattern = (tunnel ? DEFAULT_TUNNEL_CMD : DEFAULT_VIA_CMD);
+      pattern =
+        (params.tunnel.get() ? DEFAULT_TUNNEL_CMD :
+                               (params.jump.get() != null ? DEFAULT_JUMP_CMD :
+                                                            DEFAULT_VIA_CMD));
 
-    String command = fillCmdPattern(pattern, gatewaySSHUser, gatewayHost,
-                                    remoteSSHUser, remoteHost, remotePort,
-                                    params.udsPath, localPort, params, tunnel);
+    String command = fillCmdPattern(pattern, vncPort, params.udsPath,
+                                    localPort, params);
 
     vlog.debug("SSH command line: " + command);
     List<String> args = ArgumentTokenizer.tokenize(command);
@@ -400,26 +398,21 @@ public class Tunnel {
 
   public static final String DEFAULT_TUNNEL_CMD_UDS =
     "%S -- %H exec socat stdio unix-connect:%R";
-  public static final String DEFAULT_VIA_CMD_UDS =
+  public static final String DEFAULT_JUMP_CMD_UDS =
     "%S -J %G -- %H exec socat stdio unix-connect:%R";
 
-  private static Socket createTunnelExtUDS(String gatewaySSHUser,
-                                           String gatewayHost,
-                                           String remoteSSHUser,
-                                           String remoteHost, String pattern,
-                                           Params params, boolean tunnel)
+  private static Socket createTunnelExtUDS(String pattern, Params params)
                                            throws Exception {
     if (pattern == null || pattern.length() < 1)
-      pattern = (tunnel ? DEFAULT_TUNNEL_CMD_UDS : DEFAULT_VIA_CMD_UDS);
+      pattern = (params.tunnel.get() ? DEFAULT_TUNNEL_CMD_UDS :
+                                       DEFAULT_JUMP_CMD_UDS);
 
     // Escape the Unix domain socket path twice, since it will be interpreted
     // once by ArgumentTokenizer.tokenize() and again by the remote shell.
     String udsPath = escapeUDSPath(params.udsPath, true);
     udsPath = escapeUDSPath(udsPath, false);
 
-    String command = fillCmdPattern(pattern, gatewaySSHUser, gatewayHost,
-                                    remoteSSHUser, remoteHost, -1, udsPath, -1,
-                                    params, tunnel);
+    String command = fillCmdPattern(pattern, -1, udsPath, -1, params);
 
     vlog.debug("SSH command line (stdio): " + command);
     List<String> args = ArgumentTokenizer.tokenize(command);
@@ -449,12 +442,9 @@ public class Tunnel {
     }
   }
 
-  private static String fillCmdPattern(String pattern, String gatewaySSHUser,
-                                       String gatewayHost,
-                                       String remoteSSHUser, String remoteHost,
-                                       int remotePort, String udsPath,
-                                       int localPort, Params params,
-                                       boolean tunnel) {
+  private static String fillCmdPattern(String pattern, int vncPort,
+                                       String udsPath, int localPort,
+                                       Params params) {
     int i, j;
     boolean hFound = false, gFound = false, rFound = false, lFound = false;
     String command = "";
@@ -466,25 +456,24 @@ public class Tunnel {
             command += params.extSSHCommand.get();
             continue;
           case 'H':
-            String sshUser = (tunnel ? gatewaySSHUser : remoteSSHUser);
-            if (sshUser != null)
-              command += sshUser + "@" + (tunnel ? gatewayHost : remoteHost);
+            String vncSSHUser = Hostname.getSSHUser(params.server.get());
+            String vncHost = Hostname.getHost(params.server.get());
+            if (vncSSHUser != null)
+              command += vncSSHUser + "@" + vncHost;
             else
-              command += (tunnel ? gatewayHost : remoteHost);
+              command += vncHost;
             hFound = true;
             continue;
           case 'G':
-            if (gatewaySSHUser != null)
-              command += gatewaySSHUser + "@" + gatewayHost;
-            else
-              command += gatewayHost;
+            command += (params.jump.get() != null ? params.jump.get() :
+                                                    params.via.get());
             gFound = true;
             continue;
           case 'R':
             if (udsPath != null)
               command += udsPath;
             else
-              command += remotePort;
+              command += vncPort;
             rFound = true;
             continue;
           case 'L':
@@ -499,7 +488,7 @@ public class Tunnel {
     if (!hFound || !rFound || (!lFound && udsPath == null))
       throw new ErrorException("%H, %R or %L absent in tunneling command template.");
 
-    if (!tunnel && !gFound)
+    if ((params.jump.get() != null || params.via.get() != null) && !gFound)
       throw new ErrorException("%G pattern absent in tunneling command template.");
 
     return command;

doc/index.html
@@ -3,7 +3,7 @@
 <head>
 <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
 <meta name="language" content="en">
-<meta name="date" content="2025-01-13T14:33:46">
+<meta name="date" content="2025-01-23T12:47:25">
 <meta name="generator" content="deplate.rb 0.8.5">
 <title>User&rsquo;s Guide for TurboVNC 3.2</title>
 <link rel="start" href="index.html" title="Frontpage">
@@ -1021,12 +1021,12 @@ use Anonymous TLS encryption.  (Refer to Chapter
 preferable to secure the TurboVNC connection using SSH rather than 
 Anonymous TLS encryption, particularly if one does not want to open 
 additional ports in the host&rsquo;s firewall.  This can easily be 
-accomplished using the TurboVNC Viewer&rsquo;s <code>Via</code> and 
-<code>Tunnel</code> parameters (or the equivalent GUI options, which are 
+accomplished using the TurboVNC Viewer&rsquo;s <code>Tunnel</code> and 
+<code>Jump</code> parameters (or the equivalent GUI options, which are 
 located under the &ldquo;Security&rdquo; tab in the TurboVNC Viewer 
 Options dialog.)</p>
 
-<p>The TurboVNC Viewer&rsquo;s <code>Via</code> and <code>Tunnel</code> 
+<p>The TurboVNC Viewer&rsquo;s <code>Tunnel</code> and <code>Jump</code> 
 parameters take advantage of the port forwarding feature in SSH.  For 
 instance, running</p>
 
@@ -1035,26 +1035,21 @@ instance, running</p>
 
 <pre class="verbatim">ssh -L <em>fp</em>:localhost:<em>5900+n</em> <em>user</em>@<em>host</em>
 <em>vncviewer</em> localhost::<em>fp</em></pre>
-<p>and running</p>
+<p>where <em><code>fp</code></em> is a free TCP port on the client machine 
+(this is automatically determined by the TurboVNC Viewer.)  Similarly, 
+running</p>
 
-<pre class="verbatim"><em>vncviewer</em> -via <em>gateway-user</em>@<em>gateway-host</em> <em>vnc-user</em>@<em>vnc-host</em>:<em>n</em></pre>
+<pre class="verbatim"><em>vncviewer</em> -jump <em>jump-user</em>@<em>jump-host</em>:<em>jump-port</em> <em>vnc-user</em>@<em>vnc-host</em>:<em>n</em></pre>
 <p>is the equivalent of running</p>
 
-<pre class="verbatim">ssh -J <em>gateway-user</em>@<em>gateway-host</em> -L <em>fp</em>:localhost:<em>5900+n</em> <em>vnc-user</em>@<em>vnc-host</em>
+<pre class="verbatim">ssh -J <em>jump-user</em>@<em>jump-host</em>:<em>jump-port</em> -L <em>fp</em>:localhost:<em>5900+n</em> <em>vnc-user</em>@<em>vnc-host</em>
 <em>vncviewer</em> localhost::<em>fp</em></pre>
-<p>where <em><code>fp</code></em> is a free TCP port on the client machine 
-(this is automatically determined by the TurboVNC Viewer.)</p>
-
 <div class="important"><p class="important">
 In the above examples, <em><code>vncviewer</code></em> is the command used to launch the TurboVNC Viewer&ndash; <code>/opt/TurboVNC/bin/vncviewer</code> on Linux/Un*x and Mac systems and <code>c:\Program&nbsp;Files\TurboVNC\vncviewer.bat</code> on Windows systems.
 </p></div>
 
 <div class="important"><p class="important">
-The <code>Tunnel</code> parameter produces better performance than the <code>Via</code> parameter when the SSH and VNC hosts are the same machine, since the <code>Tunnel</code> parameter avoids an unnecessary two-level SSH tunnel.
-</p></div>
-
-<div class="important"><p class="important">
-When using the <code>Via</code> parameter, the VNC host is specified from the point of view of the gateway host.
+When using the <code>Jump</code> parameter, the VNC host is specified from the point of view of the gateway host.
 </p></div>
 
 
@@ -2996,42 +2991,6 @@ The Java system properties listed below can also be specified in
         The default behavior of the TurboVNC Viewer is to display the banner 
         message from the SSH server on the command line.  Enabling this property 
         causes the viewer to display the banner message in a dialog box instead.
-    </dd>
-</dl>
-
-<div class="table">
-<table class="standard">
-  <tr class="standard">
-    <td class="high standard">Java System Property</td>
-    <td class="standard"><code>turbovnc.viajump = <em>0 | 1</em></code></td>
-  </tr>
-  <tr class="standard">
-    <td class="high standard">Summary</td>
-    <td class="standard">Disable/enable multi-level SSH tunneling with the <code>Via</code> parameter</td>
-  </tr>
-  <tr class="standard">
-    <td class="high standard">Default Value</td>
-    <td class="standard">Enabled</td>
-  </tr>
-</table>
-</div>
-
-
-<dl class="Description">
-    <dt class="Description-1 Description">Description</dt>
-    <dd class="Description-1 Description">
-        By default, the TurboVNC Viewer&rsquo;s <code>Via</code> parameter 
-        creates an intermediate SSH tunnel to the gateway host and uses that 
-        tunnel to create the final SSH tunnel to the VNC host.  This eliminates 
-        the need to open RFB ports in the VNC host&rsquo;s firewall, it ensures 
-        that the RFB connection is encrypted on the server-area network, and it 
-        allows the TurboVNC Session Manager to be used with TurboVNC hosts that 
-        are behind an SSH gateway. Disabling this property restores the behavior 
-        of version 3.1.x and prior of the TurboVNC Viewer, whereby the 
-        <code>Via</code> parameter creates an SSH tunnel to the gateway host and 
-        forwards the RFB connection directly to the VNC host from the gateway 
-        host.  Note that this property is effectively always enabled when using 
-        the TurboVNC Session Manager.
     </dd>
 </dl>
 

java/com/turbovnc/rfb/Hostname.java
@@ -1,4 +1,4 @@
-/* Copyright (C) 2012, 2016, 2018, 2020, 2022-2024 D. R. Commander.
+/* Copyright (C) 2012, 2016, 2018, 2020, 2022-2025 D. R. Commander.
  *                                                 All Rights Reserved.
  * Copyright (C) 2002-2005 RealVNC Ltd.  All Rights Reserved.
  *
@@ -80,11 +80,24 @@ public final class Hostname {
       return null;
 
     int atPos = vncServerName.lastIndexOf('@');
-    String sshUser = null;
 
     if (atPos > 0)
       return vncServerName.substring(0, atPos);
     return null;
+  }
+
+  public static String getSSHHost(String vncServerName) {
+    if (vncServerName == null)
+      return null;
+
+    int colonPos = getColonPos(vncServerName);
+    int atPos = vncServerName.lastIndexOf('@');
+
+    if (colonPos == atPos + 1 || atPos == vncServerName.length() - 1)
+      return null;
+    if (colonPos == -1 || colonPos < atPos)
+      colonPos = vncServerName.length();
+    return vncServerName.substring(atPos + 1, colonPos);
   }
 
   public static String getHost(String vncServerName) {
@@ -99,6 +112,21 @@ public final class Hostname {
     if (colonPos == -1 || colonPos < atPos)
       colonPos = vncServerName.length();
     return vncServerName.substring(atPos + 1, colonPos);
+  }
+
+  public static int getSSHPort(String vncServerName) {
+    int colonPos = getColonPos(vncServerName);
+
+    if (colonPos >= 0 && colonPos != vncServerName.length() - 1) {
+      try {
+        int port = Integer.parseInt(vncServerName.substring(colonPos + 1));
+        if (port >= 0 && port <= 65535)
+          return port;
+      } catch (NumberFormatException e) {
+      }
+    }
+
+    return -1;
   }
 
   public static int getPort(String vncServerName) {

java/com/turbovnc/rfb/IntParameter.java
@@ -1,4 +1,4 @@
-/* Copyright (C) 2012, 2017-2018, 2022-2024 D. R. Commander.
+/* Copyright (C) 2012, 2017-2018, 2022-2025 D. R. Commander.
  *                                          All Rights Reserved.
  * Copyright (C) 2002-2005 RealVNC Ltd.  All Rights Reserved.
  *
@@ -100,6 +100,10 @@ public class IntParameter extends VoidParameter {
     if (defValue >= 0)
       return Integer.toString(defValue);
     return null;
+  }
+
+  public synchronized int getDefault() {
+    return defValue;
   }
 
   public synchronized String getStr() { return Integer.toString(value); }

commit 07b2638580e1f9c776132e2d3983f64f56bd1120

Viewer: Don't require Tunnel w/ sess mgr & ExtSSH
Using an external SSH client with the TurboVNC Session Manager is not
officially documented.  However, it works with the caveat that the
external SSH client is only used for the final VNC connection.  (The
built-in SSH client is still used for session management.)
ae1c3fafe02982a871c1d30eb21c8771546cfd56 introduced a regression
whereby, when attempting to use ExtSSH with the TurboVNC Session
Manager, the viewer used the Via command-line template by default
instead of the Tunnel command-line template, which failed because no
gateway host had been specified.

java/com/turbovnc/vncviewer/Tunnel.java
@@ -371,11 +371,14 @@ public class Tunnel {
   private static void createTunnelExt(int vncPort, int localPort,
                                       String pattern, Params params)
                                       throws Exception {
+    boolean tunnel = params.tunnel.get() ||
+                     (params.sessMgrActive && params.sessMgrAuto.get());
+
     if (pattern == null || pattern.length() < 1)
       pattern =
-        (params.tunnel.get() ? DEFAULT_TUNNEL_CMD :
-                               (params.jump.get() != null ? DEFAULT_JUMP_CMD :
-                                                            DEFAULT_VIA_CMD));
+        (tunnel ? DEFAULT_TUNNEL_CMD :
+                  (params.jump.get() != null ? DEFAULT_JUMP_CMD :
+                                               DEFAULT_VIA_CMD));
 
     String command = fillCmdPattern(pattern, vncPort, params.udsPath,
                                     localPort, params);